63856ed4fd2bd641bd9804e8b846a297f7094f7a
smarshall <smarshall@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2007-07-24 07:56:38 +0000

    svnimporter: adding tags to revision 311
    git-svn-id: svn://127.0.0.1/Core@312 17c802ae-cbca-534c-9e8e-6d19a4926c45

---
+++ tags/v4.2.1.0/admin/ctx/nm_sql_context.sql
@@ -0,0 +1,11 @@
+--   PVCS Identifiers :-
+--
+--       sccsid           : $Header:   //vm_latest/archives/nm3/admin/ctx/nm_sql_context.sql-arc   2.0   Jul 24 2007 08:56:38   smarshall  $
+--       Module Name      : $Workfile:   nm_sql_context.sql  $
+--       Date into PVCS   : $Date:   Jul 24 2007 08:56:38  $
+--       Date fetched Out : $Modtime:   Jul 24 2007 08:56:18  $
+--       PVCS Version     : $Revision:   2.0  $
+--
+-- requires create any context system privilege
+create or replace context NM_SQL using NM3SQL
+/
