1aadf891851ab7197e376713c89ea317e5b6d34f
ptanava <ptanava@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2007-09-07 15:42:24 +0000

    Added nm3dbg and nm3sql
    git-svn-id: svn://127.0.0.1/Core@461 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/pck/nm3pkb.sql
+++ trunk/admin/pck/nm3pkb.sql
@@ -2,11 +2,11 @@
 --
 --   PVCS Identifiers :-
 --
---       pvcsid           : $Header:   //vm_latest/archives/nm3/admin/pck/nm3pkb.sql-arc   2.2   Jul 19 2007 15:12:44   smarshall  $
+--       pvcsid           : $Header:   //vm_latest/archives/nm3/admin/pck/nm3pkb.sql-arc   2.3   Sep 07 2007 16:42:24   ptanava  $
 --       Module Name      : $Workfile:   nm3pkb.sql  $
---       Date into PVCS   : $Date:   Jul 19 2007 15:12:44  $
---       Date fetched Out : $Modtime:   Jul 19 2007 14:21:44  $
---       PVCS Version     : $Revision:   2.2  $
+--       Date into PVCS   : $Date:   Sep 07 2007 16:42:24  $
+--       Date fetched Out : $Modtime:   Sep 07 2007 16:40:18  $
+--       PVCS Version     : $Revision:   2.3  $
 --
 --
 --   Author : Graeme Johnson
@@ -1844,6 +1844,28 @@ FROM dual
 / 
 start '&run_file'
 
+--
+----------------------------------------------------------------------------------------- 
+--
+SET TERM ON 
+PROMPT nm3dbg.pkw
+SET TERM OFF
+SET DEFINE ON 
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||'&terminator'||'pck'||'&terminator'||'nm3dbg.pkw' run_file
+FROM dual 
+/ 
+start '&run_file'
+--
+----------------------------------------------------------------------------------------- 
+--
+SET TERM ON 
+PROMPT nm3sql.pkw
+SET TERM OFF
+SET DEFINE ON 
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||'&terminator'||'pck'||'&terminator'||'nm3sql.pkw' run_file
+FROM dual 
+/ 
+start '&run_file'
 --
 -- New PACKAGE BODIES above here
 --
