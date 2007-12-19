0f69430fc83f6d9bad2c29c6d963673739f7138b
jwadsworth <jwadsworth@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2007-12-19 10:03:12 +0000

    Removed call to nm0575_matching_records.vw
    git-svn-id: svn://127.0.0.1/Core@812 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/views/nm3views.sql
+++ trunk/admin/views/nm3views.sql
@@ -903,17 +903,6 @@ start '&run_file'
 ----------------------------------------------------------------------------------------- 
 --
 SET TERM ON 
-PROMPT nm0575_matching_records.vw
-SET TERM OFF
-SET DEFINE ON 
-SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||'&terminator'||'views'||'&terminator'||'nm0575_matching_records.vw' run_file 
-FROM dual 
-/ 
-start '&run_file' 
---
------------------------------------------------------------------------------------------ 
---
-SET TERM ON 
 PROMPT v_load_resize_route.vw
 SET TERM OFF
 SET DEFINE ON 
