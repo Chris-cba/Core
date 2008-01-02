a609d592b0778f5cefe05fd23e4a6fae6e700b6f
bodriscoll <bodriscoll@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2008-01-02 16:23:50 +0000

    Added v_std_text_modules.vw
    git-svn-id: svn://127.0.0.1/Core@835 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/views/nm3views.sql
+++ trunk/admin/views/nm3views.sql
@@ -957,3 +957,14 @@ start '&run_file'
 --
 ----------------------------------------------------------------------------------------- 
 --
+SET TERM ON 
+PROMPT v_std_text_modules.vw
+SET TERM OFF
+SET DEFINE ON 
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||'&terminator'||'views'||'&terminator'||'v_std_text_modules.vw' run_file 
+FROM dual 
+/ 
+start '&run_file' 
+--
+----------------------------------------------------------------------------------------- 
+--
