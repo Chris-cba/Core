f9d6ca5233d835d8b5d10654a7fe1141ecfc7f56
aedwards <aedwards@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2008-06-12 10:28:30 +0000

    Added nm3sdo_check and nm3sde_check packages
    git-svn-id: svn://127.0.0.1/Core@1107 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/pck/nm3pkh.sql
+++ trunk/admin/pck/nm3pkh.sql
@@ -2,11 +2,11 @@
 --
 --   PVCS Identifiers :-
 --
---       pvcsid           : $Header:   //vm_latest/archives/nm3/admin/pck/nm3pkh.sql-arc   2.6   Jan 24 2008 20:41:22   aedwards  $
+--       pvcsid           : $Header:   //vm_latest/archives/nm3/admin/pck/nm3pkh.sql-arc   2.7   Jun 12 2008 11:28:30   aedwards  $
 --       Module Name      : $Workfile:   nm3pkh.sql  $
---       Date into PVCS   : $Date:   Jan 24 2008 20:41:22  $
---       Date fetched Out : $Modtime:   Jan 24 2008 20:40:26  $
---       PVCS Version     : $Revision:   2.6  $
+--       Date into PVCS   : $Date:   Jun 12 2008 11:28:30  $
+--       Date fetched Out : $Modtime:   Jun 12 2008 11:28:22  $
+--       PVCS Version     : $Revision:   2.7  $
 --
 --
 --   Author : Graeme Johnson
@@ -2152,7 +2152,28 @@ start '&run_file'
 --
 ----------------------------------------------------------------------------------------- 
 --
-
+SET TERM ON 
+PROMPT nm3sdo_check.pkh
+SET TERM OFF
+SET DEFINE ON 
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||'&terminator'||'pck'||'&terminator'||'nm3sdo_check.pkh' run_file
+FROM dual 
+/ 
+start '&run_file'
+--
+----------------------------------------------------------------------------------------- 
+--
+SET TERM ON 
+PROMPT nm3sde_check.pkh
+SET TERM OFF
+SET DEFINE ON 
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||'&terminator'||'pck'||'&terminator'||'nm3sde_check.pkh' run_file
+FROM dual 
+/ 
+start '&run_file'
+--
+----------------------------------------------------------------------------------------- 
+--
 --
 -- New PACKAGE HEADERS above here
 --
