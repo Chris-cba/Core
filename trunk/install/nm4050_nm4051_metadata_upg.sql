5096314f8cb1faac45f6a919a478a12287bb8031
aedwards <aedwards@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2008-12-05 12:09:14 +0000

    Ammeded the mapbuilder upgrade to make it a bit safer.
    git-svn-id: svn://127.0.0.1/Core@1578 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/install/nm4050_nm4051_metadata_upg.sql
+++ trunk/install/nm4050_nm4051_metadata_upg.sql
@@ -8,11 +8,11 @@
 --
 --   PVCS Identifiers :-
 --
---       PVCS id          : $Header:   //vm_latest/archives/nm3/install/nm4050_nm4051_metadata_upg.sql-arc   3.2   Dec 03 2008 09:48:28   malexander  $
+--       PVCS id          : $Header:   //vm_latest/archives/nm3/install/nm4050_nm4051_metadata_upg.sql-arc   3.3   Dec 05 2008 12:09:14   aedwards  $
 --       Module Name      : $Workfile:   nm4050_nm4051_metadata_upg.sql  $
---       Date into PVCS   : $Date:   Dec 03 2008 09:48:28  $
---       Date fetched Out : $Modtime:   Dec 03 2008 09:44:58  $
---       Version          : $Revision:   3.2  $
+--       Date into PVCS   : $Date:   Dec 05 2008 12:09:14  $
+--       Date fetched Out : $Modtime:   Dec 05 2008 12:08:32  $
+--       Version          : $Revision:   3.3  $
 --
 ------------------------------------------------------------------
 --	Copyright (c) exor corporation ltd, 2008
@@ -232,6 +232,7 @@ END;
 
 
 
+
 --------------------------------------------------
 --  Apply new USER_SDO_X views to HIGHWAYS OWNER
 --------------------------------------------------
@@ -341,6 +342,230 @@ BEGIN
 END;
 /
 
+BEGIN
+  EXECUTE IMMEDIATE 'DROP TRIGGER USER_SDO_MAPS_INS_TRG';
+EXCEPTION
+  WHEN OTHERS THEN NULL;
+END;
+/
+
+PROMPT Create USER_SDO_MAPS trigger
+
+BEGIN
+--
+  EXECUTE IMMEDIATE
+    'CREATE OR REPLACE TRIGGER '||hig.get_application_owner||'.USER_SDO_MAPS_INS_TRG '||chr(10)||
+    '------------------------------------------------------------------------- '||chr(10)||
+    '--   PVCS Identifiers :- '||chr(10)||
+    '-- '||chr(10)||
+    '--       PVCS id          : $Header:   //vm_latest/archives/nm3/install/nm4050_nm4051_metadata_upg.sql-arc   3.3   Dec 05 2008 12:09:14   aedwards  $ '||chr(10)||
+    '--       Module Name      : $Workfile:   nm4050_nm4051_metadata_upg.sql  $ '||chr(10)||
+    '--       Date into PVCS   : $Date:   Dec 05 2008 12:09:14  $ '||chr(10)||
+    '--       Date fetched Out : $Modtime:   Dec 05 2008 12:08:32  $ '||chr(10)||
+    '--       Version          : $Revision:   3.3  $ '||chr(10)||
+    '--       Based on SCCS version : '||chr(10)|| 
+    '------------------------------------------------------------------------- '||chr(10)||
+    'INSTEAD OF DELETE OR INSERT OR UPDATE '||chr(10)||
+    'ON '||hig.get_application_owner||'.USER_SDO_MAPS '||chr(10)|| 
+    'REFERENCING NEW AS NEW OLD AS OLD '||chr(10)||
+    'FOR EACH ROW '||chr(10)||
+    'DECLARE '||chr(10)||
+    '   l_rec_usm user_sdo_maps%ROWTYPE; '||chr(10)||
+    'BEGIN '||chr(10)||
+    '-- '||chr(10)||
+    '  IF INSERTING '||chr(10)|| 
+    '  THEN '||chr(10)||
+    '  -- '||chr(10)||
+    '    l_rec_usm.name        := :NEW.name; '||chr(10)||
+    '    l_rec_usm.description := :NEW.description; '||chr(10)||
+    '    l_rec_usm.definition  := :NEW.definition; '||chr(10)|| 
+    '  -- '||chr(10)||
+    '    nm3msv_sec.do_usm_ins ( pi_username => USER '||chr(10)||
+    '                          , pi_rec_usm  => l_rec_usm ); '||chr(10)||
+    '  -- '||chr(10)||
+    '  ELSIF DELETING '||chr(10)|| 
+    '  THEN '||chr(10)||
+    '  -- '||chr(10)||
+    '    l_rec_usm.name        := :OLD.name; '||chr(10)||
+    '    l_rec_usm.description := :OLD.description; '||chr(10)||
+    '    l_rec_usm.definition  := :OLD.definition; '||chr(10)||
+    '  -- '||chr(10)||
+    '    nm3msv_sec.do_usm_del ( pi_username => USER '||chr(10)||
+    '                          , pi_rec_usm  => l_rec_usm ); '||chr(10)||
+    '  -- '||chr(10)||
+    '  ELSIF UPDATING '||chr(10)|| 
+    '  THEN '||chr(10)||
+    '  -- '||chr(10)||
+    '    l_rec_usm.name        := NVL (:NEW.name, :OLD.name); '||chr(10)||
+    '    l_rec_usm.description := NVL (:NEW.description, :OLD.description); '||chr(10)||
+    '    l_rec_usm.definition  := NVL (:NEW.definition, :OLD.definition); '||chr(10)||
+    '  -- '||chr(10)||
+    '    nm3msv_sec.do_usm_upd ( pi_username => USER '||chr(10)||
+    '                          , pi_rec_usm  => l_rec_usm ); '||chr(10)||
+    '  -- '||chr(10)||
+    '  END IF; '||chr(10)||
+    '-- '||chr(10)||
+    'END; ';
+END;
+/
+
+
+
+BEGIN
+  EXECUTE IMMEDIATE 'DROP TRIGGER USER_SDO_STYLES_INS_TRG';
+EXCEPTION
+  WHEN OTHERS THEN NULL;
+END;
+/
+
+PROMPT Create USER_SDO_STYLES trigger
+
+BEGIN
+--
+  EXECUTE IMMEDIATE
+    'CREATE OR REPLACE TRIGGER '||hig.get_application_owner||'.USER_SDO_STYLES_INS_TRG '||chr(10)||
+    '------------------------------------------------------------------------- '||chr(10)||
+    '--   PVCS Identifiers :- '||chr(10)||
+    '-- '||chr(10)||
+    '--       PVCS id          : $Header:   //vm_latest/archives/nm3/install/nm4050_nm4051_metadata_upg.sql-arc   3.3   Dec 05 2008 12:09:14   aedwards  $ '||chr(10)||
+    '--       Module Name      : $Workfile:   nm4050_nm4051_metadata_upg.sql  $ '||chr(10)||
+    '--       Date into PVCS   : $Date:   Dec 05 2008 12:09:14  $ '||chr(10)||
+    '--       Date fetched Out : $Modtime:   Dec 05 2008 12:08:32  $ '||chr(10)||
+    '--       Version          : $Revision:   3.3  $ '||chr(10)||
+    '--       Based on SCCS version : '||chr(10)|| 
+    '------------------------------------------------------------------------- '||chr(10)||
+    'INSTEAD OF DELETE OR INSERT OR UPDATE '||chr(10)||
+    'ON '||hig.get_application_owner||'.USER_SDO_STYLES REFERENCING NEW AS NEW OLD AS OLD '||chr(10)||
+    'FOR EACH ROW '||chr(10)||
+    'DECLARE '||chr(10)||
+    '  l_rec_uss user_sdo_styles%ROWTYPE; '||chr(10)||
+    'BEGIN '||chr(10)||
+    '-- '||chr(10)||
+    '  IF INSERTING '||chr(10)|| 
+    '  THEN '||chr(10)||
+    '  -- '||chr(10)||
+    '    l_rec_uss.name         := :NEW.name; '||chr(10)||
+    '    l_rec_uss.type         := :NEW.type; '||chr(10)||
+    '    l_rec_uss.description  := :NEW.description; '||chr(10)||
+    '    l_rec_uss.definition   := :NEW.definition; '||chr(10)||
+    '    l_rec_uss.image        := :NEW.image; '||chr(10)||
+    '    l_rec_uss.geometry     := :NEW.geometry; '||chr(10)||
+    '  -- '||chr(10)||
+    '    nm3msv_sec.do_uss_ins ( pi_username => USER '||chr(10)||
+    '                          , pi_rec_uss  => l_rec_uss ); '||chr(10)||
+    '  -- '||chr(10)||
+    '  ELSIF DELETING '||chr(10)|| 
+    '  THEN '||chr(10)||
+    '  -- '||chr(10)||
+    '    l_rec_uss.name         := :OLD.name; '||chr(10)||
+    '    l_rec_uss.type         := :OLD.type; '||chr(10)||
+    '    l_rec_uss.description  := :OLD.description; '||chr(10)||
+    '    l_rec_uss.definition   := :OLD.definition; '||chr(10)||
+    '    l_rec_uss.image        := :OLD.image; '||chr(10)||
+    '    l_rec_uss.geometry     := :OLD.geometry; '||chr(10)||
+    '  -- '||chr(10)||
+    '     nm3msv_sec.do_uss_del ( pi_username => USER '||chr(10)||
+    '                           , pi_rec_uss  => l_rec_uss ); '||chr(10)|| 
+    '  -- '||chr(10)||
+    '  ELSIF UPDATING '||chr(10)|| 
+    '  THEN '||chr(10)||
+    '  -- '||chr(10)||
+    '    l_rec_uss.name         := NVL (:NEW.name, :OLD.name); '||chr(10)||
+    '    l_rec_uss.type         := NVL (:NEW.type, :OLD.type); '||chr(10)||
+    '    l_rec_uss.description  := NVL (:NEW.description, :OLD.description); '||chr(10)||
+    '    l_rec_uss.definition   := NVL (:NEW.definition, :OLD.definition); '||chr(10)||
+    '  -- '||chr(10)||
+    '    IF :NEW.image IS NOT NULL '||chr(10)|| 
+    '    THEN '||chr(10)||
+    '      l_rec_uss.image     :=:NEW.image; '||chr(10)||
+    '    ELSE '||chr(10)||
+    '      l_rec_uss.image     :=:OLD.image; '||chr(10)||
+    '    END IF; '||chr(10)||
+    '  -- '||chr(10)||
+    '    l_rec_uss.geometry := NVL (:NEW.geometry, :OLD.geometry); '||chr(10)||
+    '  -- '||chr(10)||
+    '    nm3msv_sec.do_uss_upd ( pi_username => USER '||chr(10)||
+    '                          , pi_rec_uss  => l_rec_uss ); '||chr(10)||
+    '  -- '||chr(10)||
+    '  END IF; '||chr(10)||
+    '-- '||chr(10)||
+    'END; ';
+END;
+/
+
+BEGIN
+  EXECUTE IMMEDIATE 'DROP TRIGGER USER_SDO_THEMES_INS_TRG';
+EXCEPTION
+  WHEN OTHERS THEN NULL;
+END;
+/
+
+PROMPT Create USER_SDO_THEMES trigger
+
+BEGIN
+--
+  EXECUTE IMMEDIATE
+    'CREATE OR REPLACE TRIGGER '||hig.get_application_owner||'.USER_SDO_THEMES_INS_TRG '||chr(10)||
+    '-------------------------------------------------------------------------'||chr(10)||
+    '--   PVCS Identifiers :-'||chr(10)||
+    '-- '||chr(10)||
+    '--       PVCS id          : $Header:   //vm_latest/archives/nm3/install/nm4050_nm4051_metadata_upg.sql-arc   3.3   Dec 05 2008 12:09:14   aedwards  $'||chr(10)||
+    '--       Module Name      : $Workfile:   nm4050_nm4051_metadata_upg.sql  $'||chr(10)||
+    '--       Date into PVCS   : $Date:   Dec 05 2008 12:09:14  $'||chr(10)||
+    '--       Date fetched Out : $Modtime:   Dec 05 2008 12:08:32  $'||chr(10)||
+    '--       Version          : $Revision:   3.3  $'||chr(10)||
+    '--       Based on SCCS version :'|| chr(10)||
+    '-------------------------------------------------------------------------'||chr(10)||
+    'INSTEAD OF DELETE OR INSERT OR UPDATE '||chr(10)||
+    'ON '||hig.get_application_owner||'.USER_SDO_THEMES '||chr(10)|| 
+    'REFERENCING NEW AS NEW OLD AS OLD '||chr(10)||
+    'FOR EACH ROW '||chr(10)||
+    'DECLARE '||chr(10)||
+    '   l_rec_ust user_sdo_themes%ROWTYPE; '||chr(10)||
+    'BEGIN '||chr(10)||
+    '-- '||chr(10)||
+    '  IF INSERTING '||chr(10)|| 
+    '  THEN '||chr(10)||
+    '  -- '||chr(10)||
+    '    l_rec_ust.name             := :NEW.name; '||chr(10)||
+    '    l_rec_ust.description      := :NEW.description; '||chr(10)||
+    '    l_rec_ust.base_table       := :NEW.base_table; '||chr(10)||
+    '    l_rec_ust.geometry_column  := :NEW.geometry_column; '||chr(10)||
+    '    l_rec_ust.styling_rules    := :NEW.styling_rules; '||chr(10)||
+    '  -- '||chr(10)||
+    '    nm3msv_sec.do_ust_ins ( pi_username => USER '||chr(10)||
+    '                          , pi_rec_ust  => l_rec_ust ); '||chr(10)||
+    '  -- '||chr(10)||
+    '  ELSIF DELETING '||chr(10)|| 
+    '  THEN '||chr(10)||
+    '  -- '||chr(10)||
+    '    l_rec_ust.name             := :OLD.name; '||chr(10)||
+    '    l_rec_ust.description      := :OLD.description; '||chr(10)||
+    '    l_rec_ust.base_table       := :OLD.base_table; '||chr(10)||
+    '    l_rec_ust.geometry_column  := :OLD.geometry_column; '||chr(10)||
+    '    l_rec_ust.styling_rules    := :OLD.styling_rules; '||chr(10)||
+    '  -- '||chr(10)||
+    '    nm3msv_sec.do_ust_del ( pi_username => USER '||chr(10)||
+    '                          , pi_rec_ust  => l_rec_ust ); '||chr(10)||
+    '  -- '||chr(10)||
+    '  ELSIF UPDATING '||chr(10)|| 
+    '  THEN '||chr(10)||
+    '  -- '||chr(10)||
+    '    l_rec_ust.name             := NVL(:NEW.name, :OLD.name); '||chr(10)||
+    '    l_rec_ust.description      := NVL (:NEW.description, :OLD.description); '||chr(10)||
+    '    l_rec_ust.base_table       := NVL(:NEW.base_table, :OLD.base_table); '||chr(10)||
+    '    l_rec_ust.geometry_column  := NVL(:NEW.geometry_column, :OLD.geometry_column); '||chr(10)||
+    '    l_rec_ust.styling_rules    := NVL (:NEW.styling_rules, :OLD.styling_rules); '||chr(10)||
+    '  -- '||chr(10)||
+    '    nm3msv_sec.do_ust_upd ( pi_username => USER '||chr(10)||
+    '                          , pi_rec_ust  => l_rec_ust ); '||chr(10)||
+    '  -- '||chr(10)||
+    '  END IF; '||chr(10)||
+    '-- '||chr(10)||
+    'END; ';
+END;
+/
+
 PROMPT Recompile dependent packages
 
 BEGIN
@@ -348,6 +573,7 @@ BEGIN
   EXECUTE IMMEDIATE 'ALTER PACKAGE NM3SDM COMPILE BODY';
 END;
 /
+
 
 
 ------------------------------------------------------------------
