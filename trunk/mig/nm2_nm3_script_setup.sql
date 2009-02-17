6224dd48d5f9547c249d22cb6c79092b547d15d8
Ian <Ian@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2009-02-17 09:44:38 +0000

    changes from clive
    git-svn-id: svn://127.0.0.1/Core@1752 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/mig/nm2_nm3_script_setup.sql
+++ trunk/mig/nm2_nm3_script_setup.sql
@@ -30,6 +30,11 @@ undefine v2_owner
 undefine spatial_table
 undefine spatial_column
 undefine spatial_id
+
+undefine tma_yn
+undefine open_only
+undefine ukp_only
+
 undefine p_part1
 undefine p_part2_step1
 undefine p_part2_step2
@@ -49,7 +54,7 @@ col p_part1       new_value p_part1       noprint
 col p_part2_step1 new_value p_part2_step1 noprint
 col p_part2_step2 new_value p_part2_step2 noprint
 col p_part2_step3 new_value p_part2_step3 noprint
-col p_part2_step4 new_value p_part2_step4 noprint
+col p_part2_step4 new_value p_part2_step4 noprint 
 col p_part2_step5 new_value p_part2_step5 noprint
 col p_part2_step6 new_value p_part2_step6 noprint
 col p_part2_step7 new_value p_part2_step7 noprint
@@ -67,6 +72,10 @@ accept v2_owner prompt  "Enter the v2 Application owner [HIGHWAYS] : " DEFAULT H
 accept spatial_table prompt "Enter the Spatial Table Name [HIG_NET_MAP] : " DEFAULT HIG_NET_MAP
 accept spatial_column prompt "Enter the Spatial Column Name [SHAPE] : " DEFAULT SHAPE
 accept spatial_id prompt "Enter the Spatial Road ID Column Name [RSE_HE_ID] : " DEFAULT RSE_HE_ID
+
+accept tma_yn prompt "Are you migrating into an existing TMA System (Y/N) [Y] : " DEFAULT Y
+accept open_only prompt "Do you wish to migrate open assets only (Y) or all assets(N) [N] : " DEFAULT N
+accept ukp_only prompt "Do you wish to migrate all assets (A), UKP only (U) or Non UKP assets only (N) [A] : " DEFAULT A
 
 --
 ---------------------------------------------------------------------------------------------------------------
@@ -89,6 +98,13 @@ select  'PROMPT ======================================================='||chr(10
       ||'PROMPT Done' ||chr(10)
       ||'PROMPT Check log file on database server in directory &dir' ||chr(10)||chr(10) p_part1	  	  
 from dual 
+where upper('&tma_yn')='N'
+union all
+select  'PROMPT ======================================================='||chr(10)
+      ||'PROMPT Do not run Part 1 when migrating into an existing system'||chr(10) 
+      ||'PROMPT ======================================================='||chr(10)	         
+from dual 
+where upper('&tma_yn')='Y'
 /
 --
 ---------------------------------------------------------------------------------------------------------------
@@ -212,6 +228,7 @@ FROM dual
 --
 ---------------------------------------------------------------------------------------------------------------
 --
+
 SELECT  'PROMPT =============================================================='||chr(10)
       ||'PROMPT Migration of Core Highways and Network Manager - Part 2 Step 8'||chr(10)
       ||'PROMPT =============================================================='||chr(10)	   
@@ -220,6 +237,8 @@ SELECT  'PROMPT =============================================================='|
       ||'BEGIN'||chr(10)
       ||'nm2_nm3_migration.migrate_network_and_inventory (pi_log_file_location => ''&dir'''||chr(10)
       ||'                                                ,pi_netw_inv_type     => '''||UPPER('&inv_type')||''''||CHR(10)
+      ||'                                                ,pi_ukp_only	       => '''||UPPER('&ukp_only')||''''||CHR(10)
+      ||'                                                ,pi_open_only	       => '''||UPPER('&open_only')||''''||CHR(10)
       ||'                                                ,pi_step	       => 8);'||chr(10)	
       ||'END;'||chr(10)||'/'||chr(10)||chr(10) 	  
       ||'PROMPT Done' ||chr(10)
