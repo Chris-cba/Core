15c4f307a38eb4bea06faeb4d9a426bfce47fc44
malexander <malexander@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2009-02-20 14:36:16 +0000

    Regenerated to add DDL from Ade: alter table hig_option_values modify hov_value VARCHAR2(1000)
    git-svn-id: svn://127.0.0.1/Core@1781 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/install/nm4051_nm4052_ddl_upg.sql
+++ trunk/install/nm4051_nm4052_ddl_upg.sql
@@ -8,11 +8,11 @@
 --
 --   PVCS Identifiers :-
 --
---       PVCS id          : $Header:   //vm_latest/archives/nm3/install/nm4051_nm4052_ddl_upg.sql-arc   3.1   Feb 10 2009 17:23:58   malexander  $
+--       PVCS id          : $Header:   //vm_latest/archives/nm3/install/nm4051_nm4052_ddl_upg.sql-arc   3.2   Feb 20 2009 14:36:16   malexander  $
 --       Module Name      : $Workfile:   nm4051_nm4052_ddl_upg.sql  $
---       Date into PVCS   : $Date:   Feb 10 2009 17:23:58  $
---       Date fetched Out : $Modtime:   Feb 10 2009 17:22:20  $
---       Version          : $Revision:   3.1  $
+--       Date into PVCS   : $Date:   Feb 20 2009 14:36:16  $
+--       Date fetched Out : $Modtime:   Feb 20 2009 14:35:08  $
+--       Version          : $Revision:   3.2  $
 --
 ------------------------------------------------------------------
 --    Copyright (c) exor corporation ltd, 2009
@@ -38,6 +38,24 @@ SET TERM OFF
 ALTER TABLE docs
 ADD (DOC_OUTCOME varchar2(10)
    , DOC_OUTCOME_REASON varchar2(254))
+/
+
+------------------------------------------------------------------
+
+
+------------------------------------------------------------------
+SET TERM ON
+PROMPT Increase HOV.HOV_VALUE column length
+SET TERM OFF
+
+------------------------------------------------------------------
+-- 
+-- DEVELOPMENT COMMENTS (ADRIAN EDWARDS)
+-- **** COMMENTS TO BE ADDED BY ADRIAN EDWARDS ****
+-- 
+------------------------------------------------------------------
+alter table hig_option_values
+modify hov_value VARCHAR2(1000)
 /
 
 ------------------------------------------------------------------
