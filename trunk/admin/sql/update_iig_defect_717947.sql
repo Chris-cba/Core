4d9c6739fc6035c07a83950b585211e650a3dbc5
lsorathia <lsorathia@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2009-02-24 14:43:18 +0000

    Added asset end date while closing groupings
    git-svn-id: svn://127.0.0.1/Core@1794 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/sql/update_iig_defect_717947.sql
+++ trunk/admin/sql/update_iig_defect_717947.sql
@@ -3,11 +3,11 @@
 --------------------------------------------------------------------------------
 --   PVCS Identifiers :-
 --
---       sccsid           : $Header:   //vm_latest/archives/nm3/admin/sql/update_iig_defect_717947.sql-arc   3.1   Feb 24 2009 14:40:20   lsorathia  $
+--       sccsid           : $Header:   //vm_latest/archives/nm3/admin/sql/update_iig_defect_717947.sql-arc   3.2   Feb 24 2009 14:43:18   lsorathia  $
 --       Module Name      : $Workfile:   update_iig_defect_717947.sql  $
---       Date into PVCS   : $Date:   Feb 24 2009 14:40:20  $
---       Date fetched Out : $Modtime:   Feb 24 2009 14:26:16  $
---       PVCS Version     : $Revision:   3.1  $
+--       Date into PVCS   : $Date:   Feb 24 2009 14:43:18  $
+--       Date fetched Out : $Modtime:   Feb 24 2009 14:39:36  $
+--       PVCS Version     : $Revision:   3.2  $
 --
 --------------------------------------------------------------------------------
 --
@@ -32,7 +32,8 @@ BEGIN
    --
    FORALL i IN 1..iit_tab.count
    UPDATE nm_inv_item_groupings iig
-   SET    iig.iig_end_date = Trunc(sysdate)
+   SET    iig.iig_end_date =Nvl((SELECT Nvl(iit_end_date,Trunc(sysdate)) FROM nm_inv_items_all 
+                                WHERE  iit_ne_id = iit_tab(i)),Trunc(sysdate))
    WHERE  iig.iig_item_id  = iit_tab(i) ;
    --
    Dbms_Output.Put_line('Count '||SQL%ROWCOUNT);   
