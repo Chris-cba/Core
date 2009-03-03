be932904a0080884a87afea9080d0bdac5151d2e
lsorathia <lsorathia@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2009-03-03 15:31:52 +0000

    Log-696122 Drop the trigger instead of disbale
    git-svn-id: svn://127.0.0.1/Core@1843 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/sql/disabled_trigger_on_nm_inv_items_all.sql
+++ trunk/admin/sql/disabled_trigger_on_nm_inv_items_all.sql
@@ -1,20 +1,20 @@
--- Disable Tigger NM_INV_ITEMS_ALL_A_DT_TRG as the same code is called in NM_INV_ITEMS_ALL_A_INS_UPD
+-- Drop Tigger NM_INV_ITEMS_ALL_A_DT_TRG as the same code is called in NM_INV_ITEMS_ALL_A_INS_UPD
 --
 --------------------------------------------------------------------------------
 --   PVCS Identifiers :-
 --
---       sccsid           : $Header:   //vm_latest/archives/nm3/admin/sql/disabled_trigger_on_nm_inv_items_all.sql-arc   3.1   Feb 24 2009 15:34:30   lsorathia  $
+--       sccsid           : $Header:   //vm_latest/archives/nm3/admin/sql/disabled_trigger_on_nm_inv_items_all.sql-arc   3.2   Mar 03 2009 15:31:52   lsorathia  $
 --       Module Name      : $Workfile:   disabled_trigger_on_nm_inv_items_all.sql  $
---       Date into PVCS   : $Date:   Feb 24 2009 15:34:30  $
---       Date fetched Out : $Modtime:   Feb 24 2009 15:29:12  $
---       PVCS Version     : $Revision:   3.1  $
+--       Date into PVCS   : $Date:   Mar 03 2009 15:31:52  $
+--       Date fetched Out : $Modtime:   Mar 03 2009 15:29:36  $
+--       PVCS Version     : $Revision:   3.2  $
 --
 --------------------------------------------------------------------------------
 --
 
-PROMPT Disable Tigger NM_INV_ITEMS_ALL_A_DT_TRG as the same code is called in NM_INV_ITEMS_ALL_A_INS_UPD
+PROMPT Drop Tigger NM_INV_ITEMS_ALL_A_DT_TRG as the same code is called in NM_INV_ITEMS_ALL_A_INS_UPD
 
-ALTER TRIGGER NM_INV_ITEMS_ALL_A_DT_TRG DISABLE 
+DROP TRIGGER NM_INV_ITEMS_ALL_A_DT_TRG 
 /
 
 
