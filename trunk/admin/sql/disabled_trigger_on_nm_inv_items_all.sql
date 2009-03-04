e02d71f6abc279e2de837a1cd416c59178e8bc98
lsorathia <lsorathia@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2009-03-04 09:03:18 +0000

    Log-696122 Used PL/SQL to drop the trigger
    git-svn-id: svn://127.0.0.1/Core@1849 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/sql/disabled_trigger_on_nm_inv_items_all.sql
+++ trunk/admin/sql/disabled_trigger_on_nm_inv_items_all.sql
@@ -3,19 +3,25 @@
 --------------------------------------------------------------------------------
 --   PVCS Identifiers :-
 --
---       sccsid           : $Header:   //vm_latest/archives/nm3/admin/sql/disabled_trigger_on_nm_inv_items_all.sql-arc   3.2   Mar 03 2009 15:31:52   lsorathia  $
+--       sccsid           : $Header:   //vm_latest/archives/nm3/admin/sql/disabled_trigger_on_nm_inv_items_all.sql-arc   3.3   Mar 04 2009 09:03:18   lsorathia  $
 --       Module Name      : $Workfile:   disabled_trigger_on_nm_inv_items_all.sql  $
---       Date into PVCS   : $Date:   Mar 03 2009 15:31:52  $
---       Date fetched Out : $Modtime:   Mar 03 2009 15:29:36  $
---       PVCS Version     : $Revision:   3.2  $
+--       Date into PVCS   : $Date:   Mar 04 2009 09:03:18  $
+--       Date fetched Out : $Modtime:   Mar 04 2009 09:02:28  $
+--       PVCS Version     : $Revision:   3.3  $
 --
 --------------------------------------------------------------------------------
 --
 
 PROMPT Drop Tigger NM_INV_ITEMS_ALL_A_DT_TRG as the same code is called in NM_INV_ITEMS_ALL_A_INS_UPD
 
-DROP TRIGGER NM_INV_ITEMS_ALL_A_DT_TRG 
+BEGIN
+   EXECUTE IMMEDIATE ('DROP TRIGGER NM_INV_ITEMS_ALL_A_DT_TRG');
+EXCEPTION
+  WHEN Others Then
+   Null;
+END; 
 /
+
 
 
 
