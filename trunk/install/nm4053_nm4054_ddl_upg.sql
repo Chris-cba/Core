301d68db9b79a2f82fbdfeb331693a1c6a369437
malexander <malexander@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2009-03-31 11:07:24 +0000

    Regenerated for build 2
    git-svn-id: svn://127.0.0.1/Core@2005 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/install/nm4053_nm4054_ddl_upg.sql
+++ trunk/install/nm4053_nm4054_ddl_upg.sql
@@ -8,11 +8,11 @@
 --
 --   PVCS Identifiers :-
 --
---       PVCS id          : $Header:   //vm_latest/archives/nm3/install/nm4053_nm4054_ddl_upg.sql-arc   3.0   Mar 23 2009 12:22:30   malexander  $
+--       PVCS id          : $Header:   //vm_latest/archives/nm3/install/nm4053_nm4054_ddl_upg.sql-arc   3.1   Mar 31 2009 12:07:24   malexander  $
 --       Module Name      : $Workfile:   nm4053_nm4054_ddl_upg.sql  $
---       Date into PVCS   : $Date:   Mar 23 2009 12:22:30  $
---       Date fetched Out : $Modtime:   Mar 23 2009 12:09:50  $
---       Version          : $Revision:   3.0  $
+--       Date into PVCS   : $Date:   Mar 31 2009 12:07:24  $
+--       Date fetched Out : $Modtime:   Mar 31 2009 12:05:04  $
+--       Version          : $Revision:   3.1  $
 --
 ------------------------------------------------------------------
 --	Copyright (c) exor corporation ltd, 2009
@@ -21,6 +21,62 @@ SET ECHO OFF
 SET LINESIZE 120
 SET HEADING OFF
 SET FEEDBACK OFF
+------------------------------------------------------------------
+
+
+------------------------------------------------------------------
+SET TERM ON
+PROMPT Removed the decimal place constraint
+SET TERM OFF
+
+------------------------------------------------------------------
+-- ASSOCIATED PROBLEM MANAGER LOG#
+-- 698292
+-- 
+-- CUSTOMER
+-- Kansas DOT
+-- 
+-- PROBLEM
+-- When running merge results extract from a result set that had either Meaning or Both selected for domain values and the merge query definition selected a number attribute, an invalid number error occurs.What the app is doing is populating the units definition for the attribute (such as metersinv) into the attribute value.  The error is being generated because a text value has been written to a number column.
+-- 
+------------------------------------------------------------------
+
+------------------------------------------------------------------
+-- 
+-- DEVELOPMENT COMMENTS (LINESH SORATHIA)
+-- Log-698292 Made chnages to working of Nm7055
+-- 
+------------------------------------------------------------------
+alter table NM_MRG_OUTPUT_COLS drop constraint NMC_DEC_PLACES_CHK ;
+
+------------------------------------------------------------------
+
+
+------------------------------------------------------------------
+SET TERM ON
+PROMPT Removed decimal place constraint
+SET TERM OFF
+
+------------------------------------------------------------------
+-- ASSOCIATED PROBLEM MANAGER LOG#
+-- 698292
+-- 
+-- CUSTOMER
+-- Kansas DOT
+-- 
+-- PROBLEM
+-- When running merge results extract from a result set that had either Meaning or Both selected for domain values and the merge query definition selected a number attribute, an invalid number error occurs.What the app is doing is populating the units definition for the attribute (such as metersinv) into the attribute value.  The error is being generated because a text value has been written to a number column.
+-- 
+------------------------------------------------------------------
+
+------------------------------------------------------------------
+-- 
+-- DEVELOPMENT COMMENTS (LINESH SORATHIA)
+-- Log-698292 Made chnages to working of Nm7055
+-- 
+------------------------------------------------------------------
+alter table NM_MRG_OUTPUT_COLS drop constraint NMC_DISP_DP_CHK2;
+
 ------------------------------------------------------------------
 
 
