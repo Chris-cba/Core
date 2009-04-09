d50d20adeee6dbfea869498a6af8f15d6cdc2ff0
smarshall <smarshall@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2009-04-09 13:09:26 +0000

    Changed prompt
    git-svn-id: svn://127.0.0.1/Core@2060 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/views/imf_hig_views.sql
+++ trunk/admin/views/imf_hig_views.sql
@@ -1,11 +1,11 @@
 -------------------------------------------------------------------------
 --   PVCS Identifiers :-
 --
---       PVCS id          : $Header:   //vm_latest/archives/nm3/admin/views/imf_hig_views.sql-arc   3.0   Apr 08 2009 16:08:00   smarshall  $
+--       PVCS id          : $Header:   //vm_latest/archives/nm3/admin/views/imf_hig_views.sql-arc   3.1   Apr 09 2009 14:09:26   smarshall  $
 --       Module Name      : $Workfile:   imf_hig_views.sql  $
---       Date into PVCS   : $Date:   Apr 08 2009 16:08:00  $
---       Date fetched Out : $Modtime:   Apr 08 2009 16:07:40  $
---       Version          : $Revision:   3.0  $
+--       Date into PVCS   : $Date:   Apr 09 2009 14:09:26  $
+--       Date fetched Out : $Modtime:   Apr 09 2009 14:09:16  $
+--       Version          : $Revision:   3.1  $
 -------------------------------------------------------------------------
 --
 SET echo OFF
@@ -16,7 +16,7 @@ COL run_file new_value run_file noprint
 -------------------------------------------------------------------------
 --
 SET TERM ON
-PROMPT imf_net_network_all
+PROMPT imf_hig_financial_years
 SET TERM OFF
 SET DEFINE ON
 SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||'&terminator'||'views'||'&terminator'||'imf_hig_financial_years.vw' run_file
