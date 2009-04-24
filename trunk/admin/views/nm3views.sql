08c0963653998d4216f10b1235a67f4f2389db8a
lsorathia <lsorathia@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2009-04-24 13:33:20 +0000

    Log 719763 New view created for execlusion of Reserve Words
    git-svn-id: svn://127.0.0.1/Core@2114 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/views/nm3views.sql
+++ trunk/admin/views/nm3views.sql
@@ -968,3 +968,14 @@ start '&run_file'
 --
 ----------------------------------------------------------------------------------------- 
 --
+SET TERM ON 
+PROMPT nm_reserve_words_vw.vw
+SET TERM OFF
+SET DEFINE ON 
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||'&terminator'||'views'||'&terminator'||'nm_reserve_words_vw.vw' run_file 
+FROM dual 
+/ 
+start '&run_file' 
+--
+----------------------------------------------------------------------------------------- 
+--
