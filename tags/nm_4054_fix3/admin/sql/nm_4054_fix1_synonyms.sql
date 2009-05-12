37c9f3dbf344273d3e0bd73fe1beedb15997deb4
bodriscoll <bodriscoll@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2009-05-12 13:36:14 +0000

    svnimporter: adding tags to revision 2158
    git-svn-id: svn://127.0.0.1/Core@2159 17c802ae-cbca-534c-9e8e-6d19a4926c45

---
+++ tags/nm_4054_fix3/admin/sql/nm_4054_fix1_synonyms.sql
@@ -0,0 +1,6 @@
+BEGIN
+  nm3ddl.create_synonym_for_object('HIG_USER_CONTACTS_ALL');
+  nm3ddl.create_synonym_for_object('HIG_USER_DETAILS_VW');
+  nm3ddl.create_synonym_for_object('HIG_USERS_UTILITY');
+END;
+/
