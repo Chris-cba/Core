682a4939cdae0f8d5883295a4d7540f13e510d19
bodriscoll <bodriscoll@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2009-05-12 13:00:26 +0000

    Added synonyms for objects HIG_USER_CONTACTS_ALL, HIG_USER_DETAILS_VW and HIG_USERS_UTILITY.
    git-svn-id: svn://127.0.0.1/Core@2155 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/sql/nm_4054_fix1_ddl.sql
+++ trunk/admin/sql/nm_4054_fix1_ddl.sql
@@ -91,4 +91,11 @@ BEGIN
 --
 END hig_user_contacts_all_who;
 /
+BEGIN
+  nm3ddl.create_synonym_for_object('HIG_USER_CONTACTS_ALL');
+  nm3ddl.create_synonym_for_object('HIG_USER_DETAILS_VW');
+  nm3ddl.create_synonym_for_object('HIG_USERS_UTILITY');
+END;
+/
+
 
