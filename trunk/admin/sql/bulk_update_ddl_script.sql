533a899ca0ae2da7e232ee4bebff80fef5d6d254
lsorathia <lsorathia@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2009-06-04 15:17:18 +0000

    Made changes to script
    git-svn-id: svn://127.0.0.1/Core@2223 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/sql/bulk_update_ddl_script.sql
+++ trunk/admin/sql/bulk_update_ddl_script.sql
@@ -22,11 +22,11 @@ SELECT
 -------------------------------------------------------------------------
 --   PVCS Identifiers :-
 --
---       PVCS id          : $Header:   //vm_latest/archives/nm3/admin/sql/bulk_update_ddl_script.sql-arc   3.0   Jun 01 2009 09:55:18   lsorathia  $
+--       PVCS id          : $Header:   //vm_latest/archives/nm3/admin/sql/bulk_update_ddl_script.sql-arc   3.1   Jun 04 2009 16:17:18   lsorathia  $
 --       Module Name      : $Workfile:   bulk_update_ddl_script.sql  $
---       Date into PVCS   : $Date:   Jun 01 2009 09:55:18  $
---       Date fetched Out : $Modtime:   May 28 2009 11:51:46  $
---       Version          : $Revision:   3.0  $
+--       Date into PVCS   : $Date:   Jun 04 2009 16:17:18  $
+--       Date fetched Out : $Modtime:   Jun 04 2009 16:14:38  $
+--       Version          : $Revision:   3.1  $
 --       Based on SCCS version : 
 -------------------------------------------------------------------------
 --
@@ -45,81 +45,94 @@ WHERE  ne.ne_admin_unit = nau.nau_admin_unit
 /
 
 
-CREATE OR REPLACE FORCE VIEW nm_attrib_view_vw (
-nav_disp_ord,
-nav_nt_type ,
-nav_inv_type,
-nav_col_name,
-nav_col_type ,
-nav_col_length ,
-nav_col_mandatory ,
-nav_col_domain,
-nav_col_updatable,
-nav_col_prompt,
-nav_col_format,
-nav_col_seq_no,
-nav_gty_type ,
-nav_parent_type_inc,
-nav_child_type_inc,
-nav_child_col,
-nav_parent_col
-)
-AS
-SELECT
+
+DECLARE
 --
+   l_sql VARCHAR2(4000);
 --
--------------------------------------------------------------------------
---   PVCS Identifiers :-
+BEGIN
 --
---       PVCS id          : $Header:   //vm_latest/archives/nm3/admin/sql/bulk_update_ddl_script.sql-arc   3.0   Jun 01 2009 09:55:18   lsorathia  $
---       Module Name      : $Workfile:   bulk_update_ddl_script.sql  $
---       Date into PVCS   : $Date:   Jun 01 2009 09:55:18  $
---       Date fetched Out : $Modtime:   May 28 2009 11:51:46  $
---       Version          : $Revision:   3.0  $
---       Based on SCCS version : 
--------------------------------------------------------------------------
--- 
-1 disp_ord,
-       ntc_nt_type ,
-       Null ,
-       ntc_column_name ,
-       ntc_column_type ,
-       ntc_str_length ,
-       ntc_mandatory ,
-       ntc_domain,
-       nm3_bulk_attrib_upd.check_col_upd(ntc_column_name,ntc_nt_type),
-       ntc_prompt,
-       ntc_format,
-       ntc_seq_no,
-       Null ,
-       nm3_bulk_attrib_upd.parent_inclusion_type(ntc_column_name,ntc_nt_type) parent_type_inc,
-       nm3_bulk_attrib_upd.child_inclusion_type(ntc_column_name,ntc_nt_type)  child_type_inc,
-       (SELECT nti_child_column FROM nm_type_inclusion WHERE  nti_nw_parent_type = ntc_nt_type 
-                                                     ) child_col,
-       (SELECT nti_parent_column FROM nm_type_inclusion WHERE  nti_nw_child_type = ntc_nt_type AND nti_child_column = ntc_column_name 
-                                                     ) parent_col
-FROM   nm_type_columns ntc
-union
-select 2 disp_ord,
-       ita_inv_type,
-       nad_nt_type ,
-       ita_attrib_name,
-       ita_format,
-       ita_fld_length,
-       ita_mandatory_yn,
-       ita_id_domain,'Y' ,
-       ita_scrn_text,
-       ita_format,
-       ita_disp_seq_no ,
-       nad_gty_type   ,
-       Null ,
-       Null,
-       Null,
-       Null 
-FROM   nm_inv_type_attribs ita,nm_nw_ad_types nad
-WHERE  ita.ita_inv_type =  nad.nad_inv_type
-AND    nad_primary_ad   = 'Y'
+   l_sql := 'CREATE OR REPLACE FORCE VIEW nm_attrib_view_vw ( '||
+            ' nav_disp_ord, '||
+            ' nav_nt_type , '|| 
+            ' nav_inv_type, '||
+            ' nav_col_name, '||
+            ' nav_col_type , '||
+            ' nav_col_length , '||
+            ' nav_col_mandatory , '||
+            ' nav_col_domain, '||
+            ' nav_col_updatable, '||
+            ' nav_col_prompt, '||
+            ' nav_col_format, '||
+            ' nav_col_seq_no, '||
+            ' nav_gty_type , '||
+            ' nav_parent_type_inc, '||
+            ' nav_child_type_inc, '||
+            ' nav_child_col, '||
+            ' nav_parent_col '||
+            ' ) '||
+            ' AS '||
+            ' SELECT '||
+            ' -- '||Chr(10)||
+            ' -- '||Chr(10)||
+            ' ------------------------------------------------------------------------- '||Chr(10)||
+            ' --   PVCS Identifiers :- '||Chr(10)||
+            ' -- '||Chr(10)||
+            ' --       PVCS id          : $Header:   //vm_latest/archives/nm3/admin/sql/bulk_update_ddl_script.sql-arc   3.1   Jun 04 2009 16:17:18   lsorathia  $ '||Chr(10)||
+            ' --       Module Name      : $Workfile:   bulk_update_ddl_script.sql  $ '||Chr(10)||
+            ' --       Date into PVCS   : $Date:   Jun 04 2009 16:17:18  $ '||Chr(10)||
+            ' --       Date fetched Out : $Modtime:   Jun 04 2009 16:14:38  $ '||Chr(10)||
+            ' --       Version          : $Revision:   3.1  $ '||Chr(10)||
+            ' --       Based on SCCS version :  '||Chr(10)||
+            ' ------------------------------------------------------------------------- '||Chr(10)||
+            ' --  '||Chr(10)||
+            '        1 disp_ord, '||
+            '        ntc_nt_type , '||
+            '        Null , '||
+            '        ntc_column_name , '||
+            '        ntc_column_type , '||
+            '        ntc_str_length , '||
+            '        ntc_mandatory , '||
+            '        ntc_domain, '||
+            '        nm3_bulk_attrib_upd.check_col_upd(ntc_column_name,ntc_nt_type), '||
+            '        ntc_prompt, '||
+            '        ntc_format, '||
+            '        ntc_seq_no, '||
+            '        Null , '||
+            '        nm3_bulk_attrib_upd.parent_inclusion_type(ntc_column_name,ntc_nt_type) parent_type_inc, '||
+            '        nm3_bulk_attrib_upd.child_inclusion_type(ntc_column_name,ntc_nt_type)  child_type_inc, '||
+            '        (SELECT nti_child_column FROM nm_type_inclusion WHERE  nti_nw_parent_type = ntc_nt_type  '||
+            '                                                      ) child_col, '||
+            '        (SELECT nti_parent_column FROM nm_type_inclusion WHERE  nti_nw_child_type = ntc_nt_type AND nti_child_column = ntc_column_name  '||
+            '                                                      ) parent_col '||
+            ' FROM   nm_type_columns ntc '||
+            ' union '||
+            ' select 2 disp_ord, '||
+            '        ita_inv_type, '||
+            '        nad_nt_type , '||
+            '        ita_attrib_name, '||
+            '        ita_format, '||
+            '        ita_fld_length, '||
+            '        ita_mandatory_yn, '||
+            '        ita_id_domain,''Y'' , '||
+            '        ita_scrn_text, '||
+            '        ita_format, '||
+            '        ita_disp_seq_no  , '||
+            '        nad_gty_type   , '||
+            '        Null , '||
+            '        Null, '||
+            '        Null, '||
+            '        Null  '||
+            ' FROM   nm_inv_type_attribs ita,nm_nw_ad_types nad '||
+            ' WHERE  ita.ita_inv_type =  nad.nad_inv_type '||
+            ' AND    nad_primary_ad   = ''Y'' ' ;
+   Execute Immediate l_sql;
+EXCEPTION
+   When Others THEN
+   NUll;
+END ;
 /
+
 
 INSERT INTO nm_errors
 SELECT 'NET'
