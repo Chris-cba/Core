9953fcaddb4f63bbaf98231078e8ac45caec85dd
aedwards <aedwards@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2009-07-13 15:54:56 +0000

    Added nm3gaz_query_saved
    git-svn-id: svn://127.0.0.1/Core@2347 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/pck/nm3pkb.sql
+++ trunk/admin/pck/nm3pkb.sql
@@ -2,11 +2,11 @@
 --
 --   PVCS Identifiers :-
 --
---       pvcsid           : $Header:   //vm_latest/archives/nm3/admin/pck/nm3pkb.sql-arc   2.13   Jul 13 2009 09:27:50   lsorathia  $
+--       pvcsid           : $Header:   //vm_latest/archives/nm3/admin/pck/nm3pkb.sql-arc   2.14   Jul 13 2009 16:54:56   aedwards  $
 --       Module Name      : $Workfile:   nm3pkb.sql  $
---       Date into PVCS   : $Date:   Jul 13 2009 09:27:50  $
---       Date fetched Out : $Modtime:   Jul 13 2009 09:26:32  $
---       PVCS Version     : $Revision:   2.13  $
+--       Date into PVCS   : $Date:   Jul 13 2009 16:54:56  $
+--       Date fetched Out : $Modtime:   Jul 13 2009 16:54:36  $
+--       PVCS Version     : $Revision:   2.14  $
 --
 --
 --   Author : Graeme Johnson
@@ -2034,10 +2034,19 @@ FROM dual
 / 
 start '&run_file'
 --
+----------------------------------------------------------------------------------------- 
+--
+SET TERM ON 
+PROMPT nm3gaz_query_saved.pkw
+SET TERM OFF
+SET DEFINE ON 
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||'&terminator'||'pck'||'&terminator'||'nm3gaz_query_saved.pkw' run_file
+FROM dual 
+/ 
+start '&run_file'
+--
 --
 --
 -- New PACKAGE BODIES above here
 --
 SET term on
-
-nm3_bulk_attrib_upd.pkb
