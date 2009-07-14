6c281a3002d1a075fa1b6966f5ed799c10e287fe
lsorathia <lsorathia@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2009-07-14 10:10:08 +0000

    Made changes to the script
    git-svn-id: svn://127.0.0.1/Core@2354 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/install/exor_core_user_creation.sql
+++ trunk/install/exor_core_user_creation.sql
@@ -1,17 +1,31 @@
+undefine p_deftab
+
+ACCEPT p_deftab   char prompt 'ENTER THE HIGHWAYS OWNERS DEFAULT TABLESPACE : '
+
+SET verify OFF;
+SET feedback OFF;
+SET serveroutput ON
+
 -------------------------------------------------------------------------
 --   PVCS Identifiers :-
 --
---       PVCS id          : $Header:   //vm_latest/archives/nm3/install/exor_core_user_creation.sql-arc   3.0   Jul 14 2009 09:16:12   lsorathia  $
+--       PVCS id          : $Header:   //vm_latest/archives/nm3/install/exor_core_user_creation.sql-arc   3.1   Jul 14 2009 11:10:08   lsorathia  $
 --       Module Name      : $Workfile:   exor_core_user_creation.sql  $
---       Date into PVCS   : $Date:   Jul 14 2009 09:16:12  $
---       Date fetched Out : $Modtime:   Jul 14 2009 09:14:38  $
---       Version          : $Revision:   3.0  $
+--       Date into PVCS   : $Date:   Jul 14 2009 11:10:08  $
+--       Date fetched Out : $Modtime:   Jul 14 2009 11:08:32  $
+--       Version          : $Revision:   3.1  $
 --       Based on SCCS version : 1.1
 --       This scripts creates new user EXOR_CORE 
 -------------------------------------------------------------------------
+DECLARE
+--
+   l_default_tablespace Varchar2(100) := UPPER('&P_DEFTAB');
+   l_invalid_tablespace Exception;
+   pragma exception_init(l_invalid_tablespace,-00959);   
+-- 
 BEGIN
 --
-   EXECUTE IMMEDIATE 'CREATE USER exor_core IDENTIFIED BY exor_core' ;
+   EXECUTE IMMEDIATE 'CREATE USER exor_core IDENTIFIED BY exor_core Default Tablespace '||l_default_tablespace ;
 
    EXECUTE IMMEDIATE 'GRANT CONNECT TO exor_core' ;
 
@@ -20,9 +34,16 @@ BEGIN
    EXECUTE IMMEDIATE 'GRANT CREATE ANY CONTEXT TO exor_core' ;   
 
    EXECUTE IMMEDIATE 'GRANT Create Public Synonym TO exor_core' ; 
+   
+   Dbms_Output.Put_Line('User Exor_CORE created successfully');
 --
 EXCEPTION
-   WHEN OTHERS THEN
-   Null ;
+   WHEN l_invalid_tablespace 
+   THEN
+       Raise_Application_Error(-20001,'Error while creating user EXOR_CORE : Invalid Tablesapce Name '||Chr(10)||
+                                      'Please run the script again with the valid Tablesapce Name');
+   When Others 
+   THEN
+       Null; 
 END ;
 /
