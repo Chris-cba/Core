b7de00284efac4cd1a0f483bad02913a882e2227
aedwards <aedwards@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2009-07-16 09:24:06 +0000

    Rerunable
    git-svn-id: svn://127.0.0.1/Core@2383 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/install/nm3jobs.sql
+++ trunk/install/nm3jobs.sql
@@ -2,15 +2,40 @@
 --------------------------------------------------------------------------------
 --   PVCS Identifiers :-
 --
---       sccsid           : $Header:   //vm_latest/archives/nm3/install/nm3jobs.sql-arc   3.0   Jul 14 2009 09:22:36   aedwards  $
+--       sccsid           : $Header:   //vm_latest/archives/nm3/install/nm3jobs.sql-arc   3.1   Jul 16 2009 10:24:06   aedwards  $
 --       Module Name      : $Workfile:   nm3jobs.sql  $
---       Date into PVCS   : $Date:   Jul 14 2009 09:22:36  $
---       Date fetched Out : $Modtime:   Jul 14 2009 09:20:48  $
---       PVCS Version     : $Revision:   3.0  $
+--       Date into PVCS   : $Date:   Jul 16 2009 10:24:06  $
+--       Date fetched Out : $Modtime:   Jul 16 2009 10:23:50  $
+--       PVCS Version     : $Revision:   3.1  $
 --
 --------------------------------------------------------------------------------
 --
+DECLARE
+  ex_no_exists       EXCEPTION;
+  PRAGMA             EXCEPTION_INIT (ex_no_exists,-27475);
 BEGIN
+--
+  BEGIN
+    nm3jobs.drop_job ( pi_job_name => 'CLEAROUT_GDO_DATA'
+                     , pi_force    => TRUE );
+  EXCEPTION
+    WHEN ex_no_exists THEN NULL;
+  END;
+--
+  BEGIN
+    nm3jobs.drop_job ( pi_job_name => 'CLEAROUT_NGQI_DATA'
+                     , pi_force    => TRUE );
+  EXCEPTION
+    WHEN ex_no_exists THEN NULL;
+  END;
+--
+  BEGIN
+    nm3jobs.drop_job ( pi_job_name => 'CLEAROUT_ND_DATA'
+                     , pi_force    => TRUE );
+  EXCEPTION
+    WHEN ex_no_exists THEN NULL;
+  END;
+--
   nm3jobs.create_job
               ( pi_job_name   => 'CLEAROUT_GDO_DATA'
               , pi_job_action => 'BEGIN nm3data.cleardown_gdo; END;'
