596f9a39722bbd7e51cc75d887b1eaf0fa3c72fe
cstrettle <cstrettle@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2009-11-12 12:13:44 +0000

    task 0108716 change made to include the nm_sql_context.sql to include the nm_sql context which caused this issue. I have also added PVCS tags.
    git-svn-id: svn://127.0.0.1/Core@2790 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/ctx/nm3ctx.sql
+++ trunk/admin/ctx/nm3ctx.sql
@@ -1,6 +1,16 @@
 -------------------------------------------------------------------------
+--------------------------------------------------------------------
+--   PVCS Identifiers :-
+--
+--       PVCS id          : $Header:   //vm_latest/archives/nm3/admin/ctx/nm3ctx.sql-arc   2.1   Nov 12 2009 12:13:44   cstrettle  $
+--       Module Name      : $Workfile:   nm3ctx.sql  $
+--       Date into PVCS   : $Date:   Nov 12 2009 12:13:44  $
+--       Date fetched Out : $Modtime:   Nov 12 2009 12:07:10  $
+--       Version          : $Revision:   2.1  $
+--       Based on SCCS version : 2.0
+--------------------------------------------------------------------
 -- SCCS ID -- Do NOT remove -----
-define sccsid = '$Revision:   2.0  $'
+define sccsid = '$Revision:   2.1  $'
 -------------------------------------------------------------------------
 --	Copyright (c) 1997 exor corporation.
 --
@@ -36,6 +46,13 @@ start '&run_file'
 set define on
 select '&exor_base'||'nm3'||'&terminator'||'admin'||
          '&terminator'||'ctx'||'&terminator'||'invsec.pkw' run_file
+from dual
+/
+start '&run_file'
+--
+set define on
+select '&exor_base'||'nm3'||'&terminator'||'admin'||
+         '&terminator'||'ctx'||'&terminator'||'nm_sql_context.sql' run_file
 from dual
 /
 start '&run_file'
