75835970d29e15372649561e9f2384d63927e8b8
malexander <malexander@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2010-01-12 12:31:44 +0000

    svnimporter: adding tags to revision 3082
    git-svn-id: svn://127.0.0.1/Core@3083 17c802ae-cbca-534c-9e8e-6d19a4926c45

---
+++ tags/v4.2.0.0/install/nm410x_nm4200_upg.sql
@@ -0,0 +1,430 @@
+--
+-----------------------------------------------------------------------------
+--
+--   PVCS Identifiers :-
+--
+--       PVCS id          : $Header:   //vm_latest/archives/nm3/install/nm410x_nm4200_upg.sql-arc   3.1   Jan 12 2010 12:31:44   malexander  $
+--       Module Name      : $Workfile:   nm410x_nm4200_upg.sql  $
+--       Date into PVCS   : $Date:   Jan 12 2010 12:31:44  $
+--       Date fetched Out : $Modtime:   Jan 12 2010 12:31:26  $
+--       Version          : $Revision:   3.1  $
+--
+--   Product upgrade script
+--
+-----------------------------------------------------------------------------
+--	Copyright (c) exor corporation ltd, 2009
+-----------------------------------------------------------------------------
+SET ECHO OFF
+SET LINESIZE 120
+SET HEADING OFF
+SET FEEDBACK OFF
+-- Grab date/time to append to log file names this is standard to all upgrade/install scripts
+undefine log_extension
+undefine four_one_to_4200
+undefine four_101_to_4200
+undefine log_extension
+undefine logfile1
+undefine logfile2
+col      four_one_to_4200  new_value four_one_to_4200 noprint
+col      four_101_to_4200  new_value four_101_to_4200 noprint	
+col      log_extension     new_value log_extension    noprint
+col      logfile1          new_value logfile1         noprint
+col      logfile2          new_value logfile2         noprint
+--       
+-------------------------------------------------------------------------
+--
+set term off
+
+SELECT 'Y' four_one_to_4200
+FROM hig_products WHERE hpr_product IN ('HIG','NET','DOC','AST','WMP') AND hpr_version = '4.1.0.0'
+UNION
+SELECT 'N' four_one_to_4200
+FROM hig_products WHERE hpr_product IN ('HIG','NET','DOC','AST','WMP') AND hpr_version != '4.1.0.0'
+/
+SELECT 'Y' four_101_to_4200
+FROM hig_products WHERE hpr_product IN ('HIG','NET','DOC','AST','WMP') AND hpr_version IN ('4.1.0.0','4.1.0.1')
+UNION
+SELECT 'N' four_101_to_4200
+FROM hig_products WHERE hpr_product IN ('HIG','NET','DOC','AST','WMP') AND hpr_version NOT IN ('4.1.0.0','4.1.0.1')
+/
+select  TO_CHAR(sysdate,'DDMONYYYY_HH24MISS')||'.LOG' log_extension from dual
+/
+SELECT 'nm'||replace(hpr_version,'.',Null)||'_nm4200_1_&log_extension' logfile1
+FROM hig_products WHERE hpr_product = 'NET' 
+/
+SELECT 'nm'||replace(hpr_version,'.',Null)||'_nm4200_2_&log_extension' logfile2
+FROM hig_products WHERE hpr_product = 'NET' 
+/
+
+set term on
+---------------------------------------------------------------------------------------------------
+--
+spool &logfile1
+--get some db info
+SELECT 'Install Running on ' ||LOWER(USER||'@'||instance_name||'.'||host_name)||' - DB ver : '||version
+FROM v$instance;
+SELECT 'Current version of '||hpr_product||' ' ||hpr_version
+FROM hig_products
+WHERE hpr_product IN ('HIG','NET','DOC','AST','WMP');
+---------------------------------------------------------------------------------------------------
+--                        ****************   CHECK(S)   *******************
+WHENEVER SQLERROR EXIT
+begin
+   hig2.pre_upgrade_check (p_product               => 'HIG'
+                          ,p_new_version           => '4.2.0.0'
+                          ,p_allowed_old_version_1 => '4.1.0.0'
+                          ,p_allowed_old_version_2 => '4.1.0.1'
+                          );
+END;
+/
+WHENEVER SQLERROR CONTINUE
+--
+---------------------------------------------------------------------------------------------------
+--                        **************** DROP POLICIES *******************
+--
+-- drop any policies that could be effected by table changes
+-- ensure that later on - after a compile schema these policies are re-created
+SET TERM ON
+PROMPT Dropping Policies...
+SET TERM OFF
+SET DEFINE ON
+SET VERIFY OFF
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||
+        '&terminator'||'ctx'||'&terminator'||'drop_policy' run_file
+FROM dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+---------------------------------------------------------------------------------------------------
+--                        **************** TYPES   ****************
+--
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||
+        '&terminator'||'typ'||'&terminator'||'nm3typ.sql' run_file
+FROM dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+---------------------------------------------------------------------------------------------------
+--                        ****************   DDL   *******************
+SET TERM ON
+PROMPT 4.1.0.0 to 4.1.0.1 DDL Changes (if required)...
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'nm3'||'&terminator'||'install'||
+        '&terminator'||'nm4100_nm4101_ddl_upg.sql' run_file
+FROM dual
+WHERE '&four_one_to_4200' = 'Y'
+UNION
+select '&exor_base'||'nm3'||'&terminator'||'install'||
+        '&terminator'||'dummy' run_file
+FROM dual
+WHERE '&four_one_to_4200' = 'N'
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF  
+--
+----
+--
+SET TERM ON
+PROMPT 4.1.0.1 to 4.2.0.0 DDL Changes ...
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'nm3'||'&terminator'||'install'||
+        '&terminator'||'nm4101_nm4200_ddl_upg.sql' run_file
+FROM dual
+WHERE '&four_101_to_4200' = 'Y'
+UNION
+select '&exor_base'||'nm3'||'&terminator'||'install'||
+        '&terminator'||'dummy' run_file
+FROM dual
+WHERE '&four_101_to_4200' = 'N'
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+---------------------------------------------------------------------------------------------------
+--               ****************   RE-RUN INDEXES   *******************
+--
+-- not this time around
+--
+--
+---------------------------------------------------------------------------------------------------
+--               ****************   RE-RUN CONSTRAINTS   *******************
+--
+-- not this time around
+--
+---------------------------------------------------------------------------------------------------
+--                        **************** VIEWS   ****************
+SET TERM ON
+PROMPT Views...
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||
+        '&terminator'||'views'||'&terminator'||'nm3views.sql' run_file
+FROM dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'nm3'||'&terminator'||'install'||
+        '&terminator'||'higviews.sql' run_file
+FROM dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+---------------------------------------------------------------------------------------------------
+--                        **************** TRIGGERS   ****************
+--
+SET TERM ON
+PROMPT Triggers...
+SET TERM OFF
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||
+        '&terminator'||'trg'||'&terminator'||'nm3trg.sql' run_file
+FROM dual
+/
+SET FEEDBACK ON
+start '&&run_file'
+SET FEEDBACK OFF
+
+SET TERM ON
+PROMPT Who Triggers
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||'&terminator'||'trg'||
+       '&terminator'||'who_trg.sql' run_file
+FROM dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+---------------------------------------------------------------------------------------------------
+--                        **************** APPLICATION CONTEXTS ****************
+--   
+SET TERM ON
+Prompt Application Contexts...
+--SET TERM OFF
+SET DEFINE ON
+select '&exor_base'||'nm3'||'&terminator'||'admin'||
+        '&terminator'||'ctx'||'&terminator'||'nm3ctx.sql' run_file
+from dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+---------------------------------------------------------------------------------------------------
+--                  **************** PACKAGE HEADERS AND BODIES   ****************
+--
+SET TERM ON
+PROMPT Package Headers...
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||
+       '&terminator'||'pck'||'&terminator'||'nm3pkh.sql' run_file
+FROM dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT Package Bodies...
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||
+       '&terminator'||'pck'||'&terminator'||'nm3pkb.sql' run_file
+FROM dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+---------------------------------------------------------------------------------------------------
+--                        ****************   COMPILE SCHEMA   *******************
+SET TERM ON
+Prompt Creating Compiling Schema Script...
+SET TERM OFF
+SPOOL OFF
+SET define ON
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||
+        '&terminator'||'utl'||'&terminator'||'compile_schema.sql' run_file
+FROM dual
+/
+start '&run_file'
+spool &logfile2
+SET TERM ON
+start compile_all.sql
+--
+---------------------------------------------------------------------------------------------------
+--                        ****************   CONTEXT   *******************
+--The compile_all will have reset the user context so we must reinitialise it
+--
+SET FEEDBACK OFF
+SET TERM ON
+PROMPT Reinitialising Context...
+SET TERM OFF
+BEGIN
+  nm3context.initialise_context;
+  nm3user.instantiate_user;
+END;
+/
+---------------------------------------------------------------------------------------------------
+--                        **************** ADD POLICIES *******************
+-- re-create the policies that were dropped at the beginning of the upgrade
+--
+SET TERM ON
+PROMPT Adding Policies...
+SET TERM OFF
+SET DEFINE ON
+SET VERIFY OFF
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||
+    '&terminator'||'ctx'||'&terminator'||'add_policy' run_file
+FROM dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+---------------------------------------------------------------------------------------------------
+--                  ****************   METADATA  *******************
+SET TERM ON
+PROMPT Metadata Changes...
+
+
+
+
+SET TERM ON
+PROMPT 4.1.0.0 to 4.1.0.1 Metadata Changes (if required)...
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'nm3'||'&terminator'||'install'||
+        '&terminator'||'nm4100_nm4101_metadata_upg.sql' run_file
+FROM dual
+WHERE '&four_one_to_4200' = 'Y'
+UNION
+select '&exor_base'||'nm3'||'&terminator'||'install'||
+        '&terminator'||'dummy' run_file
+FROM dual
+WHERE '&four_one_to_4200' = 'N'
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+----
+--
+SET TERM ON
+PROMPT 4.1.0.1 to 4.2.0.0 Metadata Changes ...
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'nm3'||'&terminator'||'install'||
+        '&terminator'||'nm4101_nm4200_metadata_upg.sql' run_file
+FROM dual
+WHERE '&four_101_to_4200' = 'Y'
+UNION
+select '&exor_base'||'nm3'||'&terminator'||'install'||
+        '&terminator'||'dummy' run_file
+FROM dual
+WHERE '&four_101_to_4200' = 'N'
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF 
+--
+----
+--
+SET DEFINE ON
+SELECT '&exor_base'||'nm3'||'&terminator'||'install'||
+        '&terminator'||'nm3data_help.sql' run_file
+FROM dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+---------------------------------------------------------------------------------------------------
+--                        ****************   SYNONYMS   *******************
+SET TERM ON
+Prompt Creating Synonyms That Do Not Exist...
+SET TERM OFF
+EXECUTE nm3ddl.refresh_all_synonyms;
+--
+---------------------------------------------------------------------------------------------------
+--                  ****************   CREATE JOBS  *******************
+--
+--
+SET TERM ON
+PROMPT Create table maintenance Jobs
+SET TERM OFF
+SET DEFINE ON
+select '&exor_base'||'nm3'||'&terminator'||'install'||
+        '&terminator'||'nm3jobs.sql' run_file
+from dual
+/
+SET FEEDBACK ON
+start '&&run_file'
+SET FEEDBACK OFF
+--
+---------------------------------------------------------------------------------------------------
+--                        ****************   ROLES   *******************
+SET TERM ON
+Prompt Updating HIG_USER_ROLES...
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'nm3'||'&terminator'||'install'||
+    '&terminator'||'hig_user_roles.sql' run_file
+from dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+Prompt Ensuring all users have HIG_USER role...
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'nm3'||'&terminator'||'install'||
+    '&terminator'||'users_without_hig_user.sql' run_file
+from dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+---------------------------------------------------------------------------------------------------
+--                        ****************   VERSION NUMBER   *******************
+SET TERM ON
+Prompt Setting The Version Number...
+SET TERM OFF
+BEGIN
+      hig2.upgrade('HIG','nm410x_nm4200_upg.sql','Upgrade from 4.1.0.0/4.1.0.1 to 4.2.0.0','4.2.0.0');
+      hig2.upgrade('NET','nm410x_nm4200_upg.sql','Upgrade from 4.1.0.0/4.1.0.1 to 4.2.0.0','4.2.0.0');
+      hig2.upgrade('DOC','nm410x_nm4200_upg.sql','Upgrade from 4.1.0.0/4.1.0.1 to 4.2.0.0','4.2.0.0');
+      hig2.upgrade('AST','nm410x_nm4200_upg.sql','Upgrade from 4.1.0.0/4.1.0.1 to 4.2.0.0','4.2.0.0');
+      hig2.upgrade('WMP','nm410x_nm4200_upg.sql','Upgrade from 4.1.0.0/4.1.0.1 to 4.2.0.0','4.2.0.0');
+END;
+/
+COMMIT;
+SET HEADING OFF
+SELECT 'Product updated to version '||hpr_product||' ' ||hpr_version product_version
+FROM hig_products
+WHERE hpr_product IN ('HIG','NET','DOC','AST','WMP');
+spool off
+exit
+---------------------------------------------------------------------------------------------------
+--                        ****************   END OF SCRIPT   *******************
