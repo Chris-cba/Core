cd5d7a8835bdc6b706801ffe41e61a2129ac5426
aedwards <aedwards@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2010-01-12 10:24:00 +0000

    Task 0108915 - Added NM3FTP package
    git-svn-id: svn://127.0.0.1/Core@3070 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/pck/nm3pkb.sql
+++ trunk/admin/pck/nm3pkb.sql
@@ -2,11 +2,11 @@
 --
 --   PVCS Identifiers :-
 --
---       pvcsid           : $Header:   //vm_latest/archives/nm3/admin/pck/nm3pkb.sql-arc   2.16   Dec 04 2009 17:03:30   aedwards  $
+--       pvcsid           : $Header:   //vm_latest/archives/nm3/admin/pck/nm3pkb.sql-arc   2.17   Jan 12 2010 10:24:00   aedwards  $
 --       Module Name      : $Workfile:   nm3pkb.sql  $
---       Date into PVCS   : $Date:   Dec 04 2009 17:03:30  $
---       Date fetched Out : $Modtime:   Dec 04 2009 17:02:54  $
---       PVCS Version     : $Revision:   2.16  $
+--       Date into PVCS   : $Date:   Jan 12 2010 10:24:00  $
+--       Date fetched Out : $Modtime:   Jan 12 2010 10:23:12  $
+--       PVCS Version     : $Revision:   2.17  $
 --
 --
 --   Author : Graeme Johnson
@@ -2063,6 +2063,17 @@ PROMPT webutil_db.pkw
 SET TERM OFF
 SET DEFINE ON 
 SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||'&terminator'||'pck'||'&terminator'||'webutil_db.pkw' run_file
+FROM dual 
+/ 
+start '&run_file'
+--
+----------------------------------------------------------------------------------------- 
+--
+SET TERM ON 
+PROMPT nm3ftp.pkw
+SET TERM OFF
+SET DEFINE ON 
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||'&terminator'||'pck'||'&terminator'||'nm3ftp.pkw' run_file
 FROM dual 
 / 
 start '&run_file'
