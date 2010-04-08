15be327deeb35308b305d5e43635096bf398fe71
cstrettle <cstrettle@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2010-04-08 09:17:08 +0000

    task 0108359: v_doc_document.vw added
    git-svn-id: svn://127.0.0.1/Core@3510 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/views/nm3views.sql
+++ trunk/admin/views/nm3views.sql
@@ -1185,8 +1185,14 @@ start '&run_file'
 --
 ----------------------------------------------------------------------------------------- 
 --
-
-
-
-
-
+SET TERM ON 
+PROMPT v_doc_documents.vw
+SET TERM OFF
+SET DEFINE ON 
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||'&terminator'||'views'||'&terminator'||'v_doc_documents.vw' run_file 
+FROM dual 
+/ 
+start '&run_file' 
+--
+----------------------------------------------------------------------------------------- 
+--
