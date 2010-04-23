d36d84ebae380baee623fdb56bfeba3fc3623172
aedwards <aedwards@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2010-04-23 13:34:30 +0000

    Added doc_locations_api
    git-svn-id: svn://127.0.0.1/Core@3668 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/pck/nm3pkb.sql
+++ trunk/admin/pck/nm3pkb.sql
@@ -2,11 +2,11 @@
 --
 --   PVCS Identifiers :-
 --
---       pvcsid           : $Header:   //vm_latest/archives/nm3/admin/pck/nm3pkb.sql-arc   2.28   Apr 23 2010 13:21:48   cstrettle  $
+--       pvcsid           : $Header:   //vm_latest/archives/nm3/admin/pck/nm3pkb.sql-arc   2.29   Apr 23 2010 14:34:30   aedwards  $
 --       Module Name      : $Workfile:   nm3pkb.sql  $
---       Date into PVCS   : $Date:   Apr 23 2010 13:21:48  $
---       Date fetched Out : $Modtime:   Apr 23 2010 13:18:34  $
---       PVCS Version     : $Revision:   2.28  $
+--       Date into PVCS   : $Date:   Apr 23 2010 14:34:30  $
+--       Date fetched Out : $Modtime:   Apr 23 2010 14:34:06  $
+--       PVCS Version     : $Revision:   2.29  $
 --
 --
 --   Author : Graeme Johnson
@@ -2223,7 +2223,17 @@ start '&run_file'
 --
 ----------------------------------------------------------------------------------------- 
 --
-
+SET TERM ON 
+PROMPT doc_locations_api.pkw
+SET TERM OFF
+SET DEFINE ON 
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||'&terminator'||'pck'||'&terminator'||'doc_locations_api.pkw' run_file
+FROM dual 
+/ 
+start '&run_file'
+--
+----------------------------------------------------------------------------------------- 
+--
 
 --
 -- New PACKAGE BODIES above here
