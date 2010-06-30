628dbae4e2edf63baad7bdb51d2d199ed1175f1a
malexander <malexander@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2010-06-30 12:46:52 +0000

    Regenerated for 4210
    git-svn-id: svn://127.0.0.1/Core@4147 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/install/nm3data4.sql
+++ trunk/install/nm3data4.sql
@@ -2,13 +2,13 @@
 --
 --   PVCS Identifiers :-
 --
---       PVCS id          : $Header:   //vm_latest/archives/nm3/install/nm3data4.sql-arc   2.10   Jun 11 2010 16:15:40   malexander  $
+--       PVCS id          : $Header:   //vm_latest/archives/nm3/install/nm3data4.sql-arc   2.11   Jun 30 2010 13:46:52   malexander  $
 --       Module Name      : $Workfile:   nm3data4.sql  $
---       Date into PVCS   : $Date:   Jun 11 2010 16:15:40  $
---       Date fetched Out : $Modtime:   Jun 11 2010 16:13:40  $
---       Version          : $Revision:   2.10  $
+--       Date into PVCS   : $Date:   Jun 30 2010 13:46:52  $
+--       Date fetched Out : $Modtime:   Jun 30 2010 13:38:14  $
+--       Version          : $Revision:   2.11  $
 --       Table Owner      : NM3_METADATA
---       Generation Date  : 11-JUN-2010 16:13
+--       Generation Date  : 30-JUN-2010 13:38
 --
 --   Product metadata script
 --   As at Release 4.2.1.0
