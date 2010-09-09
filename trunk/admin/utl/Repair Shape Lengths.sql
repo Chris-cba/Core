c0eaa6cf31632e28df7dacb81a1ac62c32893e46
Ade.Edwards <Ade.Edwards@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2010-09-09 13:15:16 +0000

    Prevent any potential errors with checking the theme gtype in the select statement by putting it inside the loop
    git-svn-id: svn://127.0.0.1/Core@4404 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/utl/Repair Shape Lengths.sql
+++ trunk/admin/utl/Repair Shape Lengths.sql
@@ -29,39 +29,42 @@ BEGIN
                                 FROM v_nm_net_themes_all
                                WHERE vnnt_nth_theme_id = nth_theme_id
                                      AND vnnt_lr_type IN ('D')))
-           AND NM3SDO.GET_THEME_GTYPE (nth_theme_id) = 3302
   --
   )
   LOOP
-    dbms_output.put_line('================================================================================');
-    dbms_output.put_line ( '## Processing Theme ['||i.nth_theme_id||'] '||i.nth_feature_table||' - Started');
   --
-    l_sql := 
-      'DECLARE '||nl||
-      '  l_geom  MDSYS.SDO_GEOMETRY;'||nl||
-      '  l_total NUMBER := 0;'||nl||
-      'BEGIN '||nl||
-      '  FOR z IN ( SELECT a.'||i.nth_feature_pk_column||' pk_column, '||i.nth_feature_shape_column 
-                 ||', diminfo, ne_length FROM '||i.nth_feature_table||' a, nm_elements_all b , user_sdo_geom_metadata '||nl||
-                  '  WHERE a.'||i.nth_feature_pk_column||' = b.ne_id '||nl||
-                     ' AND table_name = '||nm3flx.string(i.nth_feature_table)||nl||
-                     ' AND column_name = '||nm3flx.string(i.nth_feature_shape_column)||nl||
-                     ' AND sdo_lrs.geom_segment_end_measure('||i.nth_feature_shape_column||') != ne_length )'||nl||
-      '  LOOP '||nl||
-         ' l_total := l_total+1;'||nl||
-         ' l_geom := z.'|| i.nth_feature_shape_column||';'||nl||
-         ' sdo_lrs.redefine_geom_segment (l_geom, z.diminfo, 0, z.ne_length );'||nl||
-      '    UPDATE '||i.nth_feature_table||' SET '||i.nth_feature_shape_column||' = l_geom'||nl||
-          ' WHERE '||i.nth_feature_pk_column||' = z.pk_column;'||nl||
-      '  END LOOP;'||nl||
-      '  dbms_output.put_line('||nm3flx.string('## ')||'|| l_total ||'||nm3flx.string(' rows updated')||'); '||nl||
-      'END;';
-  --
-    EXECUTE IMMEDIATE l_sql;
-    -- dbms_output.put_line ('$'||l_sql);
-  --
-    dbms_output.put_line ( '## Processing Theme ['||i.nth_theme_id||'] '||i.nth_feature_table||' - Finished'); 
-    dbms_output.put_line('================================================================================');
+    IF nm3sdo.get_theme_gtype (i.nth_theme_id) = 3302
+    THEN
+      dbms_output.put_line('================================================================================');
+      dbms_output.put_line ( '## Processing Theme ['||i.nth_theme_id||'] '||i.nth_feature_table||' - Started');
+    --
+      l_sql := 
+        'DECLARE '||nl||
+        '  l_geom  MDSYS.SDO_GEOMETRY;'||nl||
+        '  l_total NUMBER := 0;'||nl||
+        'BEGIN '||nl||
+        '  FOR z IN ( SELECT a.'||i.nth_feature_pk_column||' pk_column, '||i.nth_feature_shape_column 
+                   ||', diminfo, ne_length FROM '||i.nth_feature_table||' a, nm_elements_all b , user_sdo_geom_metadata '||nl||
+                    '  WHERE a.'||i.nth_feature_pk_column||' = b.ne_id '||nl||
+                       ' AND table_name = '||nm3flx.string(i.nth_feature_table)||nl||
+                       ' AND column_name = '||nm3flx.string(i.nth_feature_shape_column)||nl||
+                       ' AND sdo_lrs.geom_segment_end_measure('||i.nth_feature_shape_column||') != ne_length )'||nl||
+        '  LOOP '||nl||
+           ' l_total := l_total+1;'||nl||
+           ' l_geom := z.'|| i.nth_feature_shape_column||';'||nl||
+           ' sdo_lrs.redefine_geom_segment (l_geom, z.diminfo, 0, z.ne_length );'||nl||
+        '    UPDATE '||i.nth_feature_table||' SET '||i.nth_feature_shape_column||' = l_geom'||nl||
+            ' WHERE '||i.nth_feature_pk_column||' = z.pk_column;'||nl||
+        '  END LOOP;'||nl||
+        '  dbms_output.put_line('||nm3flx.string('## ')||'|| l_total ||'||nm3flx.string(' rows updated')||'); '||nl||
+        'END;';
+    --
+      EXECUTE IMMEDIATE l_sql;
+      -- dbms_output.put_line ('$'||l_sql);
+    --
+      dbms_output.put_line ( '## Processing Theme ['||i.nth_theme_id||'] '||i.nth_feature_table||' - Finished'); 
+      dbms_output.put_line('================================================================================');
+    END IF;
   END LOOP;
   dbms_output.put_line ( '# Correcting Element/Shape lengths complete - Please review and issue a COMMIT to finish');
 END;
