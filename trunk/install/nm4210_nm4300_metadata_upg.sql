2e0d2cecc86a6582d8ceb5630e22e25ae8e8fde1
mike.alexander <mike.alexander@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2010-10-06 12:58:32 +0000

    Regenerated for 4300
    git-svn-id: svn://127.0.0.1/Core@4509 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/install/nm4210_nm4300_metadata_upg.sql
+++ trunk/install/nm4210_nm4300_metadata_upg.sql
@@ -8,11 +8,11 @@
 --
 --   PVCS Identifiers :-
 --
---       PVCS id          : $Header:   //vm_latest/archives/nm3/install/nm4210_nm4300_metadata_upg.sql-arc   3.1   Sep 16 2010 17:42:00   Mike.Alexander  $
+--       PVCS id          : $Header:   //vm_latest/archives/nm3/install/nm4210_nm4300_metadata_upg.sql-arc   3.2   Oct 06 2010 13:58:32   mike.alexander  $
 --       Module Name      : $Workfile:   nm4210_nm4300_metadata_upg.sql  $
---       Date into PVCS   : $Date:   Sep 16 2010 17:42:00  $
---       Date fetched Out : $Modtime:   Sep 16 2010 17:40:52  $
---       Version          : $Revision:   3.1  $
+--       Date into PVCS   : $Date:   Oct 06 2010 13:58:32  $
+--       Date fetched Out : $Modtime:   Oct 06 2010 13:51:20  $
+--       Version          : $Revision:   3.2  $
 --
 ------------------------------------------------------------------
 --	Copyright (c) exor corporation ltd, 2010
@@ -141,7 +141,7 @@ SELECT 'EDITENDDAT'
 INSERT INTO hig_option_values ( hov_id
                               , hov_value)
 SELECT 'EDITENDDAT'
-     , 'Y'
+     , 'N'
   FROM dual
  WHERE NOT EXISTS (SELECT 'X' 
                      FROM hig_option_values
@@ -204,6 +204,47 @@ SELECT 'NET',465,NULL,'You cannot perform a network based query without at least
       AND ner_appl = 'NET')
 /
 
+INSERT INTO nm_errors
+   SELECT 'NET',
+          466,
+          NULL,
+          'Cannot find Document Gateway table or appropriate synonym.',
+          'Add the relevant table and/or synonym using the Document Gateway form (DOC0130)'
+     FROM DUAL
+    WHERE NOT EXISTS
+             (SELECT 1
+                FROM nm_errors
+               WHERE ner_appl = 'NET' AND ner_id = 466)
+/
+
+INSERT INTO NM_ERRORS ( NER_APPL
+                      , NER_ID
+                      , NER_DESCR)
+SELECT 'NET'
+     , 467
+     , 'The User has not been assigned the correct admin units to carry out this action.'
+     FROM DUAL
+     WHERE NOT EXISTS (SELECT 'X' 
+                       FROM NM_ERRORS
+                       WHERE NER_APPL = 'NET'
+                       AND NER_ID = 467)
+/
+
+INSERT INTO nm_errors ( ner_appl
+                      , ner_id
+                      , ner_descr
+                      , ner_cause)
+SELECT 'NET'
+      , 468
+      , 'Please ensure all datum networks are registered with 3D diminfo.'
+      , 'Subscript beyond count'
+  FROM dual
+ WHERE NOT EXISTS (SELECT 'X' 
+                     FROM nm_errors
+                    WHERE ner_appl = 'NET'
+                      AND ner_id = 468)
+/
+
 ------------------------------------------------------------------
 
 
@@ -228,27 +269,25 @@ UPDATE hig_navigator
 SET     hnv_hier_label_3 = 'hig_nav.concate_label(hig_nav.get_road_unique(wol_rse_he_id))'
 WHERE Upper(hnv_child_table) = 'WORK_ORDER_LINES';
 
-
-DELETE FROM hig_navigator_modules WHERE HNM_MODULE_NAME IN  ('NM0510','NM0590') ;
-
-INSERT INTO HIG_NAVIGATOR_MODULES
+Insert into HIG_NAVIGATOR_MODULES
    (HNM_MODULE_NAME, HNM_MODULE_PARAM, HNM_PRIMARY_MODULE, HNM_SEQUENCE, HNM_TABLE_NAME, 
     HNM_FIELD_NAME, HNM_HIERARCHY_LABEL, HNM_DATE_CREATED, HNM_CREATED_BY, HNM_DATE_MODIFIED, 
     HNM_MODIFIED_BY)
- VALUES
-   ('NM0510', 'query_inv_item', 'Y', 1, NULL, 
-    NULL, 'Asset', To_Date('02/22/2010 16:53:43', 'MM/DD/YYYY HH24:MI:SS'), 'DORSET', To_Date('02/22/2010 16:53:43', 'MM/DD/YYYY HH24:MI:SS'), 
-    'DORSET');
+Select 'NM0510', 'query_inv_item', 'Y', 1, NULL, 
+    NULL, 'Asset', TO_DATE('02/22/2010 16:53:43', 'MM/DD/YYYY HH24:MI:SS'), 'DORSET', TO_DATE('02/22/2010 16:53:43', 'MM/DD/YYYY HH24:MI:SS'), 
+    'DORSET'
+From Dual
+Where Not Exists (Select 1 From HIG_NAVIGATOR_MODULES Where HNM_MODULE_NAME = 'NM0510' And HNM_MODULE_PARAM = 'query_inv_item' And HNM_HIERARCHY_LABEL = 'Asset');
     
-INSERT INTO HIG_NAVIGATOR_MODULES
+Insert into HIG_NAVIGATOR_MODULES
    (HNM_MODULE_NAME, HNM_MODULE_PARAM, HNM_PRIMARY_MODULE, HNM_SEQUENCE, HNM_TABLE_NAME, 
     HNM_FIELD_NAME, HNM_HIERARCHY_LABEL, HNM_DATE_CREATED, HNM_CREATED_BY, HNM_DATE_MODIFIED, 
     HNM_MODIFIED_BY)
- VALUES
-   ('NM0590', 'query_inv_item', 'N', 2, NULL, 
-    NULL, 'Asset', To_Date('03/30/2010 17:43:05', 'MM/DD/YYYY HH24:MI:SS'), 'DORSET', To_Date('03/30/2010 17:43:05', 'MM/DD/YYYY HH24:MI:SS'), 
-    'DORSET');
-
+Select 'NM0590', 'query_inv_item', 'N', 2, NULL, 
+    NULL, 'Asset', TO_DATE('03/30/2010 17:43:05', 'MM/DD/YYYY HH24:MI:SS'), 'DORSET', TO_DATE('03/30/2010 17:43:05', 'MM/DD/YYYY HH24:MI:SS'), 
+    'DORSET'
+From Dual
+Where Not Exists (Select 1 From HIG_NAVIGATOR_MODULES Where HNM_MODULE_NAME = 'NM0590' And HNM_MODULE_PARAM = 'query_inv_item' And HNM_HIERARCHY_LABEL = 'Asset');
 
 ------------------------------------------------------------------
 
@@ -276,38 +315,6 @@ WHERE  ner_id       = 527
 AND      ner_appl   = 'HIG';
 
 
-------------------------------------------------------------------
-
-
-------------------------------------------------------------------
-SET TERM ON
-PROMPT New Error Message
-SET TERM OFF
-
-------------------------------------------------------------------
--- ASSOCIATED DEVELOPMENT TASK
--- 109750
--- 
--- TASK DETAILS
--- No details supplied
--- 
--- 
--- DEVELOPMENT COMMENTS (CHRIS STRETTLE)
--- New Error Message for doc0120 form.
--- 
-------------------------------------------------------------------
-INSERT INTO nm_errors
-   SELECT 'NET',
-          466,
-          NULL,
-          'Cannot find Document Gateway table or appropriate synonym.',
-          'Add the relevant table and/or synonym using the Document Gateway form (DOC0130)'
-     FROM DUAL
-    WHERE NOT EXISTS
-             (SELECT 1
-                FROM nm_errors
-               WHERE ner_appl = 'NET' AND ner_id = 466)
-/
 ------------------------------------------------------------------
 
 
