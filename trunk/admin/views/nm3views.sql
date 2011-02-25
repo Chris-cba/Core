771df7f440aaf8cfc74a2168b03cb58137e413be
Chris.Strettle <Chris.Strettle@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2011-02-25 11:46:20 +0000

    Task 0109403: hig_processes_all_v added.
    git-svn-id: svn://127.0.0.1/Core@5212 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/views/nm3views.sql
+++ trunk/admin/views/nm3views.sql
@@ -1076,6 +1076,17 @@ start '&run_file'
 ----------------------------------------------------------------------------------------- 
 --
 SET TERM ON 
+PROMPT hig_processes_all_v.vw
+SET TERM OFF
+SET DEFINE ON 
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||'&terminator'||'views'||'&terminator'||'hig_processes_all_v.vw' run_file 
+FROM dual 
+/ 
+start '&run_file' 
+--
+----------------------------------------------------------------------------------------- 
+--
+SET TERM ON 
 PROMPT hig_processes_v.vw
 SET TERM OFF
 SET DEFINE ON 
