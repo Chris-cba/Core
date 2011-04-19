c9a948ed7d016cfc87c12fec8f6aeb36290d6118
Mike.Alexander <Mike.Alexander@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2011-04-19 09:45:26 +0000

    svnimporter: adding tags to revision 5510
    git-svn-id: svn://127.0.0.1/Core@5511 17c802ae-cbca-534c-9e8e-6d19a4926c45

---
+++ tags/nm_4300_fix13/install/log_nm_4300_fix13.sql
@@ -0,0 +1,13 @@
+BEGIN
+--
+  hig2.upgrade(p_product        => 'NET'
+              ,p_upgrade_script => 'log_nm_4300_fix13.sql'
+              ,p_remarks        => 'NET 4300 FIX 13'
+              ,p_to_version     => Null);
+--
+  commit;
+--
+EXCEPTION
+  WHEN others THEN Null;
+END;
+/
