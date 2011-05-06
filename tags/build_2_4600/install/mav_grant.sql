394b3beb947de045c3c4b183050f3b88208eecd0
Ade.Edwards <Ade.Edwards@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2011-05-06 14:09:56 +0000

    svnimporter: adding tags to revision 5540
    git-svn-id: svn://127.0.0.1/Core@5541 17c802ae-cbca-534c-9e8e-6d19a4926c45

---
+++ tags/build_2_4600/install/mav_grant.sql
@@ -0,0 +1,15 @@
+--
+--------------------------------------------------------------------------------
+--   PVCS Identifiers :-
+--
+--       sccsid           : $Header:   //vm_latest/archives/nm3/install/mav_grant.sql-arc   3.0   May 06 2011 15:09:56   Ade.Edwards  $
+--       Module Name      : $Workfile:   mav_grant.sql  $
+--       Date into PVCS   : $Date:   May 06 2011 15:09:56  $
+--       Date fetched Out : $Modtime:   May 06 2011 15:08:04  $
+--       PVCS Version     : $Revision:   3.0  $
+--
+--------------------------------------------------------------------------------
+--
+
+GRANT MERGE ANY VIEW TO PUBLIC;
+
