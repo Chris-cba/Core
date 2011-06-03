a9b079577c909ca24ed1dbded291cd099c6e64ca
Mike.Alexander <Mike.Alexander@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2011-06-03 10:43:20 +0000

    Initial revision
    git-svn-id: svn://127.0.0.1/Core@5803 17c802ae-cbca-534c-9e8e-6d19a4926c45

---
+++ trunk/install/log_nm_4300_fix15.sql
@@ -0,0 +1,13 @@
+BEGIN
+--
+  hig2.upgrade(p_product        => 'NET'
+              ,p_upgrade_script => 'log_nm_4300_fix15.sql'
+              ,p_remarks        => 'NET 4300 FIX 15'
+              ,p_to_version     => Null);
+--
+  commit;
+--
+EXCEPTION
+  WHEN others THEN Null;
+END;
+/
