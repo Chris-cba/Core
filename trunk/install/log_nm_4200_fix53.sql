b0f52a8a212065ee9bc65512918a5ea04ebad3e4
Mike.Alexander <Mike.Alexander@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2011-09-02 09:12:36 +0000

    Initial revision
    git-svn-id: svn://127.0.0.1/Core@6171 17c802ae-cbca-534c-9e8e-6d19a4926c45

---
+++ trunk/install/log_nm_4200_fix53.sql
@@ -0,0 +1,13 @@
+BEGIN
+--
+  hig2.upgrade(p_product        => 'NET'
+              ,p_upgrade_script => 'log_nm_4200_fix53.sql'
+              ,p_remarks        => 'NET 4200 FIX 53'
+              ,p_to_version     => Null);
+--
+  commit;
+--
+EXCEPTION
+  WHEN others THEN Null;
+END;
+/
