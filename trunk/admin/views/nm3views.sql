acecae4fb54f10ee39a808432496fd58190372e7
Steve.Cooper <Steve.Cooper@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2011-09-02 10:53:38 +0000

    task 0111403 - Added 3 new views to support the generation of the dynamic view scripts. v_nm_rebuild_all_inv_sdo_join, v_nm_rebuild_all_nat_sdo_join and v_nm_rebuild_all_nlt_sdo_join.
    git-svn-id: svn://127.0.0.1/Core@6181 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/views/nm3views.sql
+++ trunk/admin/views/nm3views.sql
@@ -1350,7 +1350,36 @@ start '&run_file'
 --
 ----------------------------------------------------------------------------------------- 
 --
-
+SET TERM ON 
+PROMPT v_nm_rebuild_all_inv_sdo_join.vw
+SET TERM OFF
+SET DEFINE ON 
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||'&terminator'||'views'||'&terminator'||'v_nm_rebuild_all_inv_sdo_join.vw' run_file 
+FROM dual 
+/ 
+start '&run_file'
+--
+----------------------------------------------------------------------------------------- 
+--
+SET TERM ON 
+PROMPT v_nm_rebuild_all_nat_sdo_join.vw
+SET TERM OFF
+SET DEFINE ON 
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||'&terminator'||'views'||'&terminator'||'v_nm_rebuild_all_nat_sdo_join.vw' run_file 
+FROM dual 
+/ 
+start '&run_file'
+--
+----------------------------------------------------------------------------------------- 
+--
+SET TERM ON 
+PROMPT v_nm_rebuild_all_nlt_sdo_join.vw
+SET TERM OFF
+SET DEFINE ON 
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||'&terminator'||'views'||'&terminator'||'v_nm_rebuild_all_nlt_sdo_join.vw' run_file 
+FROM dual 
+/ 
+start '&run_file'
 
 
 
