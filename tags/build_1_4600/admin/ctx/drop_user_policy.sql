b9bff86f17b7ea0312926481914e4e75238df3fa
Mike.Alexander <Mike.Alexander@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2011-09-15 10:35:14 +0000

    svnimporter: adding tags to revision 6245
    git-svn-id: svn://127.0.0.1/Core@6246 17c802ae-cbca-534c-9e8e-6d19a4926c45

---
+++ tags/build_1_4600/admin/ctx/drop_user_policy.sql
@@ -0,0 +1,50 @@
+--
+SET SERVEROUTPUT ON SIZE 100000
+--
+DECLARE
+--
+--   PVCS Identifiers :-
+--
+--       sccsid           : $Header:   //vm_latest/archives/nm3/admin/ctx/drop_user_policy.sql-arc   3.1   Sep 15 2011 11:35:14   Mike.Alexander  $
+--       Module Name      : $Workfile:   drop_user_policy.sql  $
+--       Date into SCCS   : $Date:   Sep 15 2011 11:35:14  $
+--       Date fetched Out : $Modtime:   Sep 15 2011 11:33:18  $
+--       SCCS Version     : $Revision:   3.1  $
+--
+--   Drop Inventory/Merge security policies using user for 4500 upgrade
+--    this script is for use wit 4500 upgrade only and will be made redundant 
+--    after this release
+--
+-----------------------------------------------------------------------------
+--	Copyright (c) Bentley Systems Inc., 2011
+-----------------------------------------------------------------------------
+--
+   CURSOR cs_policies_to_drop (c_owner VARCHAR2) IS
+   SELECT object_owner
+         ,object_name
+         ,policy_name
+    FROM  all_policies
+   WHERE  object_owner = c_owner
+    AND   substr(object_name,1,7) IN ('NM_INV_','NM_MRG_');
+   --
+BEGIN
+--
+   FOR cs_rec IN cs_policies_to_drop (User)
+    LOOP
+      BEGIN
+         dbms_rls.drop_policy (object_schema => cs_rec.object_owner
+                              ,object_name   => cs_rec.object_name
+                              ,policy_name   => cs_rec.policy_name
+                              );
+         dbms_output.put_line(cs_policies_to_drop%ROWCOUNT||'. '||cs_rec.policy_name||' on '||cs_rec.object_owner||'.'||cs_rec.object_name||' dropped');
+      EXCEPTION
+         WHEN others
+          THEN
+            dbms_output.put_line(cs_policies_to_drop%ROWCOUNT||'. ### '||cs_rec.policy_name||' on '||cs_rec.object_name||' ERROR');
+            dbms_output.put_line(SUBSTR(SQLERRM,1,255));
+      END;
+      dbms_output.put_line('--');
+   END LOOP;
+--
+END;
+/
