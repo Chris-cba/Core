92fc2002fbdfb6443675f2705acbe5275cc95291
Steve.Cooper <Steve.Cooper@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2012-04-10 14:23:16 +0000

    Task 0111912 - Merged script to drop the associated Pk Index into this script.
    git-svn-id: svn://127.0.0.1/Core@6929 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/utl/mv_highlight_index.sql
+++ trunk/admin/utl/mv_highlight_index.sql
@@ -2,43 +2,62 @@
 --------------------------------------------------------------------------------
 --   PVCS Identifiers :-
 --
---       sccsid           : $Header:   //vm_latest/archives/nm3/admin/utl/mv_highlight_index.sql-arc   3.0   Dec 22 2010 10:14:32   Ade.Edwards  $
+--       sccsid           : $Header:   //vm_latest/archives/nm3/admin/utl/mv_highlight_index.sql-arc   3.1   Apr 10 2012 15:23:16   Steve.Cooper  $
 --       Module Name      : $Workfile:   mv_highlight_index.sql  $
---       Date into PVCS   : $Date:   Dec 22 2010 10:14:32  $
---       Date fetched Out : $Modtime:   Dec 22 2010 10:12:08  $
---       PVCS Version     : $Revision:   3.0  $
+--       Date into PVCS   : $Date:   Apr 10 2012 15:23:16  $
+--       Date fetched Out : $Modtime:   Apr 10 2012 15:21:36  $
+--       PVCS Version     : $Revision:   3.1  $
 --
 --------------------------------------------------------------------------------
 --
 
 PROMPT Drop the Primary Key constraint
 
-DECLARE
-  ex_not_exists EXCEPTION;
-  PRAGMA EXCEPTION_INIT (ex_not_exists,-02443);
-BEGIN
-  EXECUTE IMMEDIATE 
-    'ALTER TABLE MV_HIGHLIGHT DROP CONSTRAINT MV_HIGHLIGHT_PK';
-EXCEPTION
-  WHEN ex_not_exists THEN NULL;
-END;
+Declare
+  Ex_Not_Exists Exception;
+  Pragma Exception_Init (Ex_Not_Exists,-02443);
+Begin
+  Execute Immediate 
+    'Alter Table Mv_Highlight Drop Constraint Mv_Highlight_Pk';
+Exception
+  When Ex_Not_Exists Then Null;
+End;
+/
+
+PROMPT Drop the Primary Key Index
+
+Declare
+  Index_Not_Exists Exception;
+  Pragma Exception_Init (Index_Not_Exists,-01418);
+Begin
+  Execute Immediate 'Drop Index Mv_Highlight_PK';
+Exception
+  When  Index_Not_Exists Then
+    Null;   
+End;
 /
 
 PROMPT Create a non-unique index to replace it
 
-DECLARE
-  ex_not_exists EXCEPTION;
-  PRAGMA EXCEPTION_INIT (ex_not_exists,-01418);
-BEGIN
-  EXECUTE IMMEDIATE
-    'DROP INDEX MV_HIGHLIGHT_IND';
-  EXECUTE IMMEDIATE
-    'CREATE INDEX MV_HIGHLIGHT_IND ON MV_HIGHLIGHT(MV_ID, MV_FEAT_TYPE)';
-EXCEPTION
-  WHEN ex_not_exists 
-  THEN 
-    EXECUTE IMMEDIATE
-      'CREATE INDEX MV_HIGHLIGHT_IND ON MV_HIGHLIGHT(MV_ID, MV_FEAT_TYPE)';
-END;
+
+Declare
+  Index_Not_Exists Exception;
+  Pragma Exception_Init (Index_Not_Exists,-01418);
+Begin
+  Execute Immediate 'Drop Index Mv_Highlight_Ind';
+Exception
+  When  Index_Not_Exists Then
+    Null;   
+End;
 /
+
+
+Create Index Mv_Highlight_Ind On Mv_Highlight
+(
+Mv_Id,
+Mv_Feat_Type
+)
+/
+
+
 
