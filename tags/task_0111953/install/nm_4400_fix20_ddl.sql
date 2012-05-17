c9545628db2831a82471a5d81e614224ef3d0e1c
Steve.Cooper <Steve.Cooper@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2012-05-17 14:05:10 +0000

    svnimporter: adding tags to revision 7091
    git-svn-id: svn://127.0.0.1/Core@7092 17c802ae-cbca-534c-9e8e-6d19a4926c45

---
+++ tags/task_0111953/install/nm_4400_fix20_ddl.sql
@@ -0,0 +1,24 @@
+Create Table Nm_Admin_Extents
+(
+Nae_Admin_Unit    Number(9),        
+Nae_Extent        Sdo_Geometry,
+Constraint Nae_Pk Primary Key (Nae_Admin_Unit),
+Constraint Nae_Nau_Fk Foreign Key (Nae_Admin_Unit) References Nm_Admin_Units_All(Nau_Admin_Unit) On Delete Cascade
+)
+Organization Index
+/
+
+Comment On Table Nm_Admin_Extents Is 'Used to hold the default extent per admin unit, for use with mapviewer.'
+/
+
+Comment On Column  Nm_Admin_Extents.Nae_Admin_Unit Is 'The admin unit for the extent.'
+/
+
+Comment On Column  Nm_Admin_Extents.Nae_Extent Is 'The default Extent for an admin unit.'
+/
+
+Begin
+  Nm3Ddl.Refresh_All_Synonyms;
+End;
+/
+
