5ce37f89f68bb2bf6f4c8294ebee3b243397bacb
Rob.Coupe <Rob.Coupe@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2012-07-24 13:45:18 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/Core@7371 17c802ae-cbca-534c-9e8e-6d19a4926c45

---
+++ trunk/install/doc_issued_century_repair.sql
@@ -0,0 +1,18 @@
+--
+--------------------------------------------------------------------------------
+--   PVCS Identifiers :-
+--
+--       sccsid           : $Header:   //vm_latest/archives/nm3/install/doc_issued_century_repair.sql-arc   1.0   Jul 24 2012 14:45:18   Rob.Coupe  $
+--       Module Name      : $Workfile:   doc_issued_century_repair.sql  $
+--       Date into PVCS   : $Date:   Jul 24 2012 14:45:18  $
+--       Date fetched Out : $Modtime:   Jul 24 2012 14:44:26  $
+--       PVCS Version     : $Revision:   1.0  $
+--
+--------------------------------------------------------------------------------
+--   Copyright (c) 2012 Bentley Systems Incorporated.
+--------------------------------------------------------------------------------
+-- doc_issued_century_repair.sql - module to repair damage by incorrect implicit date format mask being applied in the doc-loader process.
+--
+update docs
+set doc_date_issued = to_date(to_char(doc_date_issued, 'DD-MON-RR'), 'DD-MON-RR')
+where  doc_date_issued < to_date ('01-jan-0900', 'DD-MON-YYYY')
