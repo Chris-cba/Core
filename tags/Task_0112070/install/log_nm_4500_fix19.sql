59ee7f19a356e3c72b93f04a0730ea01ba2f1f19
Rob.Coupe <Rob.Coupe@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2012-07-26 14:12:44 +0000

    svnimporter: adding tags to revision 7397
    git-svn-id: svn://127.0.0.1/Core@7398 17c802ae-cbca-534c-9e8e-6d19a4926c45

---
+++ tags/Task_0112070/install/log_nm_4500_fix19.sql
@@ -0,0 +1,13 @@
+BEGIN
+--
+  hig2.upgrade(p_product        => 'NET'
+              ,p_upgrade_script => 'log_nm_4500_fix19.sql'
+              ,p_remarks        => 'NET 4500 FIX 19'
+              ,p_to_version     => Null);
+--
+  commit;
+--
+EXCEPTION
+  WHEN others THEN Null;
+END;
+/
