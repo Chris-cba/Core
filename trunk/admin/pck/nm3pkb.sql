2476716f1d3882f4293a26ebf66b31948984ccbc
Rob.Coupe <Rob.Coupe@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2012-08-06 09:06:24 +0000

    Task 0111916 - Delete Global assets needs package to bulk-remove asset shapes - new package nm3sdo_ops
    git-svn-id: svn://127.0.0.1/Core@7462 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/pck/nm3pkb.sql
+++ trunk/admin/pck/nm3pkb.sql
@@ -2,11 +2,11 @@
 --
 --   PVCS Identifiers :-
 --
---       pvcsid           : $Header:   //vm_latest/archives/nm3/admin/pck/nm3pkb.sql-arc   2.38   May 28 2012 14:50:14   Steve.Cooper  $
+--       pvcsid           : $Header:   //vm_latest/archives/nm3/admin/pck/nm3pkb.sql-arc   2.39   Aug 06 2012 10:06:24   Rob.Coupe  $
 --       Module Name      : $Workfile:   nm3pkb.sql  $
---       Date into PVCS   : $Date:   May 28 2012 14:50:14  $
---       Date fetched Out : $Modtime:   May 28 2012 14:47:24  $
---       PVCS Version     : $Revision:   2.38  $
+--       Date into PVCS   : $Date:   Aug 06 2012 10:06:24  $
+--       Date fetched Out : $Modtime:   Aug 06 2012 10:05:24  $
+--       PVCS Version     : $Revision:   2.39  $
 --
 --
 --   Author : Graeme Johnson
@@ -2327,6 +2327,17 @@ PROMPT web_user_info.pkw
 SET TERM OFF
 SET DEFINE ON 
 SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||'&terminator'||'pck'||'&terminator'||'web_user_info.pkw' run_file
+FROM dual 
+/ 
+start '&run_file'
+--
+----------------------------------------------------------------------------------------- 
+--
+SET TERM ON 
+PROMPT nm3sdo_ops.pkw	
+SET TERM OFF
+SET DEFINE ON 
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||'&terminator'||'pck'||'&terminator'||'nm3sdo_ops.pkw' run_file
 FROM dual 
 / 
 start '&run_file'
