850dba5dc25f31b71bbbdf4acc1e1e98eb02866d
Rob.Coupe <Rob.Coupe@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2012-08-08 10:37:56 +0000

    Approved extra metadata changes
    git-svn-id: svn://127.0.0.1/Core@7483 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/install/nm4500_nm4600_metadata_upg.sql
+++ trunk/install/nm4500_nm4600_metadata_upg.sql
@@ -8,11 +8,11 @@
 --
 --   PVCS Identifiers :-
 --
---       PVCS id          : $Header:   //vm_latest/archives/nm3/install/nm4500_nm4600_metadata_upg.sql-arc   1.0   Jul 18 2012 13:51:14   Rob.Coupe  $
+--       PVCS id          : $Header:   //vm_latest/archives/nm3/install/nm4500_nm4600_metadata_upg.sql-arc   1.1   Aug 08 2012 11:37:56   Rob.Coupe  $
 --       Module Name      : $Workfile:   nm4500_nm4600_metadata_upg.sql  $
---       Date into PVCS   : $Date:   Jul 18 2012 13:51:14  $
---       Date fetched Out : $Modtime:   Jul 18 2012 13:35:20  $
---       Version          : $Revision:   1.0  $
+--       Date into PVCS   : $Date:   Aug 08 2012 11:37:56  $
+--       Date fetched Out : $Modtime:   Aug 08 2012 11:28:34  $
+--       Version          : $Revision:   1.1  $
 --
 ------------------------------------------------------------------
 --	Copyright (c) exor corporation ltd, 2011
@@ -40,6 +40,195 @@ BEGIN
 END;
 /
 
+------------------------------------------------------------------
+
+
+------------------------------------------------------------------
+SET TERM ON
+PROMPT New process to clean up forms parameter table everyday
+SET TERM OFF
+
+------------------------------------------------------------------
+-- 
+-- DEVELOPMENT COMMENTS (STEVEN COOPER)
+-- New process to clean up forms parameter table everyday
+-- 
+------------------------------------------------------------------
+Begin
+  Insert Into Hig_Process_Types
+  (
+  Hpt_Process_Type_Id,
+  Hpt_Name,
+  Hpt_Descr,
+  Hpt_What_To_Call,
+  Hpt_Initiation_Module,
+  Hpt_Internal_Module,
+  Hpt_Internal_Module_Param,
+  Hpt_Process_Limit,
+  Hpt_Restartable,
+  Hpt_See_In_Hig2510,
+  Hpt_Polling_Enabled,
+  Hpt_Polling_Ftp_Type_Id,
+  Hpt_Area_Type
+  )
+  Values
+  ( 
+  -3,
+  'Core Housekeeping',
+  'A general collection of Core housekeeping routines.',
+  'Hig_Router_Params_Utils.Clear_Down_Old_Params;',
+  Null,
+  Null,
+  Null,
+  Null,
+  'N',
+  'Y',
+  'N',
+  Null,
+  Null
+  ); 
+Exception
+  When Dup_Val_On_Index Then
+    Null;
+End;  
+/
+
+Begin                   
+  Insert Into Hig_Process_Type_Roles
+  (
+  Hptr_Process_Type_Id,
+  Hptr_Role
+  )
+  Values
+  ( 
+  -3,
+  'NET_ADMIN' 
+  );
+Exception
+  When Dup_Val_On_Index Then
+    Null;
+End;
+/
+
+Begin
+  Insert Into Hig_Process_Type_Frequencies
+  (
+  Hpfr_Process_Type_Id,
+  Hpfr_Frequency_Id,
+  Hpfr_Seq
+  )
+  Values 
+  (
+  -3,
+  -11,
+  1
+  );
+Exception
+  When Dup_Val_On_Index Then
+    Null;
+End;
+/
+
+Declare
+  l_Process_Id            Hig_Processes.Hp_Process_Id%Type;
+  l_Job_Name              Hig_Processes.Hp_Job_Name%Type;
+  l_Scheduled_Start_Date  Date;
+Begin
+  Hig_Process_Api.Create_And_Schedule_Process (
+                                              pi_Process_Type_Id           =>   -3,
+                                              pi_Initiators_Ref            =>   'COREHOUSE',
+                                              pi_Start_Date                =>   Sysdate,
+                                              pi_Frequency_Id              =>   -11,
+                                              po_Process_Id                =>   l_Process_Id,
+                                              po_Job_Name                  =>   l_Job_Name,
+                                              po_Scheduled_Start_Date      =>   l_Scheduled_Start_Date
+                                              );
+   Commit;
+  Dbms_Output.Put_Line('Created Core Houseleeping Process');                                              
+  Dbms_Output.Put_Line('Process_Id:'            || To_Char(l_Process_Id) );
+  Dbms_Output.Put_Line('Job_Name:'              || l_Job_Name);
+  Dbms_Output.Put_Line('Scheduled_Start_Date:'  || To_Char(l_Scheduled_Start_Date,'dd-mm-yyyy hh24:mi.ss'));  
+End;
+/
+
+
+------------------------------------------------------------------
+
+
+------------------------------------------------------------------
+SET TERM ON
+PROMPT New messages used within the user form hig1832.
+SET TERM OFF
+
+------------------------------------------------------------------
+-- 
+-- DEVELOPMENT COMMENTS (STEVEN COOPER)
+-- New messages used within the user form hig1832.
+-- 
+------------------------------------------------------------------
+Begin
+  Insert Into Nm_Errors
+  (
+  Ner_Appl,
+  Ner_Id,
+  Ner_Descr
+  )
+  Values
+  (
+  'NET',
+  559,
+  'User Successfully Unlocked'
+  );
+Exception
+  When Dup_Val_On_Index Then
+    Null;
+End;
+/
+
+Begin
+  Insert Into Nm_Errors
+  (
+  Ner_Appl,
+  Ner_Id,
+  Ner_Descr
+  )
+  Values
+  (
+  'NET',
+  560,
+  'Invalid Username'
+  );
+Exception
+  When Dup_Val_On_Index Then
+    Null;
+End;
+/
+------------------------------------------------------------------
+
+
+------------------------------------------------------------------
+SET TERM ON
+PROMPT Error Messages
+SET TERM OFF
+
+------------------------------------------------------------------
+-- 
+-- DEVELOPMENT COMMENTS (LINESH SORATHIA)
+-- New error message for Navigator.
+-- 
+------------------------------------------------------------------
+INSERT INTO NM_ERRORS ( NER_APPL
+                      , NER_ID
+                      , NER_DESCR)
+SELECT 'HIG'
+     , 557
+     , 'Asset is end dated'
+     FROM DUAL
+     WHERE NOT EXISTS (SELECT 'X' 
+                       FROM NM_ERRORS
+                       WHERE NER_APPL = 'HIG'
+                       AND NER_ID = 557)
+/
 ------------------------------------------------------------------
 
 
