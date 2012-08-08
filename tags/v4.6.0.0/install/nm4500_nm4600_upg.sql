0cce55bfab038e9f14fc655e9b8013790cae5169
Rob.Coupe <Rob.Coupe@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2012-08-08 10:53:24 +0000

    svnimporter: adding tags to revision 7484
    git-svn-id: svn://127.0.0.1/Core@7485 17c802ae-cbca-534c-9e8e-6d19a4926c45

---
+++ tags/v4.6.0.0/install/nm4500_nm4600_upg.sql
@@ -0,0 +1,378 @@
+--
+-----------------------------------------------------------------------------
+--
+--   PVCS Identifiers :-
+--
+--       PVCS id          : $Header:   //vm_latest/archives/nm3/install/nm4500_nm4600_upg.sql-arc   1.7   Aug 08 2012 11:53:24   Rob.Coupe  $
+--       Module Name      : $Workfile:   nm4500_nm4600_upg.sql  $
+--       Date into PVCS   : $Date:   Aug 08 2012 11:53:24  $
+--       Date fetched Out : $Modtime:   Aug 08 2012 11:52:50  $
+--       Version          : $Revision:   1.7  $
+--
+--   Product upgrade script
+--
+-----------------------------------------------------------------------------
+--	Copyright (c) exor corporation ltd, 2011
+-----------------------------------------------------------------------------
+--
+SET ECHO OFF
+SET LINESIZE 120
+SET HEADING OFF
+SET FEEDBACK OFF
+-- Grab date/time to append to log file names this is standard to all upgrade/install scripts
+undefine log_extension
+col         log_extension new_value log_extension noprint
+set term off
+select  TO_CHAR(sysdate,'DDMONYYYY_HH24MISS')||'.LOG' log_extension from dual
+/
+set term on
+---------------------------------------------------------------------------------------------------
+-- Spool to Logfile
+define logfile1='nm4500_nm4600_1_&log_extension'
+define logfile2='nm4500_nm4600_2_&log_extension'
+spool &logfile1
+--get some db info
+SELECT 'Install Running on ' ||LOWER(USER||'@'||instance_name||'.'||host_name)||' - DB ver : '||version
+FROM v$instance;
+SELECT 'Current version of '||hpr_product||' ' ||hpr_version
+FROM hig_products
+WHERE hpr_product IN ('HIG','NET','DOC','AST','WMP');
+---------------------------------------------------------------------------------------------------
+--                        ****************   CHECK(S)   *******************
+WHENEVER SQLERROR EXIT
+-- Check that the DB version is correct
+Declare
+  Cursor c_db_version Is
+  Select '11gr2' 
+  From   v$version
+  Where  banner Like '%11.2.0.2%';
+  --
+  l_11gr2 Varchar2(10);
+Begin
+   Open  c_db_version;
+   Fetch c_db_version Into l_11gr2;
+   Close c_db_version;
+   --
+   If l_11gr2 Is Null
+   Then
+    RAISE_APPLICATION_ERROR(-20001,'The database version does not comply with the certification matrix - contact exor support for further information');
+   End If;
+End;
+/
+--
+---------------------------------------------------------------------------------------------------
+--                        ****************   CHECK(S)   *******************
+--
+begin
+   hig2.pre_upgrade_check (p_product               => 'HIG'
+                          ,p_new_version           => '4.6.0.0'
+                          ,p_allowed_old_version_1 => '4.5.0.0'
+                          );
+END;
+/
+WHENEVER SQLERROR CONTINUE
+--
+---------------------------------------------------------------------------------------------------
+--                        **************** DROP POLICIES *******************
+--
+-- drop any policies that could be effected by table changes
+-- ensure that later on - after a compile schema these policies are re-created
+SET TERM ON
+PROMPT Dropping Policies...
+SET TERM OFF
+SET DEFINE ON
+SET VERIFY OFF
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||
+        '&terminator'||'ctx'||'&terminator'||'drop_policy' run_file
+FROM dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+---------------------------------------------------------------------------------------------------
+--                        **************** TYPES   ****************
+--
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||
+        '&terminator'||'typ'||'&terminator'||'nm3typ.sql' run_file
+FROM dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+---------------------------------------------------------------------------------------------------
+--                        ****************   DDL   *******************
+SET TERM ON
+PROMPT DDL Changes...
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'nm3'||'&terminator'||'install'||
+        '&terminator'||'nm4500_nm4600_ddl_upg.sql' run_file
+FROM dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+---------------------------------------------------------------------------------------------------
+--                        **************** VIEWS   ****************
+SET TERM ON
+PROMPT Views...
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||
+        '&terminator'||'views'||'&terminator'||'nm3views.sql' run_file
+FROM dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'nm3'||'&terminator'||'install'||
+        '&terminator'||'higviews.sql' run_file
+FROM dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+---------------------------------------------------------------------------------------------------
+--                        **************** TRIGGERS   ****************
+--
+SET TERM ON
+PROMPT Triggers...
+SET TERM OFF
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||
+        '&terminator'||'trg'||'&terminator'||'nm3trg.sql' run_file
+FROM dual
+/
+SET FEEDBACK ON
+start '&&run_file'
+SET FEEDBACK OFF
+
+SET TERM ON
+PROMPT Who Triggers
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||'&terminator'||'trg'||
+       '&terminator'||'who_trg.sql' run_file
+FROM dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+---------------------------------------------------------------------------------------------------
+--                        **************** APPLICATION CONTEXTS ****************
+--
+SET TERM ON
+Prompt Application Contexts...
+--SET TERM OFF
+SET DEFINE ON
+select '&exor_base'||'nm3'||'&terminator'||'admin'||
+        '&terminator'||'ctx'||'&terminator'||'nm3ctx.sql' run_file
+from dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+---------------------------------------------------------------------------------------------------
+--                  **************** PACKAGE HEADERS AND BODIES   ****************
+--
+SET TERM ON
+PROMPT Package Headers...
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||
+       '&terminator'||'pck'||'&terminator'||'nm3pkh.sql' run_file
+FROM dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT Package Bodies...
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||
+       '&terminator'||'pck'||'&terminator'||'nm3pkb.sql' run_file
+FROM dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+---------------------------------------------------------------------------------------------------
+--                ****************   MERGE ANY VIEW  *******************
+--
+--
+SET TERM ON
+PROMPT Grant MERGE ANY VIEW to PUBLIC
+SET TERM OFF
+SET DEFINE ON
+select '&exor_base'||'nm3'||'&terminator'||'install'||
+        '&terminator'||'mav_grant.sql' run_file
+from dual
+/
+SET FEEDBACK ON
+start '&&run_file'
+SET FEEDBACK OFF
+--
+---------------------------------------------------------------------------------------------------
+--                        ****************   COMPILE SCHEMA   *******************
+SET TERM ON
+Prompt Creating Compiling Schema Script...
+SET TERM OFF
+SPOOL OFF
+SET define ON
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||
+        '&terminator'||'utl'||'&terminator'||'compile_schema.sql' run_file
+FROM dual
+/
+start '&run_file'
+spool &logfile2
+SET TERM ON
+start compile_all.sql
+--
+---------------------------------------------------------------------------------------------------
+--                        **************** ADD POLICIES *******************
+-- re-create the policies that were dropped at the beginning of the upgrade
+--
+SET TERM ON
+PROMPT Adding Policies...
+SET TERM OFF
+SET DEFINE ON
+SET VERIFY OFF
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||
+    '&terminator'||'ctx'||'&terminator'||'add_policy' run_file
+FROM dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+---------------------------------------------------------------------------------------------------
+--                  ****************   METADATA  *******************
+SET TERM ON
+PROMPT Metadata Changes...
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'nm3'||'&terminator'||'install'||
+        '&terminator'||'nm4500_nm4600_metadata_upg.sql' run_file
+FROM dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+SET DEFINE ON
+SELECT '&exor_base'||'nm3'||'&terminator'||'install'||
+        '&terminator'||'nm3data_help.sql' run_file
+FROM dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+---------------------------------------------------------------------------------------------------
+--                        ****************   SYNONYMS   *******************
+SET TERM ON
+Prompt Creating Synonyms That Do Not Exist...
+SET TERM OFF
+EXECUTE nm3ddl.refresh_all_synonyms;
+--
+---------------------------------------------------------------------------------------------------
+--                        ****************   ROLES   *******************
+SET TERM ON
+Prompt Updating HIG_USER_ROLES...
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'nm3'||'&terminator'||'install'||
+    '&terminator'||'hig_user_roles.sql' run_file
+from dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+Prompt Ensuring all users have HIG_USER role...
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'nm3'||'&terminator'||'install'||
+    '&terminator'||'users_without_hig_user.sql' run_file
+from dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+---------------------------------------------------------------------------------------------------
+--                  ****************   CREATE ACLs   *******************
+SET TERM ON
+Prompt Creating ACLs...
+SET TERM OFF
+EXECUTE nm3acl.create_standard_acls;
+--
+---------------------------------------------------------------------------------------------------
+--                  ****************   gtype update  *******************
+SET FEEDBACK OFF
+SET TERM ON
+PROMPT Converting Multi-part sdo_gtypes...
+SET TERM OFF
+SET DEFINE ON
+select '&exor_base'||'nm3'||'&terminator'||'install'||
+        '&terminator'||'gtype_3306_update.sql' run_file
+from dual
+/
+SET FEEDBACK ON
+start '&&run_file'
+SET FEEDBACK OFF
+--
+---------------------------------------------------------------------------------------------------
+--                  ****************   doc-issued century repair  *******************
+SET FEEDBACK OFF
+SET TERM ON
+PROMPT Doc-issued date century repair...
+SET TERM OFF
+SET DEFINE ON
+select '&exor_base'||'nm3'||'&terminator'||'install'||
+        '&terminator'||'doc_issued_century_repair.sql' run_file
+from dual
+/
+SET FEEDBACK ON
+start '&&run_file'
+SET FEEDBACK OFF
+
+
+--
+---------------------------------------------------------------------------------------------------
+--                        ****************   VERSION NUMBER   *******************
+SET TERM ON
+Prompt Setting The Version Number...
+SET TERM OFF
+BEGIN
+      hig2.upgrade('HIG','nm4500_nm4600_upg.sql','Upgrade from 4.5.0.0 to 4.6.0.0','4.6.0.0');
+      hig2.upgrade('NET','nm4500_nm4600_upg.sql','Upgrade from 4.5.0.0 to 4.6.0.0','4.6.0.0');
+      hig2.upgrade('DOC','nm4500_nm4600_upg.sql','Upgrade from 4.5.0.0 to 4.6.0.0','4.6.0.0');
+      hig2.upgrade('AST','nm4500_nm4600_upg.sql','Upgrade from 4.5.0.0 to 4.6.0.0','4.6.0.0');
+      hig2.upgrade('WMP','nm4500_nm4600_upg.sql','Upgrade from 4.5.0.0 to 4.6.0.0','4.6.0.0');
+END;
+/
+COMMIT;
+SET HEADING OFF
+SELECT 'Product updated to version '||hpr_product||' ' ||hpr_version product_version
+FROM hig_products
+WHERE hpr_product IN ('HIG','NET','DOC','AST','WMP');
+spool off
+exit
+---------------------------------------------------------------------------------------------------
+--                        ****************   END OF SCRIPT   *******************
