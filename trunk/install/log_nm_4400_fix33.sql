d1206aa237eb7f6c2b79f419e19d169832cdb813
Rob.Coupe <Rob.Coupe@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2012-09-18 14:34:02 +0000

    minor change
    git-svn-id: svn://127.0.0.1/Core@7678 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/install/log_nm_4400_fix33.sql
+++ trunk/install/log_nm_4400_fix33.sql
@@ -1,8 +1,8 @@
 BEGIN
 --
   hig2.upgrade(p_product        => 'NET'
-              ,p_upgrade_script => 'log_nm_4400_fix32.sql'
-              ,p_remarks        => 'NET 4400 FIX 32'
+              ,p_upgrade_script => 'log_nm_4400_fix33.sql'
+              ,p_remarks        => 'NET 4400 FIX 33'
               ,p_to_version     => Null);
 --
   commit;
