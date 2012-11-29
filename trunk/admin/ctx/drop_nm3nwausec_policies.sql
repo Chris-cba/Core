ab5228bb8ef3fd2b923b90d841e3bad723bd3ac6
Rob.Coupe <Rob.Coupe@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2012-11-29 10:12:06 +0000

    pvcs tag problem
    git-svn-id: svn://127.0.0.1/Core@7759 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/ctx/drop_nm3nwausec_policies.sql
+++ trunk/admin/ctx/drop_nm3nwausec_policies.sql
@@ -1,11 +1,11 @@
 --
 --   PVCS Identifiers :-
 --
---       sccsid           : $Header  $
+--       sccsid           : $Header:   //vm_latest/archives/nm3/admin/ctx/drop_nm3nwausec_policies.sql-arc   1.1   Nov 29 2012 10:12:06   Rob.Coupe  $
 --       Module Name      : $Workfile:   drop_nm3nwausec_policies.sql  $
---       Date into SCCS   : $Date:   Nov 29 2012 10:01:34  $
---       Date fetched Out : $Modtime:   Nov 27 2012 15:13:14  $
---       SCCS Version     : $Revision:   1.0  $
+--       Date into SCCS   : $Date:   Nov 29 2012 10:12:06  $
+--       Date fetched Out : $Modtime:   Nov 29 2012 10:11:44  $
+--       SCCS Version     : $Revision:   1.1  $
 --
 -----------------------------------------------------------------------------
 --    Copyright (c) Bentley Systems 2012
