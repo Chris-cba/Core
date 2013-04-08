52cc54dee4798602e8d9d5bb85e7c67f19e30b22
Rob.Coupe <Rob.Coupe@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2013-04-08 15:36:36 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/Core@7928 17c802ae-cbca-534c-9e8e-6d19a4926c45

---
+++ trunk/install/nm_4500_fix34.sql
@@ -0,0 +1,95 @@
+--   PVCS Identifiers :-
+--
+--       sccsid           : $Header:   //vm_latest/archives/nm3/install/nm_4500_fix34.sql-arc   1.0   Apr 08 2013 16:36:36   Rob.Coupe  $
+--       Module Name      : $Workfile:   nm_4500_fix34.sql  $ 
+--       Date into PVCS   : $Date:   Apr 08 2013 16:36:36  $
+--       Date fetched Out : $Modtime:   Apr 08 2013 16:16:24  $
+--       PVCS Version     : $Revision:   1.0  $
+--
+----------------------------------------------------------------------------
+--   Copyright (c) 2012 Bentley Systems Incorporated.
+----------------------------------------------------------------------------
+--
+
+set echo off
+set linesize 120
+set heading off
+set feedback off
+--
+-- Grab date/time to append to log file name
+--
+undefine log_extension
+col      log_extension new_value log_extension noprint
+set term off
+select  TO_CHAR(sysdate,'DDMONYYYY_HH24MISS')||'.LOG' log_extension from dual
+/
+set term on
+--
+--------------------------------------------------------------------------------
+--
+--
+-- Spool to Logfile
+--
+define logfile1='nm_4500_fix34_&log_extension'
+spool &logfile1
+--
+--------------------------------------------------------------------------------
+--
+select 'Fix Date ' || to_char(sysdate, 'DD-MON-YYYY HH24:MI:SS') from dual;
+
+SELECT 'Install Running on ' ||LOWER(USER||'@'||instance_name||'.'||host_name)||' - DB ver : '||version
+FROM v$instance;
+--
+SELECT 'Current version of '||hpr_product||' ' ||hpr_version
+FROM hig_products
+WHERE hpr_product IN ('HIG','NET');
+
+WHENEVER SQLERROR EXIT
+
+BEGIN
+ --
+ -- Check that the user isn't sys or system
+ --
+ IF USER IN ('SYS','SYSTEM')
+ THEN
+   RAISE_APPLICATION_ERROR(-20000,'You cannot install this product as ' || USER);
+ END IF;
+
+ --
+ -- Check that HIG has been installed @ v4.5.0.0
+ --
+ hig2.product_exists_at_version (p_product        => 'NET'
+                                ,p_VERSION        => '4.5.0.0'
+                                );
+
+END;
+/
+WHENEVER SQLERROR CONTINUE
+--
+--------------------------------------------------------------------------------
+-- Package Bodies
+--------------------------------------------------------------------------------
+--
+SET TERM ON 
+PROMPT nm3merge.pkw
+SET TERM OFF
+
+SET FEEDBACK ON
+start nm3merge.pkw
+SET FEEDBACK OFF
+
+--
+--------------------------------------------------------------------------------
+-- Update hig_upgrades with fix ID
+--------------------------------------------------------------------------------
+--
+SET TERM ON 
+PROMPT log_nm_4500_fix34.sql 
+--
+SET FEEDBACK ON
+start log_nm_4500_fix34.sql 
+SET FEEDBACK OFF
+SPOOL OFF
+EXIT
+--
+--
