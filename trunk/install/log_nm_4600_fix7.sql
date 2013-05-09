341c272cbfbf7955cc4c87bb62afcdebc950c48b
Graeme.Johnson <Graeme.Johnson@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2013-05-09 08:52:44 +0000

    Typo
    git-svn-id: svn://127.0.0.1/Core@8000 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/install/log_nm_4600_fix7.sql
+++ trunk/install/log_nm_4600_fix7.sql
@@ -1,8 +1,8 @@
 BEGIN
 --
   hig2.upgrade(p_product        => 'NET'
-              ,p_upgrade_script => 'log_nm_4600_fix6.sql'
-              ,p_remarks        => 'NM 4600 FIX 6'
+              ,p_upgrade_script => 'log_nm_4600_fix7.sql'
+              ,p_remarks        => 'NM 4600 FIX 7'
               ,p_to_version     => Null);
 --
   commit;
