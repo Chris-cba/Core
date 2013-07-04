7fadbf4a05c116e6c14c46d58f9cced8149fc81c
James.Wadsworth <James.Wadsworth@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2013-07-04 12:46:36 +0000

    svnimporter: adding tags to revision 8157
    git-svn-id: svn://127.0.0.1/Core@8158 17c802ae-cbca-534c-9e8e-6d19a4926c45

---
+++ tags/v4.7.0.0/install/mav_grant.sql
@@ -0,0 +1,17 @@
+--
+--------------------------------------------------------------------------------
+--   PVCS Identifiers :-
+--
+--       sccsid           : $Header:   //vm_latest/archives/nm3/install/mav_grant.sql-arc   3.1   Jul 04 2013 13:46:36   James.Wadsworth  $
+--       Module Name      : $Workfile:   mav_grant.sql  $
+--       Date into PVCS   : $Date:   Jul 04 2013 13:46:36  $
+--       Date fetched Out : $Modtime:   Jul 04 2013 13:27:20  $
+--       PVCS Version     : $Revision:   3.1  $
+--
+--------------------------------------------------------------------------------
+--    Copyright (c) 2013 Bentley Systems Incorporated. All rights reserved.
+-----------------------------------------------------------------------------
+--
+
+GRANT MERGE ANY VIEW TO PUBLIC;
+
