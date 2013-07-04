ea015a8701b15455d252b6d6ed2e5416e04cb39f
James.Wadsworth <James.Wadsworth@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2013-07-04 12:47:20 +0000

    S-95677: Added Bentley copyright notice to the PVCS header.
    git-svn-id: svn://127.0.0.1/Core@8166 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/install/nm_4400_fix3.sql
+++ trunk/install/nm_4400_fix3.sql
@@ -2,14 +2,14 @@
 --------------------------------------------------------------------------------
 --   PVCS Identifiers :-
 --
---       sccsid           : $Header:   //vm_latest/archives/nm3/install/nm_4400_fix3.sql-arc   3.0   Sep 20 2011 14:21:10   Mike.Alexander  $
+--       sccsid           : $Header:   //vm_latest/archives/nm3/install/nm_4400_fix3.sql-arc   3.1   Jul 04 2013 13:47:20   James.Wadsworth  $
 --       Module Name      : $Workfile:   nm_4400_fix3.sql  $
---       Date into PVCS   : $Date:   Sep 20 2011 14:21:10  $
---       Date fetched Out : $Modtime:   Sep 20 2011 14:18:52  $
---       PVCS Version     : $Revision:   3.0  $
+--       Date into PVCS   : $Date:   Jul 04 2013 13:47:20  $
+--       Date fetched Out : $Modtime:   Jul 04 2013 11:54:24  $
+--       PVCS Version     : $Revision:   3.1  $
 --
 --------------------------------------------------------------------------------
---   Copyright (c) Bentley Systems Incorporated, 2011
+--   Copyright (c) 2013 Bentley Systems Incorporated. All rights reserved.
 --------------------------------------------------------------------------------
 --
 set echo off
