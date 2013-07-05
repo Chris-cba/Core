f878209cc42252fdaba662c1194dc780b5b02c94
James.Wadsworth <James.Wadsworth@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2013-07-05 09:43:28 +0000

    S-95677: Added Bentley copyright notice to the file.
    git-svn-id: svn://127.0.0.1/Core@8411 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/c/gri0207/gri0207.mk
+++ trunk/admin/c/gri0207/gri0207.mk
@@ -10,6 +10,7 @@
 #       Date fetched Out : 07/06/13 13:55:37
 #       SCCS Version     : 1.1# 
 #
+#  Copyright (c) 2013 Bentley Systems Incorporated. All rights reserved.
 # ***********************************************************************
 #
 # To make this module :
