4e0d95340f7fc07fdc68b3b030c726cf0453df77
Rob.Coupe <Rob.Coupe@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2013-10-31 11:33:34 +0000

    Re-added copyright stuff
    git-svn-id: svn://127.0.0.1/Core@8547 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/install/import_nm_upload_files.bat
+++ trunk/install/import_nm_upload_files.bat
@@ -2,12 +2,15 @@
 REM
 REM   PVCS Identifiers :-
 REM
-REM      PVCS id          : $Header:   //vm_latest/archives/nm3/install/import_nm_upload_files.bat-arc   2.2   Oct 31 2013 11:28:50   Rob.Coupe  $
+REM      PVCS id          : $Header:   //vm_latest/archives/nm3/install/import_nm_upload_files.bat-arc   2.3   Oct 31 2013 11:33:34   Rob.Coupe  $
 REM      Module Name      : $Workfile:   import_nm_upload_files.bat  $
-REM      Date into PVCS   : $Date:   Oct 31 2013 11:28:50  $
-REM      Date fetched Out : $Modtime:   Oct 31 2013 11:28:10  $
-REM      Version          : $Revision:   2.2  $
+REM      Date into PVCS   : $Date:   Oct 31 2013 11:33:34  $
+REM      Date fetched Out : $Modtime:   Oct 31 2013 11:33:04  $
+REM      Version          : $Revision:   2.3  $
 REM
+rem -------------------------------------------------------------------------
+rem 	Copyright (c) 2013 Bentley Systems Incorporated. All rights reserved  
+rem -------------------------------------------------------------------------
 ECHO .
 ECHO *********************************************************
 ECHO Enter USERNAME/PASSWORD@ALIAS when prompted for username.
