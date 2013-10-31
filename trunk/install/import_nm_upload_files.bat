f45e4dd39bee24ac6edadb205256fb9a8488ee0f
Rob.Coupe <Rob.Coupe@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2013-10-31 11:28:50 +0000

    removed invalid comment markers and shifted the pvcs header
    git-svn-id: svn://127.0.0.1/Core@8546 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/install/import_nm_upload_files.bat
+++ trunk/install/import_nm_upload_files.bat
@@ -1,16 +1,13 @@
 @echo off
-rem -------------------------------------------------------------------------
-rem  SCCS Identifiers :-
-rem 
-rem        sccsid           : '"@(#)import_nm_upload_files.bat	1.1 04/16/04"'
-rem        Module Name      : '"import_nm_upload_files.bat"'
-rem        Date into SCCS   : '"04/04/16 14:14:51"'
-rem        Date fetched Out : '"07/06/13 13:57:11"'
-rem        SCCS Version     : '"1.1"'
-rem 
-rem -------------------------------------------------------------------------
-rem 	Copyright (c) 2013 Bentley Systems Incorporated. All rights reserved  
-rem -------------------------------------------------------------------------
+REM
+REM   PVCS Identifiers :-
+REM
+REM      PVCS id          : $Header:   //vm_latest/archives/nm3/install/import_nm_upload_files.bat-arc   2.2   Oct 31 2013 11:28:50   Rob.Coupe  $
+REM      Module Name      : $Workfile:   import_nm_upload_files.bat  $
+REM      Date into PVCS   : $Date:   Oct 31 2013 11:28:50  $
+REM      Date fetched Out : $Modtime:   Oct 31 2013 11:28:10  $
+REM      Version          : $Revision:   2.2  $
+REM
 ECHO .
 ECHO *********************************************************
 ECHO Enter USERNAME/PASSWORD@ALIAS when prompted for username.
@@ -20,13 +17,3 @@ ECHO Any "ORA-00001: unique constraint (NM31.NUF_PK) violated"
 ECHO  exceptions which arise from this import may be safely ignored
 ECHO .
 imp parfile=nm_upload_files.par
---
---   PVCS Identifiers :-
---
---       PVCS id          : $Header:   //vm_latest/archives/nm3/install/import_nm_upload_files.bat-arc   2.1   Jul 04 2013 13:45:32   James.Wadsworth  $
---       Module Name      : $Workfile:   import_nm_upload_files.bat  $
---       Date into PVCS   : $Date:   Jul 04 2013 13:45:32  $
---       Date fetched Out : $Modtime:   Jul 04 2013 12:03:30  $
---       Version          : $Revision:   2.1  $
---
---
