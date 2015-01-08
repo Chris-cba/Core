3a52d9d9b32f213fbf5a8731c7c86cd92b313969
Stephen.Sewell <Stephen.Sewell@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2015-01-08 10:23:16 +0000

    Updated to correct version number.
    git-svn-id: svn://127.0.0.1/Core@8805 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/install/log_nm_4700_fix14.sql
+++ trunk/install/log_nm_4700_fix14.sql
@@ -2,11 +2,11 @@
 --------------------------------------------------------------------------------
 --   PVCS Identifiers :-
 --
---       sccsid           : $Header:   //new_vm_latest/archives/nm3/install/log_nm_4700_fix14.sql-arc   1.0   Jan 08 2015 10:20:06   Stephen.Sewell  $
+--       sccsid           : $Header:   //new_vm_latest/archives/nm3/install/log_nm_4700_fix14.sql-arc   3.0   Jan 08 2015 10:23:16   Stephen.Sewell  $
 --       Module Name      : $Workfile:   log_nm_4700_fix14.sql  $
---       Date into PVCS   : $Date:   Jan 08 2015 10:20:06  $
+--       Date into PVCS   : $Date:   Jan 08 2015 10:23:16  $
 --       Date fetched Out : $Modtime:   Jan 08 2015 10:12:32  $
---       PVCS Version     : $Revision:   1.0  $
+--       PVCS Version     : $Revision:   3.0  $
 --
 --------------------------------------------------------------------------------
 --   Copyright (c) 2014 Bentley Systems Incorporated.
