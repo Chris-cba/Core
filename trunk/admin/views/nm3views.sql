e763fcfc502879789702f58500b39f0db1667cfa
Mike.Huitson <Mike.Huitson@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2015-01-16 11:36:12 +0000

    Added v_nm_msv_styles.vw
    git-svn-id: svn://127.0.0.1/Core@8889 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/views/nm3views.sql
+++ trunk/admin/views/nm3views.sql
@@ -1523,3 +1523,14 @@ SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||'&terminator'||'views'||'&te
 FROM dual 
 / 
 start '&run_file'
+--
+----------------------------------------------------------------------------------------- 
+--
+SET TERM ON 
+PROMPT v_nm_msv_styles.vw
+SET TERM OFF
+SET DEFINE ON 
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||'&terminator'||'views'||'&terminator'||'v_nm_msv_styles.vw' run_file 
+FROM dual 
+/ 
+start '&run_file'
