df59117995faead729fa0b722c09ef219f06996c
Rob.Coupe <Rob.Coupe@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2015-01-20 13:23:40 +0000

    Added list of objects/synonyms etc.
    git-svn-id: svn://127.0.0.1/Core@8899 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/install/lb_install.sql
+++ trunk/install/lb_install.sql
@@ -1,10 +1,10 @@
 --   PVCS Identifiers :-
 --
---       pvcsid           : $Header:   //new_vm_latest/archives/lb/install/lb_install.sql-arc   1.1   Jan 15 2015 21:47:46   Rob.Coupe  $
+--       pvcsid           : $Header:   //new_vm_latest/archives/lb/install/lb_install.sql-arc   1.2   Jan 20 2015 13:23:40   Rob.Coupe  $
 --       Module Name      : $Workfile:   lb_install.sql  $
---       Date into PVCS   : $Date:   Jan 15 2015 21:47:46  $
---       Date fetched Out : $Modtime:   Jan 15 2015 21:24:18  $
---       PVCS Version     : $Revision:   1.1  $
+--       Date into PVCS   : $Date:   Jan 20 2015 13:23:40  $
+--       Date fetched Out : $Modtime:   Jan 20 2015 13:22:08  $
+--       PVCS Version     : $Revision:   1.2  $
 --
 --   Author : R.A. Coupe
 --
@@ -18,12 +18,33 @@
 
 /* LB Install file */
 
---Need to upgrade int_array
+DECLARE
+   not_exists   exception;
+   PRAGMA EXCEPTION_INIT (not_exists, -942);
+BEGIN
+   EXECUTE IMMEDIATE 'DROP TABLE LB_OBJECTS';
+EXCEPTION
+   WHEN not_exists
+Then NULL;
+end;
+/ 
 
-drop type int_array force;
---
-start int_array.tyh;
-start int_array.tyb;
+create table lb_objects
+( object_name varchar2(128) NOT NULL,
+  object_type varchar2(23) NOT NULL )
+/
+  
+  
+create unique index LB_OBJECTS_PK on lb_objects (object_name, object_type )
+/
+
+ALTER TABLE LB_OBJECTS ADD (
+  PRIMARY KEY
+  (OBJECT_NAME, OBJECT_TYPE)
+  USING INDEX
+  ENABLE VALIDATE)
+/  
+
 
 start lb_data_types.sql;
 
@@ -58,4 +79,60 @@ start ..\admin\pck\LB_GET.pkb;
 start ..\admin\pck\LB_LOAD.pkb;
 start ..\admin\pck\LB_LOC.pkb;
 start ..\admin\pck\LB_PATH.pkb;
+
+
+DECLARE
+   TYPE object_name_type IS TABLE OF VARCHAR2 (123) INDEX BY binary_integer;
+--
+   TYPE object_type_type IS TABLE OF VARCHAR2 (23) INDEX BY binary_integer;
+--
+   l_object_name   object_name_type;
+   l_object_type   object_type_type;
+
+   PROCEDURE add_object (p_object_name VARCHAR2, p_object_type VARCHAR2)
+   IS
+      i   CONSTANT PLS_INTEGER := l_object_name.COUNT + 1;
+   BEGIN
+      l_object_name (i) := p_object_name;
+      l_object_type (i) := p_object_type;
+   END add_object;
+BEGIN
+   add_object ('LB_PATH_REG', 'PACKAGE');
+   add_object ('LB_OPS', 'PACKAGE');
+   add_object ('LB_REG', 'PACKAGE');
+   add_object ('LB_REF', 'PACKAGE');
+   add_object ('LB_GET', 'PACKAGE');
+   add_object ('LB_LOAD', 'PACKAGE');
+   add_object ('LB_LOC', 'PACKAGE');
+   add_object ('LB_PATH', 'PACKAGE');
+   add_object ('LB_PATH_REG', 'PACKAGE BODY');
+   add_object ('LB_OPS', 'PACKAGE BODY');
+   add_object ('LB_REG', 'PACKAGE BODY');
+   add_object ('LB_REF', 'PACKAGE BODY');
+   add_object ('LB_GET', 'PACKAGE BODY');
+   add_object ('LB_LOAD', 'PACKAGE BODY');
+   add_object ('LB_LOC', 'PACKAGE BODY');
+   add_object ('LB_PATH', 'PACKAGE BODY'); 
+   add_object ('CREATE_NLT_GEOMETRY_VIEW','PROCEDURE');  
+   add_object ('V_LB_NLT_GEOMETRY', 'VIEW' );
+--     
+   FOR i IN 1 .. l_object_name.COUNT
+   LOOP
+      INSERT INTO lb_objects (object_name, object_type)
+           VALUES (l_object_name (i), l_object_type (i));
+   END LOOP;
+END;
+/
+
+
+declare
+  cursor c1 is
+    select * from lb_objects l
+    where object_type in ('PACKAGE', 'TABLE', 'VIEW', 'VIEW', 'PROCEDURE', 'SEQUENCE' );
+begin
+  for irec in c1 loop
+    NM3DDL.CREATE_SYNONYM_FOR_OBJECT(irec.object_name, irec.object_type);
+  end loop;
+end;    
+/
 
