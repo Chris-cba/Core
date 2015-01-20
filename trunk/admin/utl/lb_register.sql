bb60494e8dbad4c240a721b5a6870d759b33dab0
Rob.Coupe <Rob.Coupe@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2015-01-20 20:05:34 +0000

    Added a descriptive text
    git-svn-id: svn://127.0.0.1/Core@8915 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/utl/lb_register.sql
+++ trunk/admin/utl/lb_register.sql
@@ -1,10 +1,10 @@
 --   PVCS Identifiers :-
 --
---       pvcsid           : $Header:   //new_vm_latest/archives/lb/admin/utl/lb_register.sql-arc   1.0   Jan 14 2015 15:52:02   Rob.Coupe  $
+--       pvcsid           : $Header:   //new_vm_latest/archives/lb/admin/utl/lb_register.sql-arc   1.1   Jan 20 2015 20:05:34   Rob.Coupe  $
 --       Module Name      : $Workfile:   lb_register.sql  $
---       Date into PVCS   : $Date:   Jan 14 2015 15:52:02  $
---       Date fetched Out : $Modtime:   Jan 14 2015 15:51:22  $
---       PVCS Version     : $Revision:   1.0  $
+--       Date into PVCS   : $Date:   Jan 20 2015 20:05:34  $
+--       Date fetched Out : $Modtime:   Jan 20 2015 20:05:12  $
+--       PVCS Version     : $Revision:   1.1  $
 --
 --       Author:  Rob Coupe
 --
@@ -12,6 +12,17 @@
 -- Copyright (c) 2015 Bentley Systems Incorporated. All rights reserved.
 -----------------------------------------------------------------------------
 --
+/*
+The script below will merely throw a set of asset types into an exor system. The asset types will be registered as
+Location Bridge asset types and will be mapped to a numeric asset type originating in an external system such as eB.
+
+The hard-coded value of 1 is a network type (from the Exor Linear Types NLT_ID). The value of 1 may not exist on the
+server being used so it is important that the data is editted to suit the source system. If th escript is executed without
+having modified the NLT_ID parameter, th euser may elect to enter the relationship in NM_INV_NW in SQL*Plus or
+use the Exor forms.
+
+*/
+
 
 DECLARE
    l_start_date   DATE;
