8b62cef1fa90c2b3d8b1b51344f6c15365ab3c69
Chris.Baugh <Chris.Baugh@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2015-03-12 14:17:52 +0000

    Added v_node_proximity_check.vw view
    git-svn-id: svn://127.0.0.1/Core@8972 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/views/nm3views.sql
+++ trunk/admin/views/nm3views.sql
@@ -1534,3 +1534,14 @@ SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||'&terminator'||'views'||'&te
 FROM dual 
 / 
 start '&run_file'
+--
+----------------------------------------------------------------------------------------- 
+--
+SET TERM ON 
+PROMPT v_node_proximity_check.vw
+SET TERM OFF
+SET DEFINE ON 
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||'&terminator'||'views'||'&terminator'||'v_node_proximity_check.vw' run_file 
+FROM dual 
+/ 
+start '&run_file'
