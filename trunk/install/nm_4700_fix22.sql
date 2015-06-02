b8eddf88a38a2775bcc9e05eec90763cfb263075
Vikas.Mhetre <Vikas.Mhetre@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2015-06-02 11:24:56 +0000

    Changes in previous version
    git-svn-id: svn://127.0.0.1/Core@9130 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/install/nm_4700_fix22.sql
+++ trunk/install/nm_4700_fix22.sql
@@ -2,26 +2,98 @@
 --------------------------------------------------------------------------------
 --   PVCS Identifiers :-
 --
---       sccsid           : $Header:   //new_vm_latest/archives/nm3/install/nm_4700_fix22.sql-arc   1.1   Jun 02 2015 12:10:12   Vikas.Mhetre  $
---       Module Name      : $Workfile:   log_nm_4700_fix22.sql  $
---       Date into PVCS   : $Date:   Jun 02 2015 12:10:12  $
---       Date fetched Out : $Modtime:   May 27 2015 12:33:18  $
---       PVCS Version     : $Revision:   1.1  $
+--       sccsid           : $Header:   //new_vm_latest/archives/nm3/install/nm_4700_fix22.sql-arc   1.2   Jun 02 2015 12:24:56   Vikas.Mhetre  $
+--       Module Name      : $Workfile:   nm_4700_fix22.sql  $
+--       Date into PVCS   : $Date:   Jun 02 2015 12:24:56  $
+--       Date fetched Out : $Modtime:   Jun 02 2015 12:24:12  $
+--       PVCS Version     : $Revision:   1.2  $
 --
 --------------------------------------------------------------------------------
 --   Copyright (c) 2015 Bentley Systems Incorporated.  All rights reserved.
 --------------------------------------------------------------------------------
 --
+SET ECHO OFF
+SET LINESIZE 120
+SET HEADING OFF
+SET FEEDBACK OFF
+--
+-- Grab date/time to append to log file name
+--
+UNDEFINE log_extension
+COL      log_extension NEW_VALUE log_extension NOPRINT
+SET TERM OFF
+SELECT  TO_CHAR(sysdate,'DDMONYYYY_HH24MISS')||'.LOG' log_extension FROM DUAL
+/
+SET TERM ON
+--
+--------------------------------------------------------------------------------
+--
+-- Spool to Logfile
+--
+DEFINE logfile1='nm_4700_fix22_&log_extension'
+SPOOL &logfile1
+--
+--------------------------------------------------------------------------------
+--
+SELECT 'Fix Date ' || TO_CHAR(sysdate, 'DD-MON-YYYY HH24:MI:SS') FROM DUAL;
+--
+SELECT 'Install Running on ' ||LOWER(USER||'@'||instance_name||'.'||host_name)||' - DB ver : '||version
+FROM v$instance;
+--
+SELECT 'Current version of '||hpr_product||' ' ||hpr_version
+FROM hig_products
+WHERE hpr_product IN ('HIG','NET');
+--
+--------------------------------------------------------------------------------
+--      Check(s)
+--------------------------------------------------------------------------------
+--
+WHENEVER SQLERROR EXIT
+--
 BEGIN
 --
-  hig2.upgrade(p_product        => 'NET'
-              ,p_upgrade_script => 'log_nm_4700_fix22.sql'
-              ,p_remarks        => 'NET 4700 FIX 22'
-              ,p_to_version     => Null);
+--      Check that the user isn't sys or system
 --
-  commit;
+        IF USER IN ('SYS','SYSTEM')
+        THEN
+          RAISE_APPLICATION_ERROR(-20000,'You cannot install this product as ' || USER);
+        END IF;
 --
-EXCEPTION
-  WHEN others THEN Null;
+--      Check that HIG has been installed @ v4.7.0.x
+--
+        HIG2.PRODUCT_EXISTS_AT_VERSION  (p_product        => 'HIG'
+                                        ,p_VERSION        => '4.7.0.0'
+                                        );
+--
+--
 END;
 /
+WHENEVER SQLERROR CONTINUE
+--
+--
+--------------------------------------------------------------------------------
+-- Package Bodies
+--------------------------------------------------------------------------------
+--
+SET TERM ON 
+PROMPT nm3homo.pkw
+SET TERM OFF
+--
+SET FEEDBACK ON
+start nm3homo.pkw
+SET FEEDBACK OFF
+--
+--------------------------------------------------------------------------------
+-- Update hig_upgrades with fix ID
+--------------------------------------------------------------------------------
+--
+SET TERM ON
+PROMPT log_nm_4700_fix22.sql
+--
+SET FEEDBACK ON
+start log_nm_4700_fix22.sql
+SET FEEDBACK OFF
+SPOOL OFF
+EXIT
+--
+--
