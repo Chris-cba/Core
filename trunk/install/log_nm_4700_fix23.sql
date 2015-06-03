451cbf0cdf6a1c2823f4e8fc5cdd4566d8a1ad55
Upendra.Hukeri <Upendra.Hukeri@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2015-06-03 09:08:40 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/Core@9138 17c802ae-cbca-534c-9e8e-6d19a4926c45

---
+++ trunk/install/log_nm_4700_fix23.sql
@@ -0,0 +1,17 @@
+BEGIN
+	--
+	hig2.upgrade(p_product        => 'NET'
+				,p_upgrade_script => 'log_nm_4700_fix23.sql'
+				,p_remarks        => 'NET 4700 FIX 23'
+				,p_to_version     => NULL
+				);
+	--
+	COMMIT;
+	--
+EXCEPTION 
+	WHEN OTHERS THEN
+	--
+		NULL;
+	--
+END;
+/
