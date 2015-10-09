a9da3757b557e7a8e09b794768403ca76e2dca18
Rob.Coupe <Rob.Coupe@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2015-10-09 13:00:12 +0000

    corrected relative path
    git-svn-id: svn://127.0.0.1/Core@9396 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/eB_interface/install_eB_interface.sql
+++ trunk/admin/eB_interface/install_eB_interface.sql
@@ -1,10 +1,10 @@
    --   PVCS Identifiers :-
    --
-   --       pvcsid           : $Header:   //new_vm_latest/archives/lb/admin/eB_interface/install_eB_interface.sql-arc   1.0   Oct 09 2015 13:38:56   Rob.Coupe  $
+   --       pvcsid           : $Header:   //new_vm_latest/archives/lb/admin/eB_interface/install_eB_interface.sql-arc   1.1   Oct 09 2015 14:00:12   Rob.Coupe  $
    --       Module Name      : $Workfile:   install_eB_interface.sql  $
-   --       Date into PVCS   : $Date:   Oct 09 2015 13:38:56  $
-   --       Date fetched Out : $Modtime:   Oct 09 2015 13:36:34  $
-   --       PVCS Version     : $Revision:   1.0  $
+   --       Date into PVCS   : $Date:   Oct 09 2015 14:00:12  $
+   --       Date fetched Out : $Modtime:   Oct 09 2015 13:58:10  $
+   --       PVCS Version     : $Revision:   1.1  $
    --
    --   Author : R.A. Coupe
    --
@@ -14,17 +14,17 @@
    -- Copyright (c) 2015 Bentley Systems Incorporated. All rights reserved.
    ----------------------------------------------------------------------------
    --
-start ..\eB_interface\eB_interface.tyh
-start ..\eB_interface\CloseLinearLocation.prc
-start ..\eB_interface\CreateLinearLocation.prc
-start ..\eB_interface\CreateLinearRange.prc
-start ..\eB_interface\GetAssetLinearLocations.fnc
-start ..\eB_interface\GetAssetLinearLocationsTab.fnc
-start ..\eB_interface\GetLinearElementTypes.prc
-start ..\eB_interface\GetLinearLocations.fnc
-start ..\eB_interface\GetLinearRanges.fnc
-start ..\eB_interface\GetNetworkLinearLocations.fnc
-start ..\eB_interface\GetNetworkLinearLocationsTab.fnc
+start ..\admin\eB_interface\eB_interface.tyh
+start ..\admin\eB_interface\CloseLinearLocation.prc
+start ..\admin\eB_interface\CreateLinearLocation.prc
+start ..\admin\eB_interface\CreateLinearRange.prc
+start ..\admin\eB_interface\GetAssetLinearLocations.fnc
+start ..\admin\eB_interface\GetAssetLinearLocationsTab.fnc
+start ..\admin\eB_interface\GetLinearElementTypes.prc
+start ..\admin\eB_interface\GetLinearLocations.fnc
+start ..\admin\eB_interface\GetLinearRanges.fnc
+start ..\admin\eB_interface\GetNetworkLinearLocations.fnc
+start ..\admin\eB_interface\GetNetworkLinearLocationsTab.fnc
 
 prompt end of Location Bridge interface for eB install script
 
