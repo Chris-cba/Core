5655626ae5d1c921fedb33025c5d67e360cdf6df
Rob.Coupe <Rob.Coupe@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2015-10-09 12:38:56 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/Core@9394 17c802ae-cbca-534c-9e8e-6d19a4926c45

---
+++ trunk/admin/eB_interface/install_eB_interface.sql
@@ -0,0 +1,32 @@
+   --   PVCS Identifiers :-
+   --
+   --       pvcsid           : $Header:   //new_vm_latest/archives/lb/admin/eB_interface/install_eB_interface.sql-arc   1.0   Oct 09 2015 13:38:56   Rob.Coupe  $
+   --       Module Name      : $Workfile:   install_eB_interface.sql  $
+   --       Date into PVCS   : $Date:   Oct 09 2015 13:38:56  $
+   --       Date fetched Out : $Modtime:   Oct 09 2015 13:36:34  $
+   --       PVCS Version     : $Revision:   1.0  $
+   --
+   --   Author : R.A. Coupe
+   --
+   --   Location Bridge interface for eB install script.
+   --
+   -----------------------------------------------------------------------------
+   -- Copyright (c) 2015 Bentley Systems Incorporated. All rights reserved.
+   ----------------------------------------------------------------------------
+   --
+start ..\eB_interface\eB_interface.tyh
+start ..\eB_interface\CloseLinearLocation.prc
+start ..\eB_interface\CreateLinearLocation.prc
+start ..\eB_interface\CreateLinearRange.prc
+start ..\eB_interface\GetAssetLinearLocations.fnc
+start ..\eB_interface\GetAssetLinearLocationsTab.fnc
+start ..\eB_interface\GetLinearElementTypes.prc
+start ..\eB_interface\GetLinearLocations.fnc
+start ..\eB_interface\GetLinearRanges.fnc
+start ..\eB_interface\GetNetworkLinearLocations.fnc
+start ..\eB_interface\GetNetworkLinearLocationsTab.fnc
+
+prompt end of Location Bridge interface for eB install script
+
+
+
