831fc96b31a81fcccbe04a1eedcb706d9283d290
Rob.Coupe <Rob.Coupe@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2015-10-19 10:44:54 +0000

    svnimporter: adding tags to revision 9440
    git-svn-id: svn://127.0.0.1/Core@9441 17c802ae-cbca-534c-9e8e-6d19a4926c45

---
+++ tags/v4.1/admin/views/views.sql
@@ -0,0 +1,4 @@
+start ..\admin\views\v_lb_path_links.vw
+start ..\admin\views\v_lb_directed_path_links.vw
+start ..\admin\views\V_LB_PATH_BETWEEN_POINTS.vw
+start ..\admin\views\v_nm_inv_on_network.vw
