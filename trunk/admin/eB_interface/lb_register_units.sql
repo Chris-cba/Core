20acb2dc020248bafbc8ad3e76da71d52a1bd843
Rob.Coupe <Rob.Coupe@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2015-10-29 07:20:38 +0000

    Re-coded to use the lb_reg procedure
    git-svn-id: svn://127.0.0.1/Core@9453 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/eB_interface/lb_register_units.sql
+++ trunk/admin/eB_interface/lb_register_units.sql
@@ -1,11 +1,11 @@
 --
 --   PVCS Identifiers :-
 --
---       pvcsid           : $Header:   //new_vm_latest/archives/lb/admin/eB_interface/lb_register_units.sql-arc   1.0   Oct 19 2015 17:37:56   Rob.Coupe  $
+--       pvcsid           : $Header:   //new_vm_latest/archives/lb/admin/eB_interface/lb_register_units.sql-arc   1.1   Oct 29 2015 07:20:38   Rob.Coupe  $
 --       Module Name      : $Workfile:   lb_register_units.sql  $
---       Date into PVCS   : $Date:   Oct 19 2015 17:37:56  $
---       Date fetched Out : $Modtime:   Oct 19 2015 17:37:28  $
---       PVCS Version     : $Revision:   1.0  $
+--       Date into PVCS   : $Date:   Oct 29 2015 07:20:38  $
+--       Date fetched Out : $Modtime:   Oct 29 2015 07:21:06  $
+--       PVCS Version     : $Revision:   1.1  $
 --
 --   Author : R.A. Coupe fro David Stow
 --
@@ -15,11 +15,11 @@
 --   Copyright (c) 2015 Bentley Systems Incorporated. All rights reserved.
 ------------------------------
 BEGIN
-   lb_register_unit(50, 'METRE', 1);
-   lb_register_unit(236, 'KILOMETRE', 2 );
-   lb_register_unit(51, 'CENTIMETRE', 3 );
-   lb_register_unit(321, 'MILE', 4 );
-   lb_register_unit(77, 'DEGREE', 5 );
-   lb_register_unit(13, 'RADIAN', 6 );
+   lb_reg.register_unit(50, 'METRE', 1);
+   lb_reg.register_unit(236, 'KILOMETRE', 2 );
+   lb_reg.register_unit(51, 'CENTIMETRE', 3 );
+   lb_reg.register_unit(321, 'MILE', 4 );
+   lb_reg.register_unit(77, 'DEGREE', 5 );
+   lb_reg.register_unit(13, 'RADIAN', 6 );
 END;
 /
