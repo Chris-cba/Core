d6311881e99b5cf1b0133eb7930033af93d5826f
Vikas.Mhetre <Vikas.Mhetre@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2015-10-30 09:08:34 +0000

    svnimporter: adding tags to revision 9475
    git-svn-id: svn://127.0.0.1/Core@9476 17c802ae-cbca-534c-9e8e-6d19a4926c45

---
+++ tags/tfs_112697/install/nm_4700_fix30.sql
@@ -0,0 +1,99 @@
+----------------------------------------------------------------------------------------------------
+--   PVCS Identifiers :-
+--
+--       PVCS id          : $Header:   //new_vm_latest/archives/nm3/install/nm_4700_fix30.sql-arc   1.0   Oct 30 2015 09:08:34   Vikas.Mhetre  $
+--       Module Name      : $Workfile:   nm_4700_fix30.sql  $ 
+--       Date into PVCS   : $Date:   Oct 30 2015 09:08:34  $
+--       Date fetched Out : $Modtime:   Oct 30 2015 08:38:50  $
+--       Version          : $Revision:   1.0  $
+--
+----------------------------------------------------------------------------------------------------
+--   Copyright (c) 2015 Bentley Systems Incorporated. All rights reserved.
+----------------------------------------------------------------------------------------------------
+--
+SET ECHO OFF
+SET LINESIZE 120
+SET HEADING OFF
+SET FEEDBACK OFF
+--
+-- Grab date/time to append to log file name
+--
+UNDEFINE log_extension
+COL      log_extension NEW_VALUE log_extension NOPRINT
+SET TERM OFF
+SELECT  TO_CHAR(SYSDATE, 'DDMONYYYY_HH24MISS') || '.LOG' log_extension FROM DUAL
+/
+SET TERM ON
+--
+--------------------------------------------------------------------------------
+-- Spool to Logfile
+--------------------------------------------------------------------------------
+--
+DEFINE logfile1='nm_4700_fix30_&log_extension'
+spool &logfile1
+--
+--------------------------------------------------------------------------------
+--
+SELECT 'Fix Date ' || TO_CHAR(SYSDATE, 'DD-MON-YYYY HH24:MI:SS') FROM DUAL;
+
+SELECT 'Install Running on ' || LOWER(USER || '@' || instance_name || '.' || host_name) || ' - DB ver : ' || version
+FROM v$instance;
+--
+SELECT 'Current version of ' || hpr_product || ' ' || hpr_version
+FROM hig_products
+WHERE hpr_product IN ('HIG', 'NET');
+--
+--
+WHENEVER SQLERROR EXIT
+--
+BEGIN
+--
+-- Check that the user isn't SYS or SYSTEM
+--
+  IF USER IN ('SYS', 'SYSTEM') THEN
+    RAISE_APPLICATION_ERROR(-20000, 'You cannot install this product as ' || USER);
+  END IF;
+--
+-- Check that NET has been installed @ v4.7.0.x
+--
+  hig2.product_exists_at_version  (p_product        => 'NET'
+                                  ,p_VERSION        => '4.7.0.1'
+                                  );
+--
+END;
+/
+--
+WHENEVER SQLERROR CONTINUE
+--
+--------------------------------------------------------------------------------
+-- Package Body
+--------------------------------------------------------------------------------
+--
+SET TERM ON 
+PROMPT Creating Package Body nm3gaz_qry
+SET TERM OFF
+--
+SET FEEDBACK ON
+start nm3gaz_qry.pkw
+SET FEEDBACK OFF
+--
+--------------------------------------------------------------------------------
+-- Update hig_upgrades with fix ID
+--------------------------------------------------------------------------------
+--
+SET TERM ON 
+PROMPT log_nm_4700_fix30.sql
+SET TERM OFF
+--
+SET FEEDBACK ON
+START log_nm_4700_fix30.sql
+SET FEEDBACK OFF
+--
+SPOOL OFF
+--
+EXIT
+--
+---------------------------------------------------------------------------------------------------
+--                        ****************   END OF SCRIPT   *******************
+---------------------------------------------------------------------------------------------------
+--
