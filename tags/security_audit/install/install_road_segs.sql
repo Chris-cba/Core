7e580e75863709332500980968a8b9567b458e0b
Rob.Coupe <Rob.Coupe@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2015-11-13 16:59:12 +0000

    svnimporter: adding tags to revision 9536
    git-svn-id: svn://127.0.0.1/Core@9537 17c802ae-cbca-534c-9e8e-6d19a4926c45

---
+++ tags/security_audit/install/install_road_segs.sql
@@ -0,0 +1,23 @@
+--
+--   PVCS Identifiers :-
+--
+--       sccsid           : $Header:   //new_vm_latest/archives/nm3/install/install_road_segs.sql-arc   1.0   Nov 13 2015 16:59:12   Rob.Coupe  $
+--       Module Name      : $Workfile:   install_road_segs.sql  $
+--       Date into SCCS   : $Date:   Nov 13 2015 16:59:12  $
+--       Date fetched Out : $Modtime:   Nov 13 2015 16:57:46  $
+--       SCCS Version     : $Revision:   1.0  $
+--
+-----------------------------------------------------------------------------
+--    Copyright (c) 2013 Bentley Systems Incorporated. All rights reserved.
+-----------------------------------------------------------------------------
+
+-- script to create new, unrestricted views for use in Locator on HE systems
+--
+
+start road_segs.vw;
+
+start road_segments_all.vw;
+
+
+
+
