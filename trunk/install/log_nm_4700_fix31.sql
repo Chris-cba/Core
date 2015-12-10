828ca14e3782b87aa5b94f3ffa6b5891a38af5d5
Rob.Coupe <Rob.Coupe@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2015-12-10 10:22:58 +0000

    Updated build number in remarks column
    git-svn-id: svn://127.0.0.1/Core@9612 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/install/log_nm_4700_fix31.sql
+++ trunk/install/log_nm_4700_fix31.sql
@@ -2,11 +2,11 @@
 --------------------------------------------------------------------------------
 --   PVCS Identifiers :-
 --
---       sccsid           : $Header:   //new_vm_latest/archives/nm3/install/log_nm_4700_fix31.sql-arc   1.0   Dec 09 2015 22:37:26   Rob.Coupe  $
+--       sccsid           : $Header:   //new_vm_latest/archives/nm3/install/log_nm_4700_fix31.sql-arc   1.1   Dec 10 2015 10:22:58   Rob.Coupe  $
 --       Module Name      : $Workfile:   log_nm_4700_fix31.sql  $
---       Date into PVCS   : $Date:   Dec 09 2015 22:37:26  $
---       Date fetched Out : $Modtime:   Dec 09 2015 22:36:54  $
---       PVCS Version     : $Revision:   1.0  $
+--       Date into PVCS   : $Date:   Dec 10 2015 10:22:58  $
+--       Date fetched Out : $Modtime:   Dec 10 2015 10:22:32  $
+--       PVCS Version     : $Revision:   1.1  $
 --
 --------------------------------------------------------------------------------
 --   Copyright (c) 2015 Bentley Systems Incorporated.  All rights reserved.
@@ -16,7 +16,7 @@ BEGIN
 --
   hig2.upgrade(p_product        => 'NET'
               ,p_upgrade_script => 'log_nm_4700_fix31.sql'
-              ,p_remarks        => 'NET 4700 FIX 31'
+              ,p_remarks        => 'NET 4700 FIX 31 Build 2'
               ,p_to_version     => Null);
 --
   commit;
