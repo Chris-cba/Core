116508ba62bc1c8ee07a99af754c0a0134c2e931
Upendra.Hukeri <Upendra.Hukeri@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2015-12-11 08:52:34 +0000

    Added (Build 2) to the remarks field.
    git-svn-id: svn://127.0.0.1/Core@9618 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/install/log_nm_4500_fix44.sql
+++ trunk/install/log_nm_4500_fix44.sql
@@ -2,7 +2,7 @@ BEGIN
 	--
 	hig2.upgrade(p_product        => 'NET'
 				,p_upgrade_script => 'log_nm_4500_fix44.sql'
-				,p_remarks        => 'NET 4500 FIX 44'
+				,p_remarks        => 'NET 4500 FIX 44 (Build 2)'
 				,p_to_version     => NULL
 				);
 	--
