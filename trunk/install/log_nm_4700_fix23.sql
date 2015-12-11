5840fda5bdce1e1575bd5662235ff115d9286eec
Upendra.Hukeri <Upendra.Hukeri@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2015-12-11 08:56:34 +0000

    Added (Build 2) to the remarks field.
    git-svn-id: svn://127.0.0.1/Core@9620 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/install/log_nm_4700_fix23.sql
+++ trunk/install/log_nm_4700_fix23.sql
@@ -2,7 +2,7 @@ BEGIN
 	--
 	hig2.upgrade(p_product        => 'NET'
 				,p_upgrade_script => 'log_nm_4700_fix23.sql'
-				,p_remarks        => 'NET 4700 FIX 23'
+				,p_remarks        => 'NET 4700 FIX 23 (Build 2)'
 				,p_to_version     => NULL
 				);
 	--
