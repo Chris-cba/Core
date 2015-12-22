931b11f8c56eddbfe187a20c49f68a928daa2343
Sarah.Williams <Sarah.Williams@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2015-12-22 17:09:04 +0000

    Amended script name
    git-svn-id: svn://127.0.0.1/Core@9677 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/install/log_nm_4700_fix33.sql
+++ trunk/install/log_nm_4700_fix33.sql
@@ -2,11 +2,11 @@
 --------------------------------------------------------------------------------
 --   PVCS Identifiers :-
 --
---       pvcsid           : $Header:   //new_vm_latest/archives/nm3/install/log_nm_4700_fix33.sql-arc   1.0   Dec 22 2015 14:25:06   Sarah.Williams  $
+--       pvcsid           : $Header:   //new_vm_latest/archives/nm3/install/log_nm_4700_fix33.sql-arc   1.1   Dec 22 2015 17:09:04   Sarah.Williams  $
 --       Module Name      : $Workfile:   log_nm_4700_fix33.sql  $
---       Date into PVCS   : $Date:   Dec 22 2015 14:25:06  $
---       Date fetched Out : $Modtime:   Dec 22 2015 14:24:52  $
---       PVCS Version     : $Revision:   1.0  $
+--       Date into PVCS   : $Date:   Dec 22 2015 17:09:04  $
+--       Date fetched Out : $Modtime:   Dec 22 2015 17:08:44  $
+--       PVCS Version     : $Revision:   1.1  $
 
 --
 --------------------------------------------------------------------------------
@@ -16,7 +16,7 @@
 BEGIN
 --
   hig2.upgrade(p_product        => 'NET'
-              ,p_upgrade_script => 'exnm04070000en_updt33.sql'
+              ,p_upgrade_script => 'nm_4700_fix33.sql'
               ,p_remarks        => 'NET 4700 FIX 33'
               ,p_to_version     => Null);
 --
