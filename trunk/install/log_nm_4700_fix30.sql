21aaf39dcacf143ad96a1659790ec47918a3a283
Vikas.Mhetre <Vikas.Mhetre@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2016-01-15 07:15:36 +0000

    Modified remark.
    git-svn-id: svn://127.0.0.1/Core@9729 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/install/log_nm_4700_fix30.sql
+++ trunk/install/log_nm_4700_fix30.sql
@@ -1,11 +1,11 @@
 ----------------------------------------------------------------------------------------------------
 --   PVCS Identifiers :-
 --
---       PVCS id          : $Header:   //new_vm_latest/archives/nm3/install/log_nm_4700_fix30.sql-arc   1.0   Oct 30 2015 09:09:18   Vikas.Mhetre  $
+--       PVCS id          : $Header:   //new_vm_latest/archives/nm3/install/log_nm_4700_fix30.sql-arc   1.1   Jan 15 2016 07:15:36   Vikas.Mhetre  $
 --       Module Name      : $Workfile:   log_nm_4700_fix30.sql  $ 
---       Date into PVCS   : $Date:   Oct 30 2015 09:09:18  $
---       Date fetched Out : $Modtime:   Oct 30 2015 08:42:26  $
---       Version          : $Revision:   1.0  $
+--       Date into PVCS   : $Date:   Jan 15 2016 07:15:36  $
+--       Date fetched Out : $Modtime:   Jan 15 2016 06:24:42  $
+--       Version          : $Revision:   1.1  $
 --
 ----------------------------------------------------------------------------------------------------
 --   Copyright (c) 2015 Bentley Systems Incorporated. All rights reserved.
@@ -13,9 +13,9 @@
 --
 BEGIN
 --
-  hig2.upgrade(p_product        => 'AST'
+  hig2.upgrade(p_product        => 'NET'
               ,p_upgrade_script => 'log_nm_4700_fix30.sql'
-              ,p_remarks        => 'AST 4700 FIX 30'
+              ,p_remarks        => 'NET 4700 FIX 30'
               ,p_to_version     => NULL
               );
 --
