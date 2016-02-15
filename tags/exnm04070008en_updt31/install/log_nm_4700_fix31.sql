16910c499262745c3b42ae97bb111b01f36bdf2a
Rob.Coupe <Rob.Coupe@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2016-02-15 12:11:20 +0000

    svnimporter: adding tags to revision 9805
    git-svn-id: svn://127.0.0.1/Core@9806 17c802ae-cbca-534c-9e8e-6d19a4926c45

---
+++ tags/exnm04070008en_updt31/install/log_nm_4700_fix31.sql
@@ -0,0 +1,27 @@
+--
+--------------------------------------------------------------------------------
+--   PVCS Identifiers :-
+--
+--       sccsid           : $Header:   //new_vm_latest/archives/nm3/install/log_nm_4700_fix31.sql-arc   1.5   Feb 15 2016 12:11:20   Rob.Coupe  $
+--       Module Name      : $Workfile:   log_nm_4700_fix31.sql  $
+--       Date into PVCS   : $Date:   Feb 15 2016 12:11:20  $
+--       Date fetched Out : $Modtime:   Feb 15 2016 12:11:20  $
+--       PVCS Version     : $Revision:   1.5  $
+--
+--------------------------------------------------------------------------------
+--   Copyright (c) 2015 Bentley Systems Incorporated.  All rights reserved.
+--------------------------------------------------------------------------------
+--
+BEGIN
+--
+  hig2.upgrade(p_product        => 'NET'
+              ,p_upgrade_script => 'log_nm_4700_fix31.sql'
+              ,p_remarks        => 'NET 4700 FIX 31 Build 8'
+              ,p_to_version     => Null);
+--
+  commit;
+--
+EXCEPTION
+  WHEN others THEN Null;
+END;
+/
