423e6ed42a3cc1147aaa61bccd50296f9047e7ff
Rob.Coupe <Rob.Coupe@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2016-05-20 10:21:02 +0000

    svnimporter: adding tags to revision 9916
    git-svn-id: svn://127.0.0.1/Core@9917 17c802ae-cbca-534c-9e8e-6d19a4926c45

---
+++ tags/v4.3/admin/views/views.sql
@@ -0,0 +1,5 @@
+start ..\admin\views\v_lb_path_links.vw
+start ..\admin\views\v_lb_directed_path_links.vw
+start ..\admin\views\V_LB_PATH_BETWEEN_POINTS.vw
+start ..\admin\views\v_nm_inv_on_network.vw
+start ..\admin\views\v_lb_type_nw_flags.vw
