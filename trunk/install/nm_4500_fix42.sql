d0e28f280ad6a01f25bb13b2ab12a5f324a4096d
Chris.Baugh <Chris.Baugh@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2016-08-26 09:21:08 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/Core@10076 17c802ae-cbca-534c-9e8e-6d19a4926c45

---
+++ trunk/install/nm_4500_fix42.sql
@@ -0,0 +1,86 @@
+--
+--------------------------------------------------------------------------------
+--   PVCS Identifiers :-
+--
+--       sccsid           : $Header:   //new_vm_latest/archives/nm3/install/nm_4500_fix42.sql-arc   1.0   Aug 26 2016 10:21:08   Chris.Baugh  $
+--       Module Name      : $Workfile:   nm_4500_fix42.sql  $
+--       Date into PVCS   : $Date:   Aug 26 2016 10:21:08  $
+--       Date fetched Out : $Modtime:   Mar 04 2015 14:15:36  $
+--       PVCS Version     : $Revision:   1.0  $
+--
+--------------------------------------------------------------------------------
+--   Copyright (c) 2014 Bentley Systems Incorporated.
+--------------------------------------------------------------------------------
+--
+set echo off
+set linesize 120
+set heading off
+set feedback off
+--
+-- Grab date/time to append to log file name
+--
+undefine log_extension
+col      log_extension new_value log_extension noprint
+set term off
+select  TO_CHAR(sysdate,'DDMONYYYY_HH24MISS')||'.LOG' log_extension from dual
+/
+set term on
+--
+--------------------------------------------------------------------------------
+--
+--
+-- Spool to Logfile
+--
+define logfile1='nm_4700_fix42_&log_extension'
+spool &logfile1
+--
+--------------------------------------------------------------------------------
+--
+select 'Fix Date ' || to_char(sysdate, 'DD-MON-YYYY HH24:MI:SS') from dual;
+
+SELECT 'Install Running on ' ||LOWER(USER||'@'||instance_name||'.'||host_name)||' - DB ver : '||version
+FROM v$instance;
+--
+SELECT 'Current version of '||hpr_product||' ' ||hpr_version
+FROM hig_products
+WHERE hpr_product IN ('HIG','NET');
+
+WHENEVER SQLERROR EXIT
+
+BEGIN
+ --
+ -- Check that the user isn't sys or system
+ --
+ IF USER IN ('SYS','SYSTEM')
+ THEN
+   RAISE_APPLICATION_ERROR(-20000,'You cannot install this product as ' || USER);
+ END IF;
+
+ --
+ -- Check that HIG has been installed @ v4.5.0.0
+ --
+ hig2.product_exists_at_version (p_product        => 'HIG'
+                                ,p_VERSION        => '4.5.0.0'
+                                );
+
+END;
+/
+WHENEVER SQLERROR CONTINUE
+
+
+--
+--------------------------------------------------------------------------------
+-- Update hig_upgrades with fix ID
+--------------------------------------------------------------------------------
+--
+SET TERM ON 
+PROMPT log_nm_4500_fix42.sql 
+--
+SET FEEDBACK ON
+start log_nm_4500_fix42.sql
+SET FEEDBACK OFF
+SPOOL OFF
+EXIT
+--
+commit;
+--
