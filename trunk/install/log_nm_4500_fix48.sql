729fb9d5ef4a77bd476d54ffdacf133acb5277a6
Upendra.Hukeri <Upendra.Hukeri@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2016-09-14 07:12:56 +0000

    Build 2 + Copyright Message
    git-svn-id: svn://127.0.0.1/Core@10130 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/install/log_nm_4500_fix48.sql
+++ trunk/install/log_nm_4500_fix48.sql
@@ -1,21 +1,21 @@
 ----------------------------------------------------------------------------------------------------
 --   PVCS Identifiers :-
 --
---       PVCS id          : $Header:   //new_vm_latest/archives/nm3/install/log_nm_4500_fix48.sql-arc   1.0   Jan 05 2016 06:35:58   Upendra.Hukeri  $
+--       PVCS id          : $Header:   //new_vm_latest/archives/nm3/install/log_nm_4500_fix48.sql-arc   1.1   Sep 14 2016 08:12:56   Upendra.Hukeri  $
 --       Module Name      : $Workfile:   log_nm_4500_fix48.sql  $ 
---       Date into PVCS   : $Date:   Jan 05 2016 06:35:58  $
---       Date fetched Out : $Modtime:   Jan 05 2016 05:23:38  $
---       Version     	  : $Revision:   1.0  $
+--       Date into PVCS   : $Date:   Sep 14 2016 08:12:56  $
+--       Date fetched Out : $Modtime:   Sep 14 2016 08:11:10  $
+--       Version     	  : $Revision:   1.1  $
 --
 ----------------------------------------------------------------------------------------------------
---   Copyright (c) 2015 Bentley Systems Incorporated. All rights reserved.
+--   Copyright (c) 2016 Bentley Systems Incorporated. All rights reserved.
 ----------------------------------------------------------------------------------------------------
 --
 BEGIN
 	--
 	hig2.upgrade(p_product        => 'NET'
 				,p_upgrade_script => 'log_nm_4500_fix48.sql'
-				,p_remarks        => 'NET 4500 FIX 48 (Build 1)'
+				,p_remarks        => 'NET 4500 FIX 48 (Build 2)'
 				,p_to_version     => NULL
 				);
 	--
