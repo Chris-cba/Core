911197d62c075b7a87388805351f45ff026a150f
Upendra.Hukeri <Upendra.Hukeri@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2016-09-16 06:35:20 +0000

    Changes for nm 4700 fix 34 build 3 (exnm04070003en_updt34)
    git-svn-id: svn://127.0.0.1/Core@10138 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/install/log_nm_4700_fix34.sql
+++ trunk/install/log_nm_4700_fix34.sql
@@ -1,11 +1,11 @@
 ----------------------------------------------------------------------------------------------------
 --   PVCS Identifiers :-
 --
---       PVCS id          : $Header:   //new_vm_latest/archives/nm3/install/log_nm_4700_fix34.sql-arc   1.1   Feb 22 2016 10:32:42   Upendra.Hukeri  $
+--       PVCS id          : $Header:   //new_vm_latest/archives/nm3/install/log_nm_4700_fix34.sql-arc   1.2   Sep 16 2016 07:35:20   Upendra.Hukeri  $
 --       Module Name      : $Workfile:   log_nm_4700_fix34.sql  $ 
---       Date into PVCS   : $Date:   Feb 22 2016 10:32:42  $
---       Date fetched Out : $Modtime:   Feb 22 2016 10:04:22  $
---       Version     	  : $Revision:   1.1  $
+--       Date into PVCS   : $Date:   Sep 16 2016 07:35:20  $
+--       Date fetched Out : $Modtime:   Sep 16 2016 07:02:44  $
+--       Version     	  : $Revision:   1.2  $
 --
 ----------------------------------------------------------------------------------------------------
 --   Copyright (c) 2016 Bentley Systems Incorporated. All rights reserved.
@@ -15,7 +15,7 @@ BEGIN
 	--
 	hig2.upgrade(p_product        => 'NET'
 				,p_upgrade_script => 'log_nm_4700_fix34.sql'
-				,p_remarks        => 'NET 4700 FIX 34 (Build 2)'
+				,p_remarks        => 'NET 4700 FIX 34 (Build 3)'
 				,p_to_version     => NULL
 				);
 	--
