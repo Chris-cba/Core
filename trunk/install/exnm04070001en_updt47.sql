5f7849e740d837fbdfe42f8b42b12ee6a10db963
Rob.Coupe <Rob.Coupe@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2016-09-16 21:51:44 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/Core@10150 17c802ae-cbca-534c-9e8e-6d19a4926c45

---
+++ trunk/install/exnm04070001en_updt47.sql
@@ -0,0 +1,27 @@
+--
+--------------------------------------------------------------------------------
+--   PVCS Identifiers :-
+--
+--       sccsid           : $Header:   //new_vm_latest/archives/lb/install/exnm04070001en_updt47.sql-arc   1.0   Sep 16 2016 22:51:44   Rob.Coupe  $
+--       Module Name      : $Workfile:   exnm04070001en_updt47.sql  $
+--       Date into PVCS   : $Date:   Sep 16 2016 22:51:44  $
+--       Date fetched Out : $Modtime:   Sep 15 2016 13:48:42  $
+--       PVCS Version     : $Revision:   1.0  $
+--
+--------------------------------------------------------------------------------
+--   Copyright (c) 2015 Bentley Systems Incorporated.  All rights reserved.
+--------------------------------------------------------------------------------
+--
+BEGIN
+--
+  hig2.upgrade(p_product        => 'LB'
+              ,p_upgrade_script => 'log_nm_4700_fix47.sql'
+              ,p_remarks        => 'NET 4700 FIX 47 Build 1'
+              ,p_to_version     => Null);
+--
+  commit;
+--
+EXCEPTION
+  WHEN others THEN Null;
+END;
+/
