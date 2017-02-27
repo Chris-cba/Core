cdbf31acfcaeb2674a15026c706a4bbf42167d6c
Upendra.Hukeri <Upendra.Hukeri@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2017-02-27 06:47:58 +0000

    svnimporter: adding tags to revision 10387
    git-svn-id: svn://127.0.0.1/Core@10388 17c802ae-cbca-534c-9e8e-6d19a4926c45

---
+++ tags/v4.8.0.0/admin/Java/auth10-federation/src/main/java/com/auth10/federation/ClaimType.java
@@ -0,0 +1,20 @@
+/*--------------------------------------------------------------------------------------+
+ |
+ |     $Source: $
+ |    $RCSfile: $
+ |   $Revision:   1.0  $
+ |       $Date:   Feb 27 2017 06:47:58  $
+ |     $Author:   Upendra.Hukeri  $
+ |
+ |  $Copyright: (c) 2009 Bentley Systems, Incorporated. All rights reserved. $
+ |
+ +--------------------------------------------------------------------------------------*/
+
+package com.auth10.federation;
+
+public class ClaimType
+{
+    public String Uri;
+
+    public boolean Optional;
+}
