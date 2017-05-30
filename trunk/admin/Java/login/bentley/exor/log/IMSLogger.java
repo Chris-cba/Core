5d5a918afee89753f0e8cf8fc1ebea8600502261
Upendra.Hukeri <Upendra.Hukeri@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2017-05-30 12:38:34 +0000

    core fix 52
    git-svn-id: svn://127.0.0.1/Core@10535 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/Java/login/bentley/exor/log/IMSLogger.java
+++ trunk/admin/Java/login/bentley/exor/log/IMSLogger.java
@@ -1,12 +1,12 @@
 /**
  *	PVCS Identifiers :-
  *
- *		PVCS id          : $Header:   //new_vm_latest/archives/nm3/admin/Java/login/bentley/exor/log/IMSLogger.java-arc   1.0   Feb 27 2017 07:04:54   Upendra.Hukeri  $
+ *		PVCS id          : $Header:   //new_vm_latest/archives/nm3/admin/Java/login/bentley/exor/log/IMSLogger.java-arc   1.1   May 30 2017 13:38:34   Upendra.Hukeri  $
  *		Module Name      : $Workfile:   IMSLogger.java  $
  *		Author			 : $Author:   Upendra.Hukeri  $
- *		Date Into PVCS   : $Date:   Feb 27 2017 07:04:54  $
- *		Date Fetched Out : $Modtime:   Feb 20 2017 10:19:38  $
- *		PVCS Version     : $Revision:   1.0  $
+ *		Date Into PVCS   : $Date:   May 30 2017 13:38:34  $
+ *		Date Fetched Out : $Modtime:   May 30 2017 10:12:52  $
+ *		PVCS Version     : $Revision:   1.1  $
  *
  *	
  *
@@ -20,17 +20,21 @@ package bentley.exor.log;
 
 import org.apache.log4j.Logger;
 
+import org.owasp.esapi.ESAPI;
+
 public class IMSLogger {
 	final static Logger logger = Logger.getLogger(IMSLogger.class);
 	
-	private final static int DEBUG	= 6;
-	private final static int INFO	= 5;
-	private final static int TRACE	= 4;
-	private final static int WARN	= 3;
-	private final static int FATAL	= 2;
-	private final static int ERROR	= 1;
+	public final static int DEBUG	= 6;
+	public final static int INFO	= 5;
+	public final static int TRACE	= 4;
+	public final static int WARN	= 3;
+	public final static int FATAL	= 2;
+	public final static int ERROR	= 1;
 	
-	public static void log(String message, int logLevel) {
+	public static void log(String logMessage, int logLevel) {
+		String message = removeCRLF(logMessage);
+		
 		switch(logLevel) {
 			case DEBUG:
 				if(logger.isDebugEnabled()) {
@@ -61,6 +65,24 @@ public class IMSLogger {
 			case ERROR:
 				logger.error(message);
 				break;
+			
+			default:
+				logger.error("Wrong logging level. Use - DEBUG | INFO | TRACE | WARN | FATAL | ERROR.");
 		}
+	}
+	
+	private static String removeCRLF(String message) {
+		// ensure no CRLF injection into logs for forging records
+		String clean = message.replace( '\n', '_' ).replace( '\r', '_' );
+		
+		if ( ESAPI.securityConfiguration().getLogEncodingRequired() ) {
+			clean = ESAPI.encoder().encodeForHTML(message);
+			
+			if (!message.equals(clean)) {
+				clean += " (Encoded)";
+			}
+		}
+		
+		return clean;
 	}
 }
