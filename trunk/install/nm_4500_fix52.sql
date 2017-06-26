7b778d778d766bdec31817d948e2fa2a10da986f
Chris.Baugh <Chris.Baugh@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2017-06-26 06:17:28 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/Core@10576 17c802ae-cbca-534c-9e8e-6d19a4926c45

---
+++ trunk/install/nm_4500_fix52.sql
@@ -0,0 +1,117 @@
+----------------------------------------------------------------------------------------------------
+--   PVCS Identifiers :-
+--
+--       PVCS id          : $Header:   //new_vm_latest/archives/nm3/install/nm_4500_fix52.sql-arc   1.0   Jun 26 2017 07:17:28   Chris.Baugh  $
+--       Module Name      : $Workfile:   nm_4500_fix52.sql  $ 
+--       Date into PVCS   : $Date:   Jun 26 2017 07:17:28  $
+--       Date fetched Out : $Modtime:   Jun 26 2017 07:05:38  $
+--       Version     	  : $Revision:   1.0  $
+--
+----------------------------------------------------------------------------------------------------
+--   Copyright (c) 2015 Bentley Systems Incorporated. All rights reserved.
+----------------------------------------------------------------------------------------------------
+--
+SET ECHO OFF
+SET LINESIZE 120
+SET HEADING OFF
+SET FEEDBACK OFF
+--
+-- Grab date/time to append to log file name
+--
+UNDEFINE log_extension
+COL      log_extension NEW_VALUE log_extension NOPRINT
+SET TERM OFF
+SELECT  TO_CHAR(SYSDATE, 'DDMONYYYY_HH24MISS') || '.LOG' log_extension FROM DUAL
+/
+SET TERM ON
+--
+--------------------------------------------------------------------------------
+--
+-- Spool to Logfile
+--
+DEFINE logfile1='nm_4500_fix52_&log_extension'
+spool &logfile1
+--
+--------------------------------------------------------------------------------
+--
+SELECT 'Fix Date ' || TO_CHAR(SYSDATE, 'DD-MON-YYYY HH24:MI:SS') FROM DUAL;
+
+SELECT 'Install Running on ' || LOWER(USER || '@' || instance_name || '.' || host_name) || ' - DB ver : ' || version
+FROM v$instance;
+--
+SELECT 'Current version of ' || hpr_product || ' ' || hpr_version
+FROM hig_products
+WHERE hpr_product IN ('HIG', 'NET');
+--
+WHENEVER SQLERROR EXIT
+--
+BEGIN
+ --
+ -- Check that the user isn't SYS or SYSTEM
+ --
+ IF USER IN ('SYS', 'SYSTEM')
+ THEN
+	RAISE_APPLICATION_ERROR(-20000, 'You cannot install this product as ' || USER);
+ END IF;
+ --
+ -- Check that NET has been installed @ v4.5.0.0
+ --
+ hig2.product_exists_at_version (p_product        => 'NET'
+                                ,p_VERSION        => '4.5.0.0'
+                                );
+
+END;
+/
+WHENEVER SQLERROR CONTINUE
+--
+--------------------------------------------------------------------------------
+-- Package Bodies
+--------------------------------------------------------------------------------
+--
+SET TERM ON 
+PROMPT creating Package Body nm3rsc.pkw
+SET TERM OFF
+--
+SET FEEDBACK ON
+START nm3rsc.pkw
+SET FEEDBACK OFF
+
+SET TERM ON 
+PROMPT creating Package Body nm3wrap.pkw
+SET TERM OFF
+--
+SET FEEDBACK ON
+START nm3wrap.pkw
+SET FEEDBACK OFF
+
+--
+--------------------------------------------------------------------------------
+-- Update hig_upgrades with fix ID
+--------------------------------------------------------------------------------
+--
+BEGIN
+	--
+	hig2.upgrade(p_product        => 'NET'
+				      ,p_upgrade_script => 'log_nm_4500_fix52.sql'
+				      ,p_remarks        => 'NET 4500 FIX 52 (Build 1)'
+				      ,p_to_version     => NULL
+				      );
+	--
+	COMMIT;
+	--
+EXCEPTION 
+	WHEN OTHERS THEN
+	--
+		NULL;
+	--
+END;
+/
+--
+SPOOL OFF
+--
+EXIT
+--
+---------------------------------------------------------------------------------------------------
+--                        ****************   END OF SCRIPT   *******************
+---------------------------------------------------------------------------------------------------
+--
