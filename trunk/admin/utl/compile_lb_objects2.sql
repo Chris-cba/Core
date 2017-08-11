763f8f10d690067db7c18ccb7f752ebe09352836
Rob.Coupe <Rob.Coupe@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2017-08-11 13:32:54 +0000

    Mods for 4.7.0.3 - include types and ensure type and package body are compiled by appropriate statement.
    git-svn-id: svn://127.0.0.1/Core@10642 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/utl/compile_lb_objects2.sql
+++ trunk/admin/utl/compile_lb_objects2.sql
@@ -1,10 +1,10 @@
    --   PVCS Identifiers :-
    --
-   --       pvcsid           : $Header:   //new_vm_latest/archives/lb/admin/utl/compile_lb_objects2.sql-arc   1.0   Oct 12 2015 16:29:26   Rob.Coupe  $
+   --       pvcsid           : $Header:   //new_vm_latest/archives/lb/admin/utl/compile_lb_objects2.sql-arc   1.1   Aug 11 2017 14:32:54   Rob.Coupe  $
    --       Module Name      : $Workfile:   compile_lb_objects2.sql  $
-   --       Date into PVCS   : $Date:   Oct 12 2015 16:29:26  $
-   --       Date fetched Out : $Modtime:   Oct 08 2015 08:59:44  $
-   --       PVCS Version     : $Revision:   1.0  $
+   --       Date into PVCS   : $Date:   Aug 11 2017 14:32:54  $
+   --       Date fetched Out : $Modtime:   Aug 11 2017 13:35:40  $
+   --       PVCS Version     : $Revision:   1.1  $
    --
    --   Author : R.A. Coupe
    --
@@ -21,22 +21,28 @@ declare
   l_object_tab  object_tab;
 --
 begin
+  dbms_output.enable(buffer_size => NULL);
+  
   select o.* 
   bulk collect into l_object_tab
   from user_objects o, lb_dependency_order d
   where  d.object_name = o.object_name
   and d.object_type = o.object_type
-  and o.object_type != 'TABLE';
+  and o.object_type not in ('TABLE', 'SEQUENCE')
+  order by d.DEP_SEQUENCE;
 --
   for i in 1..l_object_tab.count loop
+  dbms_output.put_line('Compiling '||l_object_tab(i).object_type||' '||l_object_tab(i).object_name);
     begin
-      if l_object_tab(i).object_type in ('PACKAGE BODY') then
-        execute immediate 'alter package '||l_object_tab(i).object_name||' compile body';
+      if l_object_tab(i).object_type = 'PACKAGE BODY' then
+        execute immediate 'alter PACKAGE '||l_object_tab(i).object_name||' compile body'; 
+      elsif l_object_tab(i).object_type = 'TYPE BODY' then     
+        execute immediate 'alter TYPE '||l_object_tab(i).object_name||' compile body'; 
       else
         execute immediate 'alter '||l_object_tab(i).object_type||' '||l_object_tab(i).object_name||' compile ';
       end if;
     exception
-      when others then dbms_output.put_line('Failure on '||l_object_tab(i).object_type||','||l_object_tab(i).object_name );
+      when others then dbms_output.put_line('Failure on '||l_object_tab(i).object_type||','||l_object_tab(i).object_name||' - '||sqlerrm );
     end;
   end loop;
 end;   
