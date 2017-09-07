25253008c87651a9a5b0a11bc81df45996bce3b2
Upendra.Hukeri <Upendra.Hukeri@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2017-09-07 13:40:38 +0000

    svnimporter: adding tags to revision 10710
    git-svn-id: svn://127.0.0.1/Core@10711 17c802ae-cbca-534c-9e8e-6d19a4926c45

---
+++ tags/core_fix_53/admin/Java/login/bentley/exor/log/IMSLogger.java
@@ -0,0 +1,88 @@
+/**
+ *	PVCS Identifiers :-
+ *
+ *		PVCS id          : $Header:   //new_vm_latest/archives/nm3/admin/Java/login/bentley/exor/log/IMSLogger.java-arc   1.2   Sep 07 2017 14:40:38   Upendra.Hukeri  $
+ *		Module Name      : $Workfile:   IMSLogger.java  $
+ *		Author			 : $Author:   Upendra.Hukeri  $
+ *		Date Into PVCS   : $Date:   Sep 07 2017 14:40:38  $
+ *		Date Fetched Out : $Modtime:   Sep 07 2017 14:00:00  $
+ *		PVCS Version     : $Revision:   1.2  $
+ *
+ *	
+ *
+ ****************************************************************************************************
+ *	  Copyright (c) 2017 Bentley Systems Incorporated.  All rights reserved.
+ ****************************************************************************************************
+ *
+ */
+
+package bentley.exor.log;
+
+import org.apache.log4j.Logger;
+
+import org.owasp.esapi.ESAPI;
+
+public class IMSLogger {
+	final static Logger logger = Logger.getLogger(IMSLogger.class);
+	
+	public final static int DEBUG	= 6;
+	public final static int INFO	= 5;
+	public final static int TRACE	= 4;
+	public final static int WARN	= 3;
+	public final static int FATAL	= 2;
+	public final static int ERROR	= 1;
+	
+	public static void log(String logMessage, int logLevel) {
+		String message = removeCRLF(logMessage);
+		
+		switch(logLevel) {
+			case DEBUG:
+				if(logger.isDebugEnabled()) {
+					logger.debug(message);
+				}
+				break;
+				
+			case INFO:
+				if(logger.isInfoEnabled()) {
+					logger.info(message);
+				}
+				break;
+			
+			case TRACE:
+				if(logger.isTraceEnabled()) {
+					logger.trace(message);
+				}
+				break;
+			
+			case WARN:
+				logger.warn(message);
+				break;
+				
+			case FATAL:
+				logger.fatal(message);
+				break;
+				
+			case ERROR:
+				logger.error(message);
+				break;
+			
+			default:
+				logger.error("Wrong logging level. Use - DEBUG | INFO | TRACE | WARN | FATAL | ERROR.");
+		}
+	}
+	
+	private static String removeCRLF(String message) {
+		// ensure no CRLF injection into logs for forging records
+		String clean = message.replace('\n', '_').replace('\r', '_');
+		
+		if ( ESAPI.securityConfiguration().getBooleanProp("Logger.LogEncodingRequired") ) {
+			clean = ESAPI.encoder().encodeForHTML(message);
+			
+			if (!message.equals(clean)) {
+				clean += " (Encoded)";
+			}
+		}
+		
+		return clean;
+	}
+}
