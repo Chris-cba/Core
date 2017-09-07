4954c9d9172a2c89f0d01f49be536cd4220357d6
Upendra.Hukeri <Upendra.Hukeri@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2017-09-07 13:40:10 +0000

    core fix 53
    git-svn-id: svn://127.0.0.1/Core@10706 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/Java/login/bentley/exor/pwdenc/MachineInfo.java
+++ trunk/admin/Java/login/bentley/exor/pwdenc/MachineInfo.java
@@ -1,12 +1,12 @@
 /**
  *	PVCS Identifiers :-
  *
- *		PVCS id          : $Header:   //new_vm_latest/archives/nm3/admin/Java/login/bentley/exor/pwdenc/MachineInfo.java-arc   1.0   Feb 27 2017 07:05:52   Upendra.Hukeri  $
+ *		PVCS id          : $Header:   //new_vm_latest/archives/nm3/admin/Java/login/bentley/exor/pwdenc/MachineInfo.java-arc   1.1   Sep 07 2017 14:40:10   Upendra.Hukeri  $
  *		Module Name      : $Workfile:   MachineInfo.java  $
  *		Author			 : $Author:   Upendra.Hukeri  $
- *		Date Into PVCS   : $Date:   Feb 27 2017 07:05:52  $
- *		Date Fetched Out : $Modtime:   Feb 16 2017 07:00:00  $
- *		PVCS Version     : $Revision:   1.0  $
+ *		Date Into PVCS   : $Date:   Sep 07 2017 14:40:10  $
+ *		Date Fetched Out : $Modtime:   Sep 04 2017 15:52:40  $
+ *		PVCS Version     : $Revision:   1.1  $
  *
  *	
  *
@@ -20,12 +20,50 @@ package bentley.exor.pwdenc;
 
 import bentley.exor.ExorDebugger;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class MachineInfo {
 	public static String getMachineName() {
 		try {
-			java.net.InetAddress localMachine = java.net.InetAddress.getLocalHost();
+			StringBuilder ipAddresses = new StringBuilder();
+			String ipconfig = null;
 			
-			return localMachine.getCanonicalHostName();
+			Runtime run = Runtime.getRuntime();
+			Process proc = run.exec("hostname");
+			BufferedReader in = null;
+			
+			try {
+				in = new BufferedReader(new InputStreamReader(proc.getInputStream(), "UTF-8"));
+				
+				ipconfig = in.readLine();
+				
+				ipAddresses.append(ipconfig);
+				
+				proc.waitFor();
+				in.close();
+				
+				proc = run.exec("ipconfig");
+				in = new BufferedReader(new InputStreamReader(proc.getInputStream(), "UTF-8"));
+				
+				while ((ipconfig = in.readLine()) != null) {
+					if(ipconfig.indexOf("IPv4 Address") > -1) {
+						ipAddresses.append(':').append(ipconfig.substring(ipconfig.indexOf(':') + 2));
+					}
+				}
+				
+				proc.waitFor();
+			} finally {
+				if(in != null) {
+					try {	
+						in.close();
+					} catch(IOException ioe) {
+					}
+				}
+			}
+			
+			return ipAddresses.toString();
 		} catch(Exception e) {
 			return "Unable to get machine name";
 		}
