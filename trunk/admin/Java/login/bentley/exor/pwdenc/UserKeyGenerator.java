5d1d3da7f7fdd9138e1a50df645696baeca9b54e
Upendra.Hukeri <Upendra.Hukeri@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2017-09-07 13:40:18 +0000

    core fix 53
    git-svn-id: svn://127.0.0.1/Core@10708 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/Java/login/bentley/exor/pwdenc/UserKeyGenerator.java
+++ trunk/admin/Java/login/bentley/exor/pwdenc/UserKeyGenerator.java
@@ -1,12 +1,12 @@
 /**
  *	PVCS Identifiers :-
  *
- *		PVCS id          : $Header:   //new_vm_latest/archives/nm3/admin/Java/login/bentley/exor/pwdenc/UserKeyGenerator.java-arc   1.0   Feb 27 2017 07:05:52   Upendra.Hukeri  $
+ *		PVCS id          : $Header:   //new_vm_latest/archives/nm3/admin/Java/login/bentley/exor/pwdenc/UserKeyGenerator.java-arc   1.1   Sep 07 2017 14:40:18   Upendra.Hukeri  $
  *		Module Name      : $Workfile:   UserKeyGenerator.java  $
  *		Author			 : $Author:   Upendra.Hukeri  $
- *		Date Into PVCS   : $Date:   Feb 27 2017 07:05:52  $
- *		Date Fetched Out : $Modtime:   Feb 06 2017 08:57:12  $
- *		PVCS Version     : $Revision:   1.0  $
+ *		Date Into PVCS   : $Date:   Sep 07 2017 14:40:18  $
+ *		Date Fetched Out : $Modtime:   Sep 04 2017 14:47:30  $
+ *		PVCS Version     : $Revision:   1.1  $
  *
  *	
  *
@@ -18,14 +18,21 @@
 
 package bentley.exor.pwdenc;
 
+import java.io.UnsupportedEncodingException;
+
 import java.security.SecureRandom;
  
 public class UserKeyGenerator {
+	private static final SecureRandom random = new SecureRandom();
+	
     public static String generateRandomString(final int randomStrlen) {
-        SecureRandom random = new SecureRandom();
-		byte[] values = new byte[randomStrlen];
-		random.nextBytes(values);
-		
-		return values.toString();
+		try {
+			byte[] values = new byte[randomStrlen];
+			random.nextBytes(values);
+			
+			return new String(values, "UTF-8");
+		} catch(UnsupportedEncodingException uee) {
+			return "ORA-20099: " + uee.getMessage();
+		}
     }
 }
