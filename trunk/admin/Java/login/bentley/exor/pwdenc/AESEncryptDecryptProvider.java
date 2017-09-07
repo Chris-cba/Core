a430b090933fbc0c17bd1adb64d95daa643c0a19
Upendra.Hukeri <Upendra.Hukeri@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2017-09-07 13:39:10 +0000

    core fix 53
    git-svn-id: svn://127.0.0.1/Core@10701 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/Java/login/bentley/exor/pwdenc/AESEncryptDecryptProvider.java
+++ trunk/admin/Java/login/bentley/exor/pwdenc/AESEncryptDecryptProvider.java
@@ -1,12 +1,12 @@
 /**
  *	PVCS Identifiers :-
  *
- *		PVCS id          : $Header:   //new_vm_latest/archives/nm3/admin/Java/login/bentley/exor/pwdenc/AESEncryptDecryptProvider.java-arc   1.0   Feb 27 2017 07:05:52   Upendra.Hukeri  $
+ *		PVCS id          : $Header:   //new_vm_latest/archives/nm3/admin/Java/login/bentley/exor/pwdenc/AESEncryptDecryptProvider.java-arc   1.1   Sep 07 2017 14:39:10   Upendra.Hukeri  $
  *		Module Name      : $Workfile:   AESEncryptDecryptProvider.java  $
  *		Author			 : $Author:   Upendra.Hukeri  $
- *		Date Into PVCS   : $Date:   Feb 27 2017 07:05:52  $
- *		Date Fetched Out : $Modtime:   Feb 20 2017 10:21:28  $
- *		PVCS Version     : $Revision:   1.0  $
+ *		Date Into PVCS   : $Date:   Sep 07 2017 14:39:10  $
+ *		Date Fetched Out : $Modtime:   Sep 04 2017 15:55:26  $
+ *		PVCS Version     : $Revision:   1.1  $
  *
  *	
  *
@@ -21,45 +21,58 @@ package bentley.exor.pwdenc;
 import bentley.exor.ExorDebugger;
 
 import java.security.Key;
+import java.security.SecureRandom;
+import java.security.Security;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 import javax.crypto.spec.IvParameterSpec;
 
-import sun.misc.*;
+import org.apache.commons.codec.binary.Base64;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.util.Arrays;
 
 public class AESEncryptDecryptProvider {
-    public static String encrypt(String inputText, String key) {
+	private static final SecureRandom random = new SecureRandom();
+	
+	static {
+		Security.addProvider(new BouncyCastleProvider());
+	}
+	
+	public static String encryptString(String plaintext, String key) {
 		try {
-			// Create key and cipher
-			Key aesKey = new SecretKeySpec(key.getBytes(), "AES");
-			Cipher cipher = Cipher.getInstance("AES");
-			
-			// encrypt the inputText
-			cipher.init(Cipher.ENCRYPT_MODE, aesKey);
-			byte[] encrypted = cipher.doFinal(inputText.getBytes());
-			return new BASE64Encoder().encode(encrypted);
-		}catch(Exception e) {
-			e.printStackTrace();
-			
+			return Base64.encodeBase64String(encrypt(plaintext, key));
+		} catch(Exception e) {
 			return "ORA-20099: " + e.getMessage();
 		}
+	}
+	
+	public static String decryptString(String encryptedText, String key) {
+		try {
+			return decrypt(Base64.decodeBase64(encryptedText), key);
+		} catch(Exception e) {
+			return "ORA-20099: " + e.getMessage();
+		}
+	}
+	
+    private static byte[] encrypt(String inputText, String key) throws Exception {
+		Key aesKey = new SecretKeySpec(key.getBytes("UTF-8"), "AES/GCM/NoPadding");
+		Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding", "BC");
+		
+		byte [] ivBytes = new byte[12];
+        random.nextBytes(ivBytes);
+		
+		cipher.init(Cipher.ENCRYPT_MODE, aesKey, new IvParameterSpec(ivBytes), random);
+		return Arrays.concatenate(ivBytes, cipher.doFinal(inputText.getBytes("UTF-8")));
     }
 	
-	public static String decrypt(String encryptedText, String key) {
-		try {
-			// Create key and cipher
-			Key aesKey = new SecretKeySpec(key.getBytes(), "AES");
-			Cipher cipher = Cipher.getInstance("AES");
-			
-			// decrypt the encryptedText
-			cipher.init(Cipher.DECRYPT_MODE, aesKey);
-			String decrypted = new String(cipher.doFinal(new BASE64Decoder().decodeBuffer(encryptedText)));
-			return decrypted;
-		}catch(Exception e) {
-			e.printStackTrace();
-			
-			return "ORA-20099: " + e.getMessage();
-		}
+	private static String decrypt(byte[] encrypted, String key) throws Exception {
+		Key aesKey = new SecretKeySpec(key.getBytes("UTF-8"), "AES/GCM/NoPadding");
+		Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding", "BC");
+		
+		cipher.init(Cipher.DECRYPT_MODE, aesKey, new IvParameterSpec(Arrays.copyOfRange(encrypted, 0, 12)), random);
+		
+		return new String(cipher.doFinal(Arrays.copyOfRange(encrypted, 12, encrypted.length)), "UTF-8");
     }
 }
