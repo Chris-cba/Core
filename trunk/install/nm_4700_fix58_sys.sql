0662236759c9e3200553324ad632a883926618fa
Chris.Baugh <Chris.Baugh@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2017-12-18 09:37:52 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/Core@10956 17c802ae-cbca-534c-9e8e-6d19a4926c45

---
+++ trunk/install/nm_4700_fix58_sys.sql
@@ -0,0 +1,108 @@
+----------------------------------------------------------------------------------------------------
+--   PVCS Identifiers :-
+--
+--       PVCS id          : $Header:   //new_vm_latest/archives/nm3/install/nm_4700_fix58_sys.sql-arc   1.0   Dec 18 2017 09:37:52   Chris.Baugh  $
+--       Module Name      : $Workfile:   nm_4700_fix58_sys.sql  $ 
+--       Date into PVCS   : $Date:   Dec 18 2017 09:37:52  $
+--       Date fetched Out : $Modtime:   Dec 18 2017 09:34:10  $
+--       Version     	  : $Revision:   1.0  $
+--
+----------------------------------------------------------------------------------------------------
+--   Copyright (c) 2016 Bentley Systems Incorporated. All rights reserved.
+----------------------------------------------------------------------------------------------------
+--
+SET ECHO OFF
+SET LINESIZE 120
+SET HEADING OFF
+SET FEEDBACK OFF
+--
+-- Grab date/time to append to log file name
+--
+UNDEFINE log_extension
+COL      log_extension NEW_VALUE log_extension NOPRINT
+SET TERM OFF
+SELECT  TO_CHAR(SYSDATE, 'DDMONYYYY_HH24MISS') || '.LOG' log_extension FROM DUAL
+/
+SET TERM ON
+--
+--------------------------------------------------------------------------------
+--
+-- Spool to Logfile
+--
+DEFINE logfile1='nm_4700_fix58_sys_&log_extension'
+spool &logfile1
+--
+--------------------------------------------------------------------------------
+--
+SELECT 'Fix Date ' || TO_CHAR(SYSDATE, 'DD-MON-YYYY HH24:MI:SS') FROM DUAL;
+
+SELECT 'Install Running on ' || LOWER(USER || '@' || instance_name || '.' || host_name) || ' - DB ver : ' || version
+FROM v$instance;
+--
+BEGIN
+ --
+ -- Ensure this script is being run as SYS User
+ --
+ IF USER != 'SYS'
+ THEN
+	RAISE_APPLICATION_ERROR(-20000, 'This script must be run as SYS User');
+ END IF;
+
+END;
+/
+ 
+WHENEVER SQLERROR CONTINUE
+--
+--
+--------------------------------------------------------------------------------
+-- Create public synonyms for SYS objects
+--------------------------------------------------------------------------------
+--
+
+SET TERM ON 
+PROMPT Creating Public Synonyms
+SET TERM OFF
+--
+DECLARE
+ synonym_exists exception;
+ pragma exception_init (synonym_exists,-955);
+BEGIN
+  EXECUTE IMMEDIATE 'CREATE PUBLIC SYNONYM proxy_users FOR proxy_users';
+EXCEPTION
+  WHEN synonym_exists THEN
+    Null;
+   WHEN others THEN
+     RAISE;
+END;
+/
+--
+--------------------------------------------------------------------------------
+-- Grant privileges for SYS objects
+--------------------------------------------------------------------------------
+--
+SET TERM ON 
+PROMPT Granting Privileges
+SET TERM OFF
+
+DECLARE
+  username hig_users.hus_username%TYPE;
+BEGIN
+  SELECT hus_username 
+  INTO username
+  FROM hig_users 
+  WHERE hus_is_hig_owner_flag = 'Y';
+
+  EXECUTE IMMEDIATE 'GRANT EXECUTE ON DBMS_CRYPTO TO ' || username;
+  EXECUTE IMMEDIATE 'GRANT SELECT ON PROXY_USERS TO HIG_USER';
+
+END;
+/
+--
+SPOOL OFF
+--
+EXIT
+--
+---------------------------------------------------------------------------------------------------
+--                        ****************   END OF SCRIPT   *******************
+---------------------------------------------------------------------------------------------------
+--
