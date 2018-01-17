d5b6d03fbf4f31ce525a05fd1960ac1d851f9ef4
Upendra.Hukeri <Upendra.Hukeri@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2018-01-17 09:03:16 +0000

    Updated script to take care of multiple installations..
    git-svn-id: svn://127.0.0.1/Core@10976 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/Java/shapefile/install/install_sdeutil.sql
+++ trunk/admin/Java/shapefile/install/install_sdeutil.sql
@@ -3,29 +3,30 @@
 --
 --   PVCS Identifiers :-
 --
---       sccsid           : $Header:   //new_vm_latest/archives/nm3/admin/Java/shapefile/install/install_sdeutil.sql-arc   1.0   Nov 29 2017 09:45:54   Upendra.Hukeri  $
+--       sccsid           : $Header:   //new_vm_latest/archives/nm3/admin/Java/shapefile/install/install_sdeutil.sql-arc   1.1   Jan 17 2018 09:03:16   Upendra.Hukeri  $
 --       Module Name      : $Workfile:   install_sdeutil.sql  $
---       Date into PVCS   : $Date:   Nov 29 2017 09:45:54  $
---       Date fetched Out : $Modtime:   Nov 29 2017 09:42:00  $
---       PVCS Version     : $Revision:   1.0  $
+--       Date into PVCS   : $Date:   Jan 17 2018 09:03:16  $
+--       Date fetched Out : $Modtime:   Jan 17 2018 09:02:24  $
+--       PVCS Version     : $Revision:   1.1  $
 --
 --   Author : Upendra Hukeri
 --
 ---------------------------------------------------------------------------------------------------
--- Copyright (c) 2017 Bentley Systems Incorporated. All rights reserved.
+-- Copyright (c) 2018 Bentley Systems Incorporated. All rights reserved.
 ---------------------------------------------------------------------------------------------------
 --
 SET ECHO OFF
 SET LINESIZE 120
 SET HEADING OFF
 SET FEEDBACK OFF
+SET SERVEROUTPUT ON
 --
 -- Grab date/time to append to log file name
 --
 UNDEFINE sde_log_extension
 COL      sde_log_extension NEW_VALUE sde_log_extension NOPRINT
 SET TERM OFF
-SELECT TO_CHAR(SYSDATE, 'DDMONYYYY_HH24MISS') || '.LOG' sde_log_extension FROM DUAL
+SELECT TO_CHAR(SYSDATE, 'ddmmyyyyhh24miss') || '.log' sde_log_extension FROM DUAL
 /
 SET TERM ON
 --
@@ -40,12 +41,48 @@ spool &sde_logfile
 -- TYPES
 --------------------------------------------------------------------------------
 --
+DECLARE 
+	CURSOR c1 
+	IS 
+	SELECT 1 
+	FROM   user_objects 
+	WHERE  object_name = 'SDE_VARCHAR_2D_ARRAY';
+	--
+	v_temp1 NUMBER := NULL;
+	--
+	CURSOR c2 
+	IS 
+	SELECT 1 
+	FROM   user_objects 
+	WHERE  object_name = 'SDE_VARCHAR_ARRAY';
+	--
+	v_temp2 NUMBER := NULL;
+BEGIN
+	OPEN  c1;
+	FETCH c1 INTO v_temp1;
+	CLOSE c1;
+	--
+	IF v_temp1 IS NOT NULL THEN 
+		EXECUTE IMMEDIATE 'DROP TYPE SDE_VARCHAR_2D_ARRAY';
+	END IF;
+	--
+	OPEN  c2;
+	FETCH c2 INTO v_temp2;
+	CLOSE c2;
+	--
+	IF v_temp2 IS NOT NULL THEN 
+		EXECUTE IMMEDIATE 'DROP TYPE SDE_VARCHAR_ARRAY';
+	END IF;
+END;
+/
+--
 SET TERM ON
 PROMPT sde_varchar_array
 SET TERM OFF
 --
 SET FEEDBACK ON
 START sde_varchar_array.tyh
+SET FEEDBACK OFF
 --
 SET TERM ON
 PROMPT sde_varchar_2d_array
@@ -53,6 +90,7 @@ SET TERM OFF
 --
 SET FEEDBACK ON
 START sde_varchar_2d_array.tyh
+SET FEEDBACK OFF
 --
 --------------------------------------------------------------------------------
 -- ROLE
@@ -62,9 +100,20 @@ SET TERM ON
 PROMPT Creating Role SDE_USER
 SET TERM OFF
 --
-SET FEEDBACK ON
-CREATE ROLE SDE_USER;
-SET FEEDBACK OFF
+BEGIN
+	EXECUTE IMMEDIATE 'CREATE ROLE SDE_USER';
+	--
+	dbms_output.put_line(CHR(10) || 'Role created.' || CHR(10));
+EXCEPTION
+	WHEN OTHERS THEN
+		-- ORA-01921: If The role name exists, ignore the error.
+		IF SQLCODE = -01921 THEN
+			dbms_output.put_line(CHR(10) || 'Role already exists.' || CHR(10));
+		ELSE
+			RAISE;
+		END IF;
+END;
+/
 --
 --------------------------------------------------------------------------------
 -- PACKAGE HEADER
