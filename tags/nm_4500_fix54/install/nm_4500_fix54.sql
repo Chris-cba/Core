88a91bb4268213dd2a257c214f8b6b2346a3eca7
Chris.Baugh <Chris.Baugh@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2018-01-17 15:00:16 +0000

    svnimporter: adding tags to revision 10979
    git-svn-id: svn://127.0.0.1/Core@10980 17c802ae-cbca-534c-9e8e-6d19a4926c45

---
+++ tags/nm_4500_fix54/install/nm_4500_fix54.sql
@@ -0,0 +1,168 @@
+----------------------------------------------------------------------------------------------------
+--   PVCS Identifiers :-
+--
+--       PVCS id          : $Header:   //new_vm_latest/archives/nm3/install/nm_4500_fix54.sql-arc   1.0   Jan 17 2018 15:00:16   Chris.Baugh  $
+--       Module Name      : $Workfile:   nm_4500_fix54.sql  $ 
+--       Date into PVCS   : $Date:   Jan 17 2018 15:00:16  $
+--       Date fetched Out : $Modtime:   Jan 16 2018 12:00:14  $
+--       Version     	  : $Revision:   1.0  $
+--
+----------------------------------------------------------------------------------------------------
+--   Copyright (c) 2015 Bentley Systems Incorporated. All rights reserved.
+----------------------------------------------------------------------------------------------------
+--
+SET ECHO OFF
+SET LINESIZE 120
+SET HEADING OFF
+SET FEEDBACK OFF
+--
+-- Grab date/time to append to log file name
+--
+UNDEFINE log_extension
+COL      log_extension NEW_VALUE log_extension NOPRINT
+SET TERM OFF
+SELECT  TO_CHAR(SYSDATE, 'DDMONYYYY_HH24MISS') || '.LOG' log_extension FROM DUAL
+/
+SET TERM ON
+--
+--------------------------------------------------------------------------------
+--
+-- Spool to Logfile
+--
+DEFINE logfile1='nm_4500_fix54_&log_extension'
+spool &logfile1
+--
+--------------------------------------------------------------------------------
+--
+SELECT 'Fix Date ' || TO_CHAR(SYSDATE, 'DD-MON-YYYY HH24:MI:SS') FROM DUAL;
+
+SELECT 'Install Running on ' || LOWER(USER || '@' || instance_name || '.' || host_name) || ' - DB ver : ' || version
+FROM v$instance;
+--
+SELECT 'Current version of ' || hpr_product || ' ' || hpr_version
+FROM hig_products
+WHERE hpr_product IN ('HIG', 'NET');
+--
+WHENEVER SQLERROR EXIT
+--
+BEGIN
+ --
+ -- Check that the user isn't SYS or SYSTEM
+ --
+ IF USER IN ('SYS', 'SYSTEM')
+ THEN
+	RAISE_APPLICATION_ERROR(-20000, 'You cannot install this product as ' || USER);
+ END IF;
+ --
+ -- Check that NET has been installed @ v4.5.0.0
+ --
+ hig2.product_exists_at_version (p_product        => 'NET'
+                                ,p_VERSION        => '4.5.0.0'
+                                );
+
+END;
+/
+WHENEVER SQLERROR CONTINUE
+--
+--------------------------------------------------------------------------------
+-- Package Bodies
+--------------------------------------------------------------------------------
+--
+SET TERM ON 
+PROMPT creating Package Body nm3mail.pkw
+SET TERM OFF
+--
+SET FEEDBACK ON
+START nm3mail.pkw
+SET FEEDBACK OFF
+
+--
+--------------------------------------------------------------------------------
+-- HIG_OPTION_LIST
+--------------------------------------------------------------------------------
+--
+SET TERM ON 
+PROMPT Adding new product option AUTHMAIL into HIG_OPTION_LIST
+SET TERM OFF
+--
+INSERT INTO HIG_OPTION_LIST 
+           (HOL_ID,
+            HOL_PRODUCT,
+            HOL_NAME,
+            HOL_REMARKS,
+            HOL_DOMAIN,
+            HOL_DATATYPE,
+            HOL_MIXED_CASE,
+            HOL_USER_OPTION,
+            HOL_MAX_LENGTH
+            )
+SELECT 'AUTHMAIL'
+      ,'NET'
+      ,'Email Authentication'
+      ,'Defines whether SMTP Server requires Username/Password authentication.'
+      ,'Y_OR_N'
+      ,'VARCHAR2'
+      ,'N'
+      ,'N'
+      ,1
+FROM DUAL
+WHERE NOT EXISTS (SELECT 1
+                    FROM HIG_OPTION_LIST
+                   WHERE HOL_ID = 'AUTHMAIL'
+                 )
+/
+--
+--
+--
+--------------------------------------------------------------------------------
+-- HIG_OPTION_VALUES
+--------------------------------------------------------------------------------
+--
+SET TERM ON 
+PROMPT Adding option values to HIG_OPTION_VALUES for AUTHMAIL
+SET TERM OFF
+--
+INSERT INTO HIG_OPTION_VALUES
+           (HOV_ID, 
+            HOV_VALUE
+           )  
+SELECT 'AUTHMAIL'
+      ,'N'
+FROM DUAL
+WHERE NOT EXISTS (SELECT 1
+                    FROM HIG_OPTION_VALUES
+                   WHERE HOV_ID = 'AUTHMAIL'
+                 )
+/
+
+--
+--------------------------------------------------------------------------------
+-- Update hig_upgrades with fix ID
+--------------------------------------------------------------------------------
+--
+BEGIN
+	--
+	hig2.upgrade(p_product        => 'NET'
+				      ,p_upgrade_script => 'log_nm_4500_fix54.sql'
+				      ,p_remarks        => 'NET 4500 FIX 54 (Build 1)'
+				      ,p_to_version     => NULL
+				      );
+	--
+	COMMIT;
+	--
+EXCEPTION 
+	WHEN OTHERS THEN
+	--
+		NULL;
+	--
+END;
+/
+--
+SPOOL OFF
+--
+EXIT
+--
+---------------------------------------------------------------------------------------------------
+--                        ****************   END OF SCRIPT   *******************
+---------------------------------------------------------------------------------------------------
+--
