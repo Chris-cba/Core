d0b13181c2156c3cfd57bd12d4a480280d7fbe57
Chris.Baugh <Chris.Baugh@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2018-03-08 16:17:44 +0000

    Added build no
    git-svn-id: svn://127.0.0.1/Core@11050 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/install/log_nm_4700_fix42.sql
+++ trunk/install/log_nm_4700_fix42.sql
@@ -1,11 +1,11 @@
 ----------------------------------------------------------------------------------------------------
 --   PVCS Identifiers :-
 --
---       PVCS id          : $Header:   //new_vm_latest/archives/nm3/install/log_nm_4700_fix42.sql-arc   1.3   Mar 08 2018 16:14:58   Chris.Baugh  $   
+--       PVCS id          : $Header:   //new_vm_latest/archives/nm3/install/log_nm_4700_fix42.sql-arc   1.4   Mar 08 2018 16:17:44   Chris.Baugh  $   
 --       Module Name      : $Workfile:   log_nm_4700_fix42.sql  $ 
---       Date into PVCS   : $Date:   Mar 08 2018 16:14:58  $
---       Date fetched Out : $Modtime:   Mar 08 2018 16:13:54  $
---       Version     	  : $Revision:   1.3  $
+--       Date into PVCS   : $Date:   Mar 08 2018 16:17:44  $
+--       Date fetched Out : $Modtime:   Mar 08 2018 16:17:20  $
+--       Version     	  : $Revision:   1.4  $
 --
 ----------------------------------------------------------------------------------------------------
 --   Copyright (c) 2016 Bentley Systems Incorporated. All rights reserved.
@@ -146,7 +146,7 @@ BEGIN
 	--
 	hig2.upgrade(p_product        => 'NET'
 				,p_upgrade_script => 'log_nm_4700_fix42.sql'
-				,p_remarks        => 'NET 4700 FIX 42 (Build 1)'
+				,p_remarks        => 'NET 4700 FIX 42 (Build 2)'
 				,p_to_version     => NULL
 				);
 	--
