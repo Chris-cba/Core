1883a4f9019a40eb5a97fb5f1ef4d058d4eb4bed
Gaurav.Gaurkar <Gaurav.Gaurkar@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2018-04-18 15:09:30 +0000

    svnimporter: adding tags to revision 11281
    git-svn-id: svn://127.0.0.1/Core@11282 17c802ae-cbca-534c-9e8e-6d19a4926c45

---
+++ tags/security_audit/install/mav_grant.sql
@@ -0,0 +1,17 @@
+--
+--------------------------------------------------------------------------------
+--   PVCS Identifiers :-
+--
+--       sccsid           : $Header:   //new_vm_latest/archives/nm3/install/mav_grant.sql-arc   3.2   Apr 18 2018 16:09:30   Gaurav.Gaurkar  $
+--       Module Name      : $Workfile:   mav_grant.sql  $
+--       Date into PVCS   : $Date:   Apr 18 2018 16:09:30  $
+--       Date fetched Out : $Modtime:   Apr 18 2018 16:02:10  $
+--       PVCS Version     : $Revision:   3.2  $
+--
+--------------------------------------------------------------------------------
+--    Copyright (c) 2018 Bentley Systems Incorporated. All rights reserved.
+-----------------------------------------------------------------------------
+--
+
+GRANT MERGE ANY VIEW TO PUBLIC;
+
