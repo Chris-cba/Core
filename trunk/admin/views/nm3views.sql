d957fb76cb0ac173f1cf2922bc95396004f17d07
Chris.Baugh <Chris.Baugh@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2018-06-01 12:55:26 +0000

    Removed v_geom_on_route, to be applied in post upgrade/install scripts due to dependency on road_sections
    git-svn-id: svn://127.0.0.1/Core@11432 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/views/nm3views.sql
+++ trunk/admin/views/nm3views.sql
@@ -1603,17 +1603,6 @@ start '&run_file'
 ----------------------------------------------------------------------------------------- 
 --
 SET TERM ON 
-PROMPT v_geom_on_route.vw 
-SET TERM OFF
-SET DEFINE ON 
-SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||'&terminator'||'views'||'&terminator'||'v_geom_on_route.vw ' run_file 
-FROM dual 
-/ 
-start '&run_file'
---
------------------------------------------------------------------------------------------ 
---
-SET TERM ON 
 PROMPT v_obj_on_route.vw 
 SET TERM OFF
 SET DEFINE ON 
