99dde8cf57e54a2ed5344b67fde56ace6748a548
Rob.Coupe <Rob.Coupe@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2018-06-22 14:15:12 +0000

    svnimporter: adding tags to revision 11463
    git-svn-id: svn://127.0.0.1/Core@11464 17c802ae-cbca-534c-9e8e-6d19a4926c45

---
+++ tags/exnm04070013en_updt55/admin/views/views.sql
@@ -0,0 +1,24 @@
+--   PVCS Identifiers :-
+--
+--       sccsid           : $Header:   //new_vm_latest/archives/lb/admin/views/views.sql-arc   1.3   Jun 22 2018 15:15:12   Rob.Coupe  $
+--       Module Name      : $Workfile:   views.sql  $ 
+--       Date into PVCS   : $Date:   Jun 22 2018 15:15:12  $
+--       Date fetched Out : $Modtime:   Jun 22 2018 15:14:32  $
+--       PVCS Version     : $Revision:   1.3  $
+--
+----------------------------------------------------------------------------
+--   Copyright (c) 2015 Bentley Systems Incorporated.  All rights reserved.
+----------------------------------------------------------------------------
+--
+
+start .\admin\views\v_lb_path_links.vw
+start .\admin\views\v_lb_directed_path_links.vw
+start .\admin\views\V_LB_PATH_BETWEEN_POINTS.vw
+start .\admin\views\v_nm_inv_on_network.vw
+start .\admin\views\v_lb_type_nw_flags.vw
+start .\admin\views\v_nm_datum_themes.vw
+start .\admin\views\v_nm_element_xsp.vw
+start .\admin\views\v_nm_element_xsp_rvrs.vw
+start .\admin\views\v_nlt_xsps.vw
+start .\admin\views\v_nlt_xsp_rvrs.vw
+start .\admin\views\v_nlt_element_xsps.vw
