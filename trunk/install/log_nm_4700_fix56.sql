c9ee6c7f2a7ec3ccca1be8ff117d873a0ee64d40
Rob.Coupe <Rob.Coupe@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2018-09-25 09:05:04 +0000

    Build 3
    git-svn-id: svn://127.0.0.1/Core@11644 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/install/log_nm_4700_fix56.sql
+++ trunk/install/log_nm_4700_fix56.sql
@@ -2,11 +2,11 @@
 --------------------------------------------------------------------------------
 --   PVCS Identifiers :-
 --
---       sccsid           : $Header:   //new_vm_latest/archives/nm3/install/log_nm_4700_fix56.sql-arc   1.1   Jul 30 2018 16:25:46   Rob.Coupe  $
+--       sccsid           : $Header:   //new_vm_latest/archives/nm3/install/log_nm_4700_fix56.sql-arc   1.2   Sep 25 2018 10:05:04   Rob.Coupe  $
 --       Module Name      : $Workfile:   log_nm_4700_fix56.sql  $
---       Date into PVCS   : $Date:   Jul 30 2018 16:25:46  $
---       Date fetched Out : $Modtime:   Jul 30 2018 16:25:32  $
---       PVCS Version     : $Revision:   1.1  $
+--       Date into PVCS   : $Date:   Sep 25 2018 10:05:04  $
+--       Date fetched Out : $Modtime:   Sep 25 2018 10:04:46  $
+--       PVCS Version     : $Revision:   1.2  $
 --
 --------------------------------------------------------------------------------
 --   Copyright (c) 2015 Bentley Systems Incorporated.  All rights reserved.
@@ -16,7 +16,7 @@ BEGIN
 --
   hig2.upgrade(p_product        => 'NET'
               ,p_upgrade_script => 'log_nm_4700_fix56.sql'
-              ,p_remarks        => 'NET 4700 FIX 56 Build 2'
+              ,p_remarks        => 'NET 4700 FIX 56 Build 3'
               ,p_to_version     => Null);
 --
   commit;
