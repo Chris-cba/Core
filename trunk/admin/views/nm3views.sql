9b9b164ccb5de107fdf353e4582abc591f327bc4
Chris.Baugh <Chris.Baugh@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2018-10-26 12:34:08 +0000

    Added v_contiguity_check
    git-svn-id: svn://127.0.0.1/Core@11676 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/views/nm3views.sql
+++ trunk/admin/views/nm3views.sql
@@ -1676,3 +1676,14 @@ SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||'&terminator'||'views'||'&te
 FROM dual 
 / 
 start '&run_file'
+--
+----------------------------------------------------------------------------------------- 
+--
+SET TERM ON 
+PROMPT v_contiguity_check.vw 
+SET TERM OFF
+SET DEFINE ON 
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||'&terminator'||'views'||'&terminator'||'v_contiguity_check.vw .vw ' run_file 
+FROM dual 
+/ 
+start '&run_file'
