3ccccfd5c7cfad3de01d439f5c29a60ca3cad44d
Chris.Baugh <Chris.Baugh@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2019-03-26 07:52:12 +0000

    added missing "/"
    git-svn-id: svn://127.0.0.1/Core@12059 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/install/rebuild_spatial_date_views.sql
+++ trunk/install/rebuild_spatial_date_views.sql
@@ -1,11 +1,11 @@
 ----------------------------------------------------------------------------------------------------
 --   PVCS Identifiers :-
 --
---       PVCS id          : $Header:   //new_vm_latest/archives/nm3/install/rebuild_spatial_date_views.sql-arc   1.0   Mar 25 2019 13:54:10   Chris.Baugh  $
+--       PVCS id          : $Header:   //new_vm_latest/archives/nm3/install/rebuild_spatial_date_views.sql-arc   1.1   Mar 26 2019 07:52:12   Chris.Baugh  $
 --       Module Name      : $Workfile:   rebuild_spatial_date_views.sql  $ 
---       Date into PVCS   : $Date:   Mar 25 2019 13:54:10  $
---       Date fetched Out : $Modtime:   Mar 25 2019 13:53:08  $
---       Version     	  : $Revision:   1.0  $
+--       Date into PVCS   : $Date:   Mar 26 2019 07:52:12  $
+--       Date fetched Out : $Modtime:   Mar 26 2019 07:51:02  $
+--       Version     	  : $Revision:   1.1  $
 --
 ----------------------------------------------------------------------------------------------------
 --   Copyright (c) 2017 Bentley Systems Incorporated. All rights reserved.
@@ -23,3 +23,4 @@ BEGIN
             'NM_NIT_' || irec.nit_inv_type || '_SDO');
     END LOOP;
 END;
+/
