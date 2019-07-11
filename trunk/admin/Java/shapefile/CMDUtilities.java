0404e84cc9d4deaeedd4784fd85cf151e14d2e70
Upendra.Hukeri <Upendra.Hukeri@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2019-07-11 11:45:50 +0000

    TFS Defect 1031405:[Security] Java class allows execution of any command
    git-svn-id: svn://127.0.0.1/Core@12204 17c802ae-cbca-534c-9e8e-6d19a4926c45

--- trunk/admin/Java/shapefile/CMDUtilities.java
+++ trunk/admin/Java/shapefile/CMDUtilities.java
@@ -1,11 +1,11 @@
 /**
  *    PVCS Identifiers :-
  *
- *       sccsid           : $Header:   //new_vm_latest/archives/nm3/admin/Java/shapefile/CMDUtilities.java-arc   1.0   Apr 24 2015 06:20:58   Upendra.Hukeri  $
+ *       sccsid           : $Header:   //new_vm_latest/archives/nm3/admin/Java/shapefile/CMDUtilities.java-arc   1.1   Jul 11 2019 12:45:50   Upendra.Hukeri  $
  *       Module Name      : $Workfile:   CMDUtilities.java  $
- *       Date into SCCS   : $Date:   Apr 24 2015 06:20:58  $
- *       Date fetched Out : $Modtime:   Apr 21 2015 12:54:50  $
- *       SCCS Version     : $Revision:   1.0  $
+ *       Date into SCCS   : $Date:   Jul 11 2019 12:45:50  $
+ *       Date fetched Out : $Modtime:   Jul 11 2019 12:43:26  $
+ *       SCCS Version     : $Revision:   1.1  $
  *       Based on 
  *
  *
@@ -14,93 +14,218 @@
  *
  *    CMDUtilities.java
  ****************************************************************************************************
- *	  Copyright (c) 2015 Bentley Systems Incorporated.  All rights reserved.
+ *	  Copyright (c) 2019 Bentley Systems Incorporated. All rights reserved. 
  ****************************************************************************************************
  *
  */
 
 import java.io.*;
 
-public class CMDUtilities {
-	String exitVal = "-1";
-	boolean checkProcOutput = false;
-	boolean isStrOutput = false;
-	String result = null;
+public class CMDUtilities { 
+	private static final String revision = "$Revision:   1.1  $"; 
 	
-	public static String runCommand(String command, String successStr, String outputMode) {
-		CMDUtilities cmdUtil = new CMDUtilities();
-		
-		if (outputMode != null) {
-			if (outputMode.equalsIgnoreCase("STRING")) {
-				cmdUtil.isStrOutput = true;
-			} else if (outputMode.equalsIgnoreCase("INTEGER")) {
-				cmdUtil.isStrOutput = false;
-			} else {
-				return cmdUtil.exitVal;
-			}
-		} else {
-			return cmdUtil.exitVal;
-		}
-		
-		String s = null;
+	public static String getVersion() { 
+		return revision; 
+	} 
 	
-		try {
+	private static String executeCommand(String command) {
+		String result = null; 
+		
+		try { 
 			Runtime rt = Runtime.getRuntime();
-		  
+			
 			Process proc = rt.exec(command);
 			proc.waitFor();
 			
-			if (successStr != null) {
-				if (successStr.length() > 0) {
-					cmdUtil.checkProcOutput = true;
+			BufferedReader isReader = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+			BufferedReader esReader = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
+			
+			String s = null;
+			
+			while ((s = isReader.readLine()) != null) {
+				if (result == null) {
+					result = s;
+				} else {
+					result += "\n" + s; 
 				}
 			}
 			
-			if (cmdUtil.checkProcOutput) {
-				InputStream is = proc.getInputStream();
-				InputStreamReader reader = new InputStreamReader(is);
-				BufferedReader br = new BufferedReader(reader);
-				
-				while ((s = br.readLine()) != null) {
-					if (cmdUtil.result == null) {
-						cmdUtil.result = s;
+			if(result == null) {
+				while ((s = esReader.readLine()) != null) {
+					if (result == null) {
+						result = s;
 					} else {
-						cmdUtil.result = cmdUtil.result + "\n" + s;
-					}
-					
-					if (s.equals(successStr)) {
-						cmdUtil.exitVal = "1";
-						break;
+						result += "\n" + s; 
 					}
 				}
-				
-				reader.close();
-				br.close();
-			} else {
-				cmdUtil.exitVal = Integer.toString(proc.exitValue());
 			}
-		} catch (Exception processException) {
-			String expMsg = "Error: Process Exception:\n" + processException.getMessage();
 			
-			System.out.println(expMsg);
-		  
-			if (cmdUtil.result == null) {
-				cmdUtil.result = expMsg;
+			isReader.close();
+			esReader.close();
+		} catch (Exception processException) { 
+			StringWriter sw = new StringWriter();
+			PrintWriter  pw = new PrintWriter(sw);
+			
+			processException.printStackTrace(pw);
+			
+			String expMsg = "Error: Process Exception:\n" + sw.toString();
+			
+			if (result == null) {
+				result = expMsg;
 			} else {
-				cmdUtil.result = cmdUtil.result + "\n" + expMsg;
+				result += "\n" + expMsg;
 			}
 		}
 		
-		if (cmdUtil.result != null) {
-			if (cmdUtil.result.length() > 32767) {
-				cmdUtil.result = cmdUtil.result.substring(0, 32767);
-			}
+		if (result != null && result.length() > 32767) { 
+			result = result.substring(0, 32767); 
+		} 
+		
+		return result; 
+	} 
+	
+	public static String extractShapefile(String host, Integer port, String sid, String username, String password, String tableColumn, String filePath, String whereClause, String attributeMappingFilePath) {
+		String sdeUtilPath = System.getenv("SDE_UTIL_PATH");
+		
+		if(sdeUtilPath == null || sdeUtilPath.length() <= 0) {
+			return "SDE_UTIL_PATH Environment Variable not found"; 
 		}
 		
-		if (cmdUtil.isStrOutput) {
-			return cmdUtil.result;
-		} else {
-			return cmdUtil.exitVal;
+		String command = "java -jar \"" + sdeUtilPath + "\\lib\\sde2shp.jar\""; 
+		
+		if(host != null && host.length() > 0) {
+			command += " -h " + host; 
+		}  
+		
+		if(port != null) {
+			command += " -p " + port; 
+		}  
+		
+		if(sid != null && sid.length() > 0) {
+			command += " -s " + sid; 
+		}  
+		
+		if(username != null && username.length() > 0) {
+			command += " -u " + username; 
+		}  
+		
+		if(password != null && password.length() > 0) {
+			command += " -d " + password; 
+		}  
+		
+		if(tableColumn != null && tableColumn.length() > 0) {
+			command += " -t " + tableColumn; 
+		}  
+		
+		if(filePath != null && filePath.length() > 0) {
+			command += " -f \"" + filePath + "\""; 
+		} 
+		
+		if(whereClause != null && whereClause.length() > 0) {
+			command += " -w \"" + whereClause + "\""; 
+		} 
+		
+		if(attributeMappingFilePath != null && attributeMappingFilePath.length() > 0) {
+			command += " -a \"" + attributeMappingFilePath + "\""; 
+		} 
+		
+		return executeCommand(command); 
+	} 
+	public static String uploadShapefile(String host, Integer port, String sid, String username, String password, String tableName, String filePath, String uniqueColumnName, Integer srid, String geomColumnName, String xBounds, String yBounds, String tolerance, String modeOfOperation, Integer uniqueColumnStartID, Integer commitInterval, String attributeMappingFilePath) {
+		String sdeUtilPath = System.getenv("SDE_UTIL_PATH");
+		
+		if(sdeUtilPath == null || sdeUtilPath.length() <= 0) {
+			return "SDE_UTIL_PATH Environment Variable not found"; 
 		}
-	}
+		
+		String command = "java -jar \"" + sdeUtilPath + "\\lib\\shp2sde.jar\""; 
+		
+		if(host != null && host.length() > 0) {
+			command += " -h " + host; 
+		} 
+		
+		if(port != null) {
+			command += " -p " + port; 
+		} 
+		
+		if(sid != null && sid.length() > 0) {
+			command += " -s " + sid; 
+		} 
+		
+		if(username != null && username.length() > 0) {
+			command += " -u " + username; 
+		} 
+		
+		if(password != null && password.length() > 0) {
+			command += " -d " + password; 
+		} 
+		
+		if(tableName != null && tableName.length() > 0) {
+			command += " -t " + tableName; 
+		} 
+		
+		if(filePath != null && filePath.length() > 0) {
+			command += " -f \"" + filePath + "\""; 
+		} 
+		
+		if(uniqueColumnName != null && uniqueColumnName.length() > 0) {
+			command += " -i " + uniqueColumnName; 
+		} 
+		
+		if(srid != null) {
+			command += " -r " + srid; 
+		} 
+		
+		if(geomColumnName != null && geomColumnName.length() > 0) {
+			command += " -g " + geomColumnName; 
+		} 
+		
+		if(xBounds != null && xBounds.length() > 0) {
+			command += " -x \"" + xBounds + "\""; 
+		} 
+		
+		if(yBounds != null && yBounds.length() > 0) {
+			command += " -y \"" + yBounds + "\""; 
+		} 
+		
+		if(tolerance != null && tolerance.length() > 0) {
+			try {
+			Double d_tolerance = Double.valueOf(tolerance);
+			command += " -m " + tolerance; 
+			} catch(NumberFormatException nfe) {
+				return "Invalid tolerance value";
+			}
+		} 
+		
+		if(modeOfOperation != null && modeOfOperation.length() > 0) {
+			command += " -o " + modeOfOperation; 
+		} 
+		
+		if(uniqueColumnStartID != null) {
+			command += " -n " + uniqueColumnStartID; 
+		} 
+		
+		if(commitInterval != null) {
+			command += " -c " + commitInterval; 
+		} 
+		
+		if(attributeMappingFilePath != null && attributeMappingFilePath.length() > 0) {
+			command += " -a \"" + attributeMappingFilePath + "\""; 
+		} 
+		
+		return executeCommand(command); 
+	} 
+	
+	public static String checkXSS(String urlParameters) {
+		String xssUtilPath = System.getenv("XSS_UTIL_PATH");
+		
+		if(xssUtilPath == null || xssUtilPath.length() <= 0) {
+			return "XSS_UTIL_PATH Environment Variable not found"; 
+		}
+		
+		String command = "java -jar \"" + xssUtilPath + "\\tig-xss.jar\" \"" + urlParameters + "\""; 
+		
+		return executeCommand(command); 
+	} 
 }
+ 
