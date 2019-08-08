d305d3610dca39a3812341fa06e316b66603a2a1
Chris.Baugh <Chris.Baugh@17c802ae-cbca-534c-9e8e-6d19a4926c45>
2019-08-08 11:12:12 +0000

    svnimporter: adding tags to revision 12249
    git-svn-id: svn://127.0.0.1/Core@12250 17c802ae-cbca-534c-9e8e-6d19a4926c45

---
+++ tags/security_audit/install/dummy.sql
@@ -0,0 +1,16 @@
+----------------------------------------------------------------------------------------------------
+--   PVCS Identifiers :-
+--
+--       PVCS id          : $Header:   //new_vm_latest/archives/nm3/install/dummy.sql-arc   2.1   Aug 08 2019 12:12:12   Chris.Baugh  $
+--       Module Name      : $Workfile:   dummy.sql  $ 
+--       Date into PVCS   : $Date:   Aug 08 2019 12:12:12  $
+--       Date fetched Out : $Modtime:   Aug 08 2019 12:07:50  $
+--       Version     	  : $Revision:   2.1  $
+--
+----------------------------------------------------------------------------------------------------
+--   Copyright (c) 2019 Bentley Systems Incorporated. All rights reserved.
+----------------------------------------------------------------------------------------------------
+--
+REM @(#)dummy.sql	1.1 02/15/05
+REM Dummy script to be called as an alternative to a genuine install/upgrade script
+REM For example from nm3data_install.sql when nm3data8 should not be called if Oracle spatial not installed
